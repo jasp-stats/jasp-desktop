6302dd52978332f3c0e5eedb1a5b09e0e9ea3171
neekro <smira.martin@gmail.com>
2013-09-12 16:21:24 +0200

    Updates to analyses

--- JASP-Engine/JASP/R/ttestindependentsamples.R
+++ JASP-Engine/JASP/R/ttestindependentsamples.R
@@ -1,51 +1,188 @@
 
-TTestIndependentSamples <- function(data, options, perform="run", callback=NULL) {
+TTestIndependentSamples <- function(dataset, options, perform="run", callback=NULL) {
+
+	install.packages("car",repos='http://cran.us.r-project.org')
+	library(car)
 
 	results <- list()
-
 	ttest <- list()
+	descriptives <- list()
+	footnotes <- list()
 
 	ttest[["title"]] <- "Independent Samples T-Test"
-	ttest[["cases"]] <- I(options$variables)
 
 	fields <- list(
+		list(id="Variance Assumption", type="text"),
 		list(id="t", type="number", format="sf:4"),
 		list(id="df", type="number", format="sf:4"),
-		list(id="p", type="number", format="dp:4;p:.001"),
-		list(id="mean difference", type="number", format="sf:4"))
+		list(id="p", type="number", format="dp:4;p:.001"))
 
+	if (options$meanDifference) {
+		fields[[length(fields) + 1]] <- list(id="Mean Difference", type="number", format="sf:4")
+		fields[[length(fields) + 1]] <- list(id="Std. Error Difference", type="number", format="sf:4")	
+	}
+	if (options$confidenceInterval) {
+		fields[[length(fields) + 1]] <- list(id="Lower CI", type="number", format="sf:4")
+		fields[[length(fields) + 1]] <- list(id="Upper CI", type="number", format="sf:4")
+		
+		# Footnote informing what confidence interval is printed 
+		CINote <- paste("The Confidence Interval is ",options$confidenceIntervalInterval*100,"%",sep="") 
+		footnotes[[2]] <- CINote
+	}
+		
 	ttest[["schema"]] <- list(fields=fields)
-
-	if (perform == "run")
-	{
-		ttest.results <- list()
-
-		for (variable in options[["variables"]])
-		{
-			result <- try (silent = TRUE, expr = {
-
-				r <- t.test(dataset[[variable]])
-
-				t <- as.numeric(r$statistic)
-				df <- as.numeric(r$parameter)
-				p <- as.numeric(r$p.value)
-				m <- as.numeric(r$estimate)
-
-				list(t=t, df=df, p=p, "mean difference"=m)
-			})
-
-			if (class(result) == "try-error")
-				result <- list(t="", df="", p="", "mean difference"="")
-
-			ttest.results[[length(ttest.results)+1]] <- result
-		}
 	
+	if (perform == "run") {	
+		
+		ttest.results <- list()
+		
+		na <- FALSE
+		if (options$missingValues == "excludeListwise") {
+			for (variable in options[["variables"]]) {
+				na <- is.na(dataset[[variable]]) | na
+			}
+		}	
+		
+		if (options$tails == "oneTailedGreaterThan") {
+			testType <- "greater"
+			hypothesisNote <- "Alternative hypothesis: true difference in means is greater than 0"
+			footnotes[[1]] <- hypothesisNote
+		} else if (options$tails == "oneTailedLessThan") {
+			testType <- "less"
+			hypothesisNote <- "Alternative hypothesis: true difference in means is less than 0"
+			footnotes[[1]] <- hypothesisNote
+		} else {
+			testType <- "two.sided"
+			hypothesisNote <- "Alternative hypothesis: true difference in means is not equal to 0"
+			footnotes[[1]] <- hypothesisNote
+		}
+		
+		if (options$equalityOfVariances == "assumeEqual") { 
+			assume <- c(TRUE, NA)
+			assumption=c("equal", NA)
+			repet <- 1
+		} else if (options$equalityOfVariances == "assumeUnequal") {
+			assume <- c(FALSE, NA)
+			assumption=c("unequal", NA)
+			repet <- 1
+		} else {  # assuming both
+			assume <- c(TRUE, FALSE)
+			assumption=c("equal", "unequal")
+			repet <- 2
+		}
+		
+		cases <- list()
+		groupingVar <- dataset[[options$groupingVariable]][!na] 
+		
+		for (variable in options[["variables"]]) {
+
+			variableData <- dataset[[variable]][!na]
+			
+			levene <- leveneTest(variableData, groupingVar, "mean")
+			
+			if (levene[1,3] > .05) {
+				LeveneNote <- "Levene's Test suggests variances being equal"
+			} else {
+				LeveneNote <- "Levene's Test suggests that variances are NOT equal"
+			}
+			footnotes[[3]] <- LeveneNote
+			
+			for (rep in 1:repet) {
+				
+				
+				cases[[length(cases) + 1]] <- variable				
+				
+				result <- try (silent=TRUE, expr= {
+					
+		
+					r <- t.test(variableData ~ groupingVar, alternative=testType,
+								var.equal=assume[rep], conf.level=options$confidenceIntervalInterval)
+									
+					
+					t <- as.numeric(r$statistic)
+					df <- as.numeric(r$parameter)
+					p <- as.numeric(r$p.value)
+					m <- as.numeric(r$estimate[1]) - as.numeric(r$estimate[2])
+					ciLow <- .clean(r$conf.int[1])
+					ciUp <- .clean(r$conf.int[2])
+					if (testType == "two.sided") {
+						sed <- .clean((ciUp - ciLow) / (2 * qt(options$confidenceIntervalInterval,r$parameter)))  # beckward approach - getting spread of CI and deviding by critical value for t
+					} else if (testType == "less") {
+						sed <- "???"# .clean((ciUp - m) / (qt(options$confidenceIntervalInterval,r$parameter)))
+					} else {
+						sed <- "???"# .clean((m - ciLow) / (qt(options$confidenceIntervalInterval,r$parameter)))
+					}
+					
+					list("Variance Assumption" = assumption[rep], t=t, df=df, p=p, "Mean Difference"=m, 
+						 "Lower CI"=ciLow, "Upper CI"=ciUp, "Std. Error Difference"=sed)
+				})
+
+				if (class(result) == "try-error") {
+					result <- list("Variance Assumption" = "", t="", df="", p="", "Mean Difference"="",
+								   "Lower CI"="", "Upper CI"="", "Std. Error Difference"="")
+				}
+				ttest.results[[length(ttest.results)+1]] <- result
+			}
+		}
+		ttest[["cases"]] <- cases
+		
+		if (options$descriptives) {
+			
+			cases <- list()
+			for (i in seq(length(options$variables))) {  # This could be done better I guess, also for nominal variables we would prefere just one (empty) line instead 
+				for (x in 1:2) {
+					cases[[length(cases) + 1]] <- options$variables[i]
+				}
+			}
+
+			fields <- list(
+				list(id="groups", type="text"),
+				list(id="N", type="number", format="sf:4"),
+				list(id="Mean", type="number", format="sf:4"),
+				list(id="Std. Deviation", type="number", format="dp:4;p:.001"),
+				list(id="Std. Error Mean", type="number", format="sf:4"))
+
+			descriptives[["title"]] <- "Group Statistics"	
+			descriptives[["cases"]] <- cases	
+			descriptives[["schema"]] <- list(fields=fields)
+			
+			descriptives.results <- list()
+			
+			for (variable in options[["variables"]]) {
+				for (group in 1:2) {
+					variableData <- dataset[[variable]][!na]  # Listwise NA reduction 
+					
+					level <- levels(groupingVar)[group]  # deviding variable of interest into 2 groups
+					groupData <- variableData[groupingVar == level]
+					groupDataOm <- na.omit(groupData)
+
+					if (class(groupDataOm) != "factor") {
+
+						n <- .clean(length(groupDataOm))
+						mean <- .clean(mean(groupDataOm))
+						stdDeviation <- .clean(sd(groupDataOm))
+						stdErrorMean <- .clean(sd(groupDataOm) / sqrt(length(groupDataOm)))
+						
+						result <- list(groups=level, N=n, Mean=mean, "Std. Deviation"=stdDeviation,
+										"Std. Error Mean"=stdErrorMean)						
+					} else {
+						n <- .clean(length(groupDataOm))
+						result <- list(groups="", N=n, Mean="", "Std. Deviation"="", "Std. Error Mean"="")
+					}
+					descriptives.results[[length(descriptives.results) + 1]] <- result
+				}
+			}
+			
+			descriptives[["data"]] <- descriptives.results
+		}
+		
 		ttest[["data"]] <- ttest.results
+		ttest[["footnotes"]] <- footnotes
 	}
 
 	results[["ttest"]] <- ttest
 	results[["inequalityOfVariances"]] <- NULL  # levene's in a separate table
-	results[["descriptives"]] <- NULL
+	results[["descriptives"]] <- descriptives
 
 	results
 }
--- JASP-Engine/JASP/R/ttestonesample.R
+++ JASP-Engine/JASP/R/ttestonesample.R
@@ -1,52 +1,124 @@
 
-TTestOneSample <- function(data, options, perform="run", callback=NULL) {
+TTestOneSample <- function(dataset, options, perform="run", callback=NULL) {
 
 	results <- list()
 
 	ttest <- list()
-
+	descriptives <- list()
+	footnotes <- list()
+	
 	ttest[["title"]] <- "One Sample T-Test"
 	ttest[["cases"]] <- I(options$variables)
 
 	fields <- list(
 		list(id="t", type="number", format="sf:4"),
 		list(id="df", type="number", format="sf:4"),
-		list(id="p", type="number", format="dp:4;p:.001"),
-		list(id="mean difference", type="number", format="sf:4"))
-
+		list(id="p", type="number", format="dp:4;p:.001"))
+		
+	if (options$meanDifference) {
+		fields[[length(fields) + 1]] <- list(id="Mean Difference", type="number", format="sf:4")
+	}
+	if (options$confidenceInterval) {
+		fields[[length(fields) + 1]] <- list(id="Lower CI", type="number", format="sf:4")
+		fields[[length(fields) + 1]] <- list(id="Upper CI", type="number", format="sf:4")
+		
+		# Footnote informing what confidence interval is printed 
+		CINote <- paste("The Confidence Interval is ", options$confidenceIntervalInterval * 100, "%", sep="") 
+		footnotes[[2]] <- CINote
+	}
+	
 	ttest[["schema"]] <- list(fields=fields)
 
-	#ttest[["testValue"]] <- options$testValue
-
-	if (perform == "run")
-	{
+	if (perform == "run") {
 		ttest.results <- list()
 
-		for (variable in options[["variables"]])
-		{
+		na <- FALSE
+		if (options$missingValues == "excludeListwise") {
+			for (variable in options[["variables"]]) {
+				na <- is.na(dataset[[variable]]) | na
+			}
+		} 		
+		for (variable in options[["variables"]]) {
+
 			result <- try (silent = TRUE, expr = {
-
-				r <- t.test(dataset[[variable]])
-
+			
+				if (options$tails == "oneTailedGreaterThan") {
+					testType <- "greater"
+					hypothesisNote <- paste("Alternative hypothesis: true mean is greater than ",
+											options$testValue, ".", sep="")
+					footnotes[[1]] <- hypothesisNote
+				} else if (options$tails == "oneTailedLessThan") {
+					testType <- "less"
+					hypothesisNote <- paste("Alternative hypothesis: true mean is less than ",
+											options$testValue, ".", sep="")
+					footnotes[[1]] <- hypothesisNote
+				} else {
+					testType <- "two.sided"
+					hypothesisNote <- paste("Alternative hypothesis: true difference in means is not equal to ",
+											options$testValue, ".", sep="")
+					footnotes[[1]] <- hypothesisNote
+				}
+				
+				r <- t.test(dataset[[variable]][!na], alternative=testType, mu=options$testValue,
+							conf.level=options$confidenceIntervalInterval)
+				
 				t <- as.numeric(r$statistic)
 				df <- as.numeric(r$parameter)
 				p <- as.numeric(r$p.value)
-				m <- as.numeric(r$estimate)
-
-				list(t=t, df=df, p=p, "mean difference"=m)
+				m <- as.numeric(r$estimate - r$null.value)
+				ciLow <- .clean(as.numeric(r$conf.int[1]) - as.numeric(r$null.value)) 
+				ciUp <- .clean(as.numeric(r$conf.int[2]) - as.numeric(r$null.value)) 
+				
+				list(t=t, df=df, p=p, "Mean Difference"=m, "Lower CI"=ciLow, "Upper CI"=ciUp)
 			})
-
-			if (class(result) == "try-error")
-				result <- list(t="", df="", p="", "mean difference"="")
-
+			if (class(result) == "try-error") {
+				result <- list(t="", df="", p="", "Mean Difference"="",	"Lower CI"="", "Upper CI"="")
+			}
 			ttest.results[[length(ttest.results)+1]] <- result
 		}
-	
+
+		if (options$descriptives) {
+			descriptives[["title"]] <- "Descriptives"
+			descriptives[["cases"]] <- I(options$variables)
+
+			fields <- list(
+				list(id="N", type="number", format="sf:4"),
+				list(id="Mean", type="number", format="sf:4"),
+				list(id="Std. Deviation", type="number", format="dp:4;p:.001"),
+				list(id="Std. Error Mean", type="number", format="sf:4"))
+
+			descriptives[["schema"]] <- list(fields=fields)
+			descriptives.results <- list()
+			
+			for (variable in options[["variables"]]) {
+				variable <- dataset[[variable]][!na]
+				omitVariable <- na.omit(variable)
+				
+				if (class(omitVariable) != "factor") {
+
+					n <- .clean(length(omitVariable))
+					mean <- .clean(mean(omitVariable))
+					stdDeviation <- .clean(sd(omitVariable))
+					stdErrorMean <- .clean(sd(omitVariable)/sqrt(length(omitVariable)))
+
+					result <- list(N = n, Mean = mean, "Std. Deviation" = stdDeviation,
+									"Std. Error Mean" = stdErrorMean)
+				} else {
+					n <- .clean(length(omitVariable))
+					result <- list(N = n, Mean = "", "Std. Deviation" = "", "Std. Error Mean" = "")
+				}
+				descriptives.results[[length(descriptives.results)+1]] <- result
+			}
+			descriptives[["data"]] <- descriptives.results
+		}	
 		ttest[["data"]] <- ttest.results
+		ttest[["footnotes"]] <- footnotes
 	}
-
+	
 	results[["ttest"]] <- ttest
+	results[["descriptives"]] <- descriptives
 
+	
 	results
 }
 
