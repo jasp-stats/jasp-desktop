b58f8d81effb10814d372d0a769e056bb3d4a8d5
Jonathon Love <jon@thon.cc>
2014-08-26 21:20:35 +0200

    Bayesian ANOVA UI: hid unused options

--- JASP-Desktop/analysisforms/ancovabayesianform.cpp
+++ JASP-Desktop/analysisforms/ancovabayesianform.cpp
@@ -42,6 +42,16 @@ AncovaBayesianForm::AncovaBayesianForm(QWidget *parent) :
 	_anovaModel = new TableModelAnovaModel(this);
 	ui->modelTerms->setModel(_anovaModel);
 	ui->modelTerms->hide();
+
+#ifdef QT_NO_DEBUG
+	// temporary hides until the appropriate R code is implemented
+
+	ui->posteriorDistributions->hide();
+	ui->posteriorEstimates->hide();
+#else
+	ui->posteriorDistributions->setStyleSheet("background-color: pink;");
+	ui->posteriorEstimates->setStyleSheet("background-color: pink;");
+#endif
 }
 
 AncovaBayesianForm::~AncovaBayesianForm()
