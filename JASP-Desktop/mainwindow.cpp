jasp-stats/jasp-desktop

commit e915bb3876e523c302f8c769c46a7b502b737a89

Fixes/hacks around linux issues

JASP-Desktop/mainwindow.cpp
@@ -164,7 +164,7 @@ void MainWindow::analysisResultsChangedHandler(Analysis *analysis)
 {
 	string results = analysis->asJSON().toStyledString();
 
-	QString eval = tq("window.analysisChanged(" + results + ")");
+    QString eval = tq("window.analysisChanged(JSON.parse('" + results + "'));").replace("\n", "");
 
 	ui->webViewResults->page()->mainFrame()->evaluateJavaScript(eval);
 }

JASP-Common/lib_json/json_writer.cpp
@@ -71,6 +71,21 @@ std::string valueToString( double value )
 #else	
    sprintf(buffer, "%#.16g", value); 
 #endif
+
+   // Jonathon's Hack, on Linux, sometimes sprintf puts , instead of . for dec. place
+   // this leads to bad JSON
+
+   for (int i = 0; buffer[i] != '\0'; i++)
+   {
+       if (buffer[i] == ',')
+       {
+           buffer[i] = '.';
+           break;
+       }
+   }
+
+   // end Jonathon's Hack
+
    char* ch = buffer + strlen(buffer) - 1;
    if (*ch != '0') return buffer; // nothing to truncate, so save time
    while(ch > buffer && *ch == '0'){

commit 5bffc641facefbe2b507e21181fb4c299dbd9c70

Fix to properly escape JSON with single quotes
This issue was introduced with the commit
e915bb3876e523c302f8c769c46a7b502b737a89

JASP-Desktop/mainwindow.cpp
@@ -162,11 +162,13 @@ void MainWindow::resizeEvent(QResizeEvent *event)
 
 void MainWindow::analysisResultsChangedHandler(Analysis *analysis)
 {
-	string results = analysis->asJSON().toStyledString();
+	QString results = tq(analysis->asJSON().toStyledString());
 
-    QString eval = tq("window.analysisChanged(JSON.parse('" + results + "'));").replace("\n", "");
+	results = results.replace("'", "\\'");
+	results = results.replace("\n", "");
+	results = "window.analysisChanged(JSON.parse('" + results + "'));";
 
-	ui->webViewResults->page()->mainFrame()->evaluateJavaScript(eval);
+	ui->webViewResults->page()->mainFrame()->evaluateJavaScript(results);
 }
 
 AnalysisForm* MainWindow::loadForm(Analysis *analysis)