16cbe4577ca65cb5cd3c85135fbcb4ad8a6d02f1
Jonathon Love <jon@thon.cc>
2014-12-12 17:42:33 +0100

    B Crosstabs: Updated the architecture
    Brought into line with classical crosstabs

--- JASP-Engine/JASP/R/crosstabsbayesian.R
+++ JASP-Engine/JASP/R/crosstabsbayesian.R
@@ -1,129 +1,245 @@
 
-.crosstabBayesian <- function(dataset, perform, analysis) {
+.crosstabBayesian <- function(dataset, options, perform, analysis) {
 
 	# analysis is a list of the form :
 	# list(columns="var.name", rows="var.name", "Layer 1"="var.name", etc...)
+	
+	dataset <- subset(dataset, select=.v(unlist(analysis)))
+	
+	
+	# the following creates a 'groups' list
+	# a 'group' represents a combinations of the levels from the layers
+	# if no layers are specified, groups is null
 
-	a.factor.contains.no.levels <- FALSE
-
-	lvls <- c()
-	if (is.factor( dataset[[ .v(analysis$rows) ]] )) {
-	
-		lvls <- levels(dataset[[ .v(analysis$rows) ]])
-		
-	} else if (perform == "run") {
-	
-		lvls <- unique(dataset[[ .v(analysis$rows) ]])
-	}
-	
-	if (length(lvls) == 0) {
-	
-		lvls = c(".")
-		a.factor.contains.no.levels <- TRUE
-	}
-		
-	cases <- data.frame(lvls, stringsAsFactors=FALSE)
-	names(cases) <- analysis$rows
-	
-	if (length(analysis) > 2)
+	if (length(analysis) >= 3)  # if layers are specified
 	{
-		for (j in 3:length(analysis))
-		{
-			var.name <- analysis[[j]]
+		lvls <- base::levels(dataset[[ .v(analysis[[3]]) ]])
 		
-			lvls <- c()
-			if (is.factor(dataset[[ .v(var.name) ]])) {
+		if (length(lvls) < 2) {
+		
+			lvls <- ""
 			
-				lvls <- levels(dataset[[ .v(var.name) ]])
-			}
-			else if (perform == "run") {
-			
-				lvls <- unique(dataset[[ .v(var.name) ]] )
-			}
-			
-			if (length(lvls) == 0) {
-			
-				lvls = c(".")
-				a.factor.contains.no.levels <- TRUE
-			}
-			
-			cases <- cbind(rep(lvls, each=dim(cases)[1]), cases, stringsAsFactors=FALSE)
-			names(cases)[1] <- var.name
+		} else {
+		
+			lvls <- c(lvls, "")  # blank means total
 		}
+
+		# here we create all combinations of the levels from the layers
+		# it is easiest to do this with a data frame
+		# at the end we convert this to a list of rows
+
+		groups <- data.frame(lvls, stringsAsFactors=FALSE)
+		base::names(groups) <- analysis[[3]]
+		
+		if (length(analysis) >= 4) {
+		
+			for (j in 4:length(analysis))
+			{
+				lvls <- base::levels(dataset[[ .v(analysis[[j]]) ]])
+				lvls <- c(lvls, "")  # blank means total
+			
+				groups <- cbind(rep(lvls, each=dim(groups)[1]), groups, stringsAsFactors=FALSE)
+				names(groups)[1] <- analysis[[j]]
+			}
+		}
+		
+		# convert all the combinations to a list of rows
+		
+		groups <- .dataFrameToRowList(groups)
+		
+	} else {  # if layers are not specified
+	
+		groups <- NULL
 	}
 	
-	cases <- .dataFrameToRowList(cases)
 
 	tables <- list()
 
-	table <- list()
-	
-	table[["title"]] <- "Bayesian Crosstabs"
+
+    ### SETUP COLUMNS COMMON TO BOTH TABLES
 
 	fields <- list()
 	
-	if (length(analysis) > 2) {
+	if (length(analysis) >= 3) {
 	
 		for (j in length(analysis):3)
 			fields[[length(fields)+1]] <- list(name=analysis[[j]], type="string", combine=TRUE)
 	}
-				
-	fields[[length(fields)+1]] <- list(name=analysis$rows, type="string")
 	
+
+	
+	### SETUP COUNTS TABLE SCHEMA
+
+	counts.table <- list()
+	
+	counts.table[["title"]] <- "Bayesian Crosstabs"
+	
+	counts.fields <- fields
+	
+	counts.fields[[length(counts.fields)+1]] <- list(name=analysis$rows, type="string", combine=TRUE)
+
 	lvls <- c()
 	if (is.factor(dataset[[ .v(analysis$columns) ]] )) {
 
-		lvls <- levels(dataset[[ .v(analysis$columns) ]])
+		lvls <- base::levels(dataset[[ .v(analysis$columns) ]])
 
 	} else if (perform == "run") {
 	
-		lvls <- unique(dataset[[ .v(analysis$columns) ]])
-	}
-	
-	if (length(lvls) == 0) {
-	
-		lvls <- c(".")
-		a.factor.contains.no.levels <- TRUE
+		lvls <- base::unique(dataset[[ .v(analysis$columns) ]])
 	}
 	
 	for (column.name in lvls) {
 
-		private.name <- paste(".", column.name, sep="")
-		fields[[length(fields)+1]] <- list(name=private.name, title=column.name, type="number")
+		private.name <- base::paste(".", column.name, sep="")
+		counts.fields[[length(counts.fields)+1]] <- list(name=private.name, title=column.name, type="number")
+	}
+
+	schema <- list(fields=counts.fields)
+
+	counts.table[["schema"]] <- schema
+	
+	
+	
+
+	### SETUP TESTS TABLE SCHEMA
+
+	tests.table <- list()
+	
+	tests.table[["title"]] <- "Bayesian Crosstabs Tests"
+	
+	tests.fields <- fields
+	
+	tests.fields[[length(tests.fields)+1]] <- list(name="type[N]", title="", type="string")
+	tests.fields[[length(tests.fields)+1]] <- list(name="value[N]", title="Value", type="integer")
+
+	if (options$oddsRatio) {
+	
+		tests.fields[[length(tests.fields)+1]] <- list(name="type[oddsRatio]", title="", type="string")
+		tests.fields[[length(tests.fields)+1]] <- list(name="value[oddsRatio]", title="Value", type="number", format="sf:4;dp:3")
 	}
 	
-	if (perform == "run" && a.factor.contains.no.levels == FALSE) {
+	schema <- list(fields=tests.fields)
+	
+	tests.table[["schema"]] <- schema
+	
+	
 
-		for (i in .indices(cases)) {
 
-			caze <- cases[[i]]
-			
-			ss.filter.string <- paste(.v(names(caze)), "==\"", caze, "\"", sep="", collapse="&")
-			ss.expression <- parse(text=ss.filter.string)
-			
-			ss <- subset(dataset, select=.v(analysis$columns), subset=eval(ss.expression))
-			
-			t <- base::table(ss)
+	# POPULATE TABLES
 
-			for (n in names(t)) {
-			
-				private.name <- paste(".", n, sep="")
-				caze[[private.name]] <- unname(t[n])
-			}
+	# create count matrices for each group
 
-			cases[[i]] <- caze
+	group.matrices <- .crosstabsCreateGroupMatrices(dataset, .v(analysis$rows), .v(analysis$columns), groups)
+	
+	counts.rows <- list()
+	tests.rows <- list()
+	
+	tests.footnotes <- .newFootnotes()
+
+	for (i in 1:length(group.matrices)) {
+	
+		group.matrix <- group.matrices[[i]]
+		
+		if ( ! is.null(groups)) {
+		
+			group <- groups[[i]]
+			
+		} else {
+		
+			group <- NULL
 		}
 	
+		next.rows <- .crosstabsCreateCountsRows(analysis$rows, group.matrix, options, perform, group)
+		counts.rows <- c(counts.rows, next.rows)
+		
+		next.rows <- .crosstabsBayesianCreateTestsRows(analysis$rows, group.matrix, tests.footnotes, options, perform, group)
+		tests.rows <- c(tests.rows, next.rows)
 	}
+
+	counts.table[["data"]] <- counts.rows
 	
-	schema <- list(fields=fields)
+	tests.table[["data"]] <- tests.rows
+	tests.table[["footnotes"]] <- as.list(tests.footnotes)
 
-	table[["schema"]] <- schema
-	table[["data"]] <- cases
-
-	tables[[1]] <- table
+	tables[[1]] <- counts.table
+	tables[[2]] <- tests.table
 	
 	tables
+}
+
+.crosstabsBayesianCreateTestsRows <- function(var.name, counts.matrix, footnotes, options, perform, group=NULL) {
+
+	row <- list()
+	
+	for (layer in names(group)) {
+	
+		level <- group[[layer]]
+		
+		if (level == "") {
+
+			row[[layer]] <- "Total"
+						
+		} else {
+		
+			row[[layer]] <- level
+		}
+	}
+	
+	row[["type[N]"]] <- "N"
+
+	if (perform == "run") {
+	
+		row[["value[N]"]] <- base::sum(counts.matrix)
+		
+	} else {
+	
+		row[["value[N]"]] <- "."
+	}
+	
+	
+	if (options$oddsRatio) {
+	
+		row[["type[oddsRatio]"]] <- "Odds ratio"
+
+		if (perform == "run") {
+		
+			chi.result <- try({
+
+				chi.result <- stats::chisq.test(counts.matrix)
+			})
+			
+			if (class(chi.result) == "try-error") {
+
+				row[["value[oddsRatio]"]] <- .clean(NaN)
+				
+				error <- .extractErrorMessage(chi.result)
+				
+				if (error == "at least one entry of 'x' must be positive")
+					error <- "\u03A7\u00B2 could not be calculated, contains no observations"
+				
+				sup   <- .addFootnote(footnotes, error)
+				row[[".footnotes"]] <- list("value[oddsRatio]"=list(sup))
+			
+			} else if (is.na(chi.result$statistic)) {
+			
+				row[["value[oddsRatio]"]] <- .clean(NaN)
+			
+				sup <- .addFootnote(footnotes, "\u03A7\u00B2 could not be calculated")
+				row[[".footnotes"]] <- list("value[oddsRatio]"=list(sup))
+			
+			} else {
+			
+				row[["value[oddsRatio]"]] <- unname(chi.result$statistic)
+			}
+			
+		} else {
+		
+			row[["value[oddsRatio]"]] <- "."
+		}
+		
+	}
+
+	list(row)
 }
 
 CrosstabsBayesian <- function(dataset=NULL, options, perform="run", callback=function(...) 0, ...) {
@@ -177,7 +293,7 @@ CrosstabsBayesian <- function(dataset=NULL, options, perform="run", callback=fun
 
 		for (analysis in analyses)
 		{
-			tables <- .crosstabBayesian(dataset, perform, analysis)
+			tables <- .crosstabBayesian(dataset, options, perform, analysis)
 			for (table in tables)
 				crosstabs[[length(crosstabs)+1]] <- table				
 		}
