4709d3c125573a5ba2d9bb74dd85ec6b69b9c44b
mmarsman1 <m.marsman@uva.nl>
2014-12-17 16:17:01 +0100

    Initial implementation for Bayesian Regression
    This is an initial implementation of the Bayesian Regression analysis.

--- JASP-Engine/JASP/R/regressionlinearbayesian.R
+++ JASP-Engine/JASP/R/regressionlinearbayesian.R
@@ -1,5 +1,9 @@
-
 RegressionLinearBayesian <- function(dataset=NULL, options, perform="run", callback=function(...) 0, ...) {
 
-	list(.meta=list())
+    options$fixedFactors <- NULL
+    options$randomFactors <- NULL
+
+    results <- AncovaBayesian(dataset, options, perform, callback)
+    
+    return(results)
 }
