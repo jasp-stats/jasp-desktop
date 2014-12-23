83fe85fdc916dacd7863f223a826333c78078191
Jonathon Love <jon@thon.cc>
2014-12-23 16:35:40 +0100

    B Correlation: Reduced it to a skeleton
    So someone can start work on it easily

--- JASP-Engine/JASP/R/correlationbayesian.R
+++ JASP-Engine/JASP/R/correlationbayesian.R
@@ -28,90 +28,29 @@ CorrelationBayesian <- function(dataset=NULL, options, perform="run", callback=f
 	
 	results[[".meta"]] <- meta
 	
-	results[["correlations"]] <- .correlationTableBayesian(dataset, perform,
-		variables=options$variables, pearson=options$pearson,
-		kendallsTauB=options$kendallsTauB, spearman=options$spearman,
-		hypothesis=options$hypothesis, reportSignificance=options$reportSignificance,
-		flagSignificant=options$flagSignificant,
-		meansAndStdDev=options$meansAndStdDev, crossProducts=options$crossProducts)
+	results[["correlations"]] <- .correlationTableBayesian(dataset, perform, variables=options$variables)
 
 	results
 }
 
-.correlationTableBayesian <- function(dataset, perform, variables=c(), pearson=TRUE, kendallsTauB=FALSE,
-	spearman=FALSE, hypothesis="correlated", reportSignificance=FALSE,
-	flagSignificant=FALSE, meansAndStdDev=FALSE, crossProducts=FALSE) {
+.correlationTableBayesian <- function(dataset, perform, variables) {
 	
 	correlation.table <- list()
 
-	tests <- c()
-	if (pearson)
-		tests <- c(tests, "pearson")
-	if (spearman)
-		tests <- c(tests, "spearman")
-	if (kendallsTauB)
-		tests <- c(tests, "kendall")
-
-	if (length(tests) != 1) {
-	
-		correlation.table[["title"]] <- "Correlation Table"
-	}
-	else if (pearson) {
-	
-		correlation.table[["title"]] <- "Pearson Correlations"
-	}
-	else if (spearman) {
-	
-		correlation.table[["title"]] <- "Spearman Correlations"
-	}
-	else if (kendallsTauB) {
-	
-		correlation.table[["title"]] <- "Kendall's Tau"
-	}
-	else {
-	
-		correlation.table[["title"]] <- "Correlation Table"
-	}
+	correlation.table[["title"]] <- "Correlation Table"
 	
 	fields <- list(list(name=".variable", title="", type="string"))
 	rows <- list()
-	v.c <- length(variables)
+	v.c <- length(variables)  # variable count
 	
 	if (v.c > 0) {
-			
-		test.names <- list(pearson="Pearson's R", spearman="Spearman's Rho", kendall="Kendall's Tau B")
 
 		column.names <- c()
-
-		for (test in tests) {
+			
+		for (variable.name in variables) {
 		
-			if (length(tests) > 1 || reportSignificance) {
-			
-				column.name <- paste(".test[", test, "]", sep="")
-				column.names[[length(column.names)+1]] <- column.name
-				fields[[length(fields)+1]] <- list(name=column.name, title="", type="string")
-			}
-				
-			for (variable.name in variables) {
-			
-				column.name <- paste(variable.name, "[", test, "]", sep="")
-				column.names[[length(column.names)+1]] <- column.name
-				fields[[length(fields)+1]] <- list(name=column.name, title=variable.name, type="number", format="dp:3")
-			}
-			
-			if (reportSignificance) {
-			
-				column.name <- paste(".test[", test, "-p]", sep="")
-				column.names[[length(column.names)+1]] <- column.name
-				fields[[length(fields)+1]] <- list(name=column.name, title="", type="string")
-
-				for (variable.name in variables) {
-			
-					column.name <- paste(variable.name, "[", test, "-p]", sep="")
-					column.names[[length(column.names)+1]] <- column.name
-					fields[[length(fields)+1]] <- list(name=column.name, title=variable.name, type="number", format="dp:3;p:.001")
-				}								
-			}
+			column.names[[length(column.names)+1]] <- variable.name
+			fields[[length(fields)+1]] <- list(name=variable.name, title=variable.name, type="number", format="dp:3")
 		}
 		
 		for (i in 1:v.c) {
@@ -121,80 +60,31 @@ CorrelationBayesian <- function(dataset=NULL, options, perform="run", callback=f
 			
 			variable.name <- variables[[i]]
 			
-			for (test in tests) {
+			for (j in .seqx(1, i-1)) {         # fill in blanks in table
 			
-				p.values <- list()
-
-				if (length(tests) > 1 || reportSignificance)
-					row[[length(row)+1]] <- test.names[[test]]
-					
-				if (reportSignificance)
-					p.values[[length(p.values)+1]] <- "p-value"
-			
-				for (j in .seqx(1, i-1)) {
-				
-					row[[length(row)+1]] <- ""
-					p.values[[length(p.values)+1]] <- ""
-				}
-
-				row[[length(row)+1]] <- "\u2014" # em-dash
-				p.values[[length(p.values)+1]] <- ""
-
-				for (j in .seqx(i+1, v.c)) {
-
-					variable.2.name <- variables[[j]]
-		
-					if (perform == "run") {
-				
-						if (hypothesis == "correlated") {
-
-							result   <- cor.test(dataset[[ .v(variable.name) ]], dataset[[ .v(variable.2.name) ]], method=test, alternative="two.sided")
-							estimate <- as.numeric(result$estimate)
-
-							p.value  <- as.numeric(result$p.value)
-							
-						}
-						else {
-						
-							result1 <- cor.test(dataset[[ .v(variable.name) ]], dataset[[ .v(variable.2.name) ]], method=test, alternative="less")
-							result2 <- cor.test(dataset[[ .v(variable.name) ]], dataset[[ .v(variable.2.name) ]], method=test, alternative="greater")
-							estimate <- as.numeric(result1$estimate)
-
-							p.value  <- min(as.numeric(result1$p.value), as.numeric(result2$p.value))
-						}
-						
-						row[[length(row)+1]] <- .clean(estimate)
-						
-						if (flagSignificant && is.na(p.value) == FALSE && p.value < .05) {
-						
-							column.name <- paste(variable.2.name, "[", test, "]", sep="")
-							row.footnotes[[column.name]] <- list(0)
-						}
-						
-						if (reportSignificance)
-							p.values[[length(p.values)+1]] <- p.value
-					
-					} else {
-				
-						row[[length(row)+1]] <- "."
-						p.values[[length(p.values)+1]] <- "."
-						
-					}
-				}
-				
-				if (reportSignificance) {
-				
-					for (p.value in p.values)
-						row[[length(row)+1]] <- p.value
-				}
-			
+				variable.2.name <- variables[[j]]
+				row[[variable.2.name]] <- ""
 			}
-	
-			names(row) <- column.names
-			row[[".variable"]] <- variable.name
 			
-			if (length(row.footnotes) > 0)
-				row[[".footnotes"]] <- row.footnotes
+			row[[".variable"]] <- variable.name
+
+			row[[variable.name]] <- "\u2014" # em-dash
+
+			for (j in .seqx(i+1, v.c)) {
+
+				variable.2.name <- variables[[j]]
+	
+				if (perform == "run") {
+
+					result <- cor.test(dataset[[ .v(variable.name) ]], dataset[[ .v(variable.2.name) ]], method="pearson", alternative="two.sided")
+					
+					row[[variable.2.name]] <- .clean(unname(result$estimate))
+				
+				} else {
+			
+					row[[variable.2.name]] <- "."
+				}
+			}
 	
 			rows[[i]] <- row
 		}
@@ -204,18 +94,6 @@ CorrelationBayesian <- function(dataset=NULL, options, perform="run", callback=f
 	
 	correlation.table[["schema"]] <- schema
 	correlation.table[["data"]] <- rows
-	
-	if (flagSignificant) {
-	
-		if (hypothesis == "correlated") {
-		
-			correlation.table[["footnotes"]] <- list(list(symbol="*", text="p < .05"))
-			
-		} else {
-		
-			correlation.table[["footnotes"]] <- list(list(symbol="*", text="p < .05, one tailed"))
-		}
-	}
 	
 	correlation.table
 }
