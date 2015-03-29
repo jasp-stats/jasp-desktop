6ecb7d47ecc7dee844b3474061d97e90c952d2b3
quentingronau <quentingronau@web.de>
2015-03-29 16:33:09 +0200

    B ind. t test: update sequential plot

--- JASP-Engine/JASP/R/ttestbayesianonesample.R
+++ JASP-Engine/JASP/R/ttestbayesianonesample.R
@@ -866,7 +866,7 @@
 				
 			}
 			
-			if (length(xx) > 1 && length(yy) > 1 && !all(xx == yy)) {
+			if (length(xx) > 1 && length(yy) > 1 && !all(xx == yy) && sd(xx) > 0 && sd(yy) > 0) {
 			
 				if (oneSided == FALSE) {
 				
