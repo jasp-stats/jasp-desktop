b46df8cb3165f7316a42670f5b41ed18ce386f9e
Jonathon Love <jon@thon.cc>
2015-04-23 13:08:11 +0200

    Implemented OS directed file opening

--- JASP-Desktop/main.cpp
+++ JASP-Desktop/main.cpp
@@ -17,14 +17,5 @@ int main(int argc, char *argv[])
 	QLocale::setDefault(QLocale(QLocale::English)); // make decimal points == .
 
 	Application a(argc, argv);
-	MainWindow w;
-
-	w.show();
-
-	QStringList args = QApplication::arguments();
-
-	if (args.length() > 1)
-		w.open(args.at(1));
-    
     return a.exec();
 }
