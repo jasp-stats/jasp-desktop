37355ede0e171258198cf7d182de391781f2014d
Jonathon Love <jon@thon.cc>
2015-06-17 11:20:16 +0200

    B Corr Matrix: Enabling prior width option
    Closes #523

--- JASP-Desktop/analysisforms/correlationbayesianform.cpp
+++ JASP-Desktop/analysisforms/correlationbayesianform.cpp
@@ -26,12 +26,10 @@ CorrelationBayesianForm::CorrelationBayesianForm(QWidget *parent) :
 	ui->optionsWidget->hide();
     ui->credibleIntervalsIntervalContainer->hide();
     ui->credibleIntervals->hide();
-	ui->groupPrior->hide();
 #else
 	ui->optionsWidget->setStyleSheet("background-color: pink ;");
 	ui->credibleIntervalsIntervalContainer->setStyleSheet("background-color: pink ;");
 	ui->credibleIntervals->setStyleSheet("background-color: pink ;");
-	ui->groupPrior->setStyleSheet("background-color: pink ;");
 #endif
 
 	ui->credibleIntervalsInterval->setLabel("Credible intervals");
