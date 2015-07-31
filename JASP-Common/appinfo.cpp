50646d892d7ccceec3b6b3aa50c94c9badcbf66f
Jonathon Love <jon@thon.cc>
2015-07-31 13:03:41 +0200

    Version bump: 0.7.1 Release

--- JASP-Common/appinfo.cpp
+++ JASP-Common/appinfo.cpp
@@ -1,6 +1,6 @@
 #include "appinfo.h"
 
-const Version AppInfo::version = Version(0, 0, 7, 1, 103);
+const Version AppInfo::version = Version(0, 0, 7, 1, 250);
 const std::string AppInfo::name = "JASP";
 
 std::string AppInfo::getShortDesc(bool includeMassive)
