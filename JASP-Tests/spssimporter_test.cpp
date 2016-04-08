1f46ecf8a022de1a001c4dd68a0c87cffde5b2d9
akashraj <akashrajkn@gmail.com>
2016-04-08 15:55:16 +0200

    removed unneccessary headers

--- JASP-Tests/spssimporter_test.cpp
+++ JASP-Tests/spssimporter_test.cpp
@@ -15,9 +15,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-#pragma once
 #include "spssimporter_test.h"
-#include "csviterator.h"
 
 
 void SPSSImporterTest::initTestCase()
