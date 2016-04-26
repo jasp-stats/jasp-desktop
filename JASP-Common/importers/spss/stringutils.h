45c8c75fa588d6e6061c224060fb87d61a7b7420
Patrick Knight <aknight1@re-self-145-109-122-117.wireless.uva.nl>
2016-04-26 17:55:46 +0200

    Added Copyright header to SPSS files.

--- JASP-Common/importers/spss/stringutils.h
+++ JASP-Common/importers/spss/stringutils.h
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2016 University of Amsterdam
+// Copyright (C) 2015-2016 University of Amsterdam
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
+
 
 #ifndef __SPSS_STRING_UTILS_H_
 #define __SPSS_STRING_UTILS_H_
