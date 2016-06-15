2010dfe290dd1bc86459f292489b739b9604d7a6
etothej <erikjan.vankesteren@gmail.com>
2016-06-15 14:18:38 +0200

    Implemented State System in Binomial Test

--- JASP-Engine/JASP/R/binomialtest.R
+++ JASP-Engine/JASP/R/binomialtest.R
@@ -21,154 +21,222 @@ BinomialTest <- function(dataset = NULL, options, perform = "run",
 	variables <- unlist(options$variables)
 
 	if (is.null(dataset)) {
-
 		if (perform == "run") {
-
 			dataset <- .readDataSetToEnd(columns.as.numeric=NULL, 
 			                             columns.as.factor=variables, 
 			                             exclude.na.listwise=NULL)
-
 		} else {
-
 			dataset <- .readDataSetHeader(columns.as.numeric=NULL, 
 			                              columns.as.factor=variables)
-			
 		}
-
 	} else {
-
 		dataset <- .vdf(dataset, columns.as.numeric=NULL, 
 		                columns.as.factor=variables)
 	}
-
-	results <- list()
-
-	results[["title"]] <- "Binomial Test"
-
-	meta <- list(list(name="binomial", type="table"))
-	results[[".meta"]] <- meta
-
-	table <- list()
-
-	table[["title"]] <- "Binomial Test"
-
-	# set up table columns
-	schema <- list(fields=list(
-		list(name="case", title="", type="string", combine=TRUE),
-		list(name="level", title="Level", type="string"),
-		list(name="counts", title="Counts", type="integer"),
-		list(name="total", title="Total", type="integer"),
-		list(name="proportion", title="Proportion", type="number", 
-		     format="sf:4;dp:3"),
-		list(name="p", title="p", type="number", format="dp:3;p:.001")
-		))
-  
-	# if confidence interval required, add two columns
-	if (options$confidenceInterval) {
-	  interval <- 100 * options$confidenceIntervalInterval
-	  title <- paste0(interval, "% Confidence Interval")
+	
+	
+	## Retrieve State
+	
+	state <- .retrieveState()
+	
+	binomResults <- NULL
+	
+	if (!is.null(state)) {  # is there state?
 	  
-	  schema$fields[[length(schema$fields) + 1]] <- list(name = "lowerCI",
-	                                                     type = "number",
-	                                       format = "sf:4;dp:3", title = "Lower",
-	                                       overTitle = title)
-	  schema$fields[[length(schema$fields) + 1]] <- list(name = "upperCI", 
-	                                                     type = "number",
-	                                       format = "sf:4;dp:3", title = "Upper",
-	                                       overTitle = title)
+	  diff <- .diff(options, state$options)  # compare old and new options
+	  
+	  if (is.list(diff) && any(diff) == TRUE) {
+	    
+	    binomResults <- state$binomResults
+	    
+	  }
 	}
 	
-	table[["schema"]] <- schema
+	
+	results <- list()
+	
+	results[["title"]] <- "Binomial Test"
+	results[[".meta"]] <- list(list(name="binomial", type="table"))
+	
+	if (is.null(binomResults)){
+	  binomResults <- .binomialTest(dataset, options, variables, perform)
+	}
+	
+	results[["binomial"]] <- .binomialTable(binomResults, options, variables, 
+	                                        perform)
+	
 
-	data <- list()
 	
-	footnotes <- .newFootnotes()
+	# Save state
+	state[["options"]] <- options
+	state[["binomResults"]] <- binomResults
 	
-	if (options$hypothesis == "notEqualToTestValue") {
-		
-		hyp <- "two.sided"
-		message <- paste0("Proportions tested against value: ", options$testValue, 
-		                  ".")
-		.addFootnote(footnotes, symbol="<em>Note.</em>", text=message)
-		
-	} else if (options$hypothesis == "greaterThanTestValue") {
-		
-		hyp <- "greater"
-		note <- "For all tests, the alternative hypothesis specifies that the proportion
-					is greater than "
-		message <- paste0(note, options$testValue, ".")
-		.addFootnote(footnotes, symbol="<em>Note.</em>", text=message)
-		
+	
+	if (perform == "init") {
+	  
+	  return(list(results=results, status="inited", state=state))
+	  
 	} else {
-		
-		hyp <- "less"
-		note <- "For all tests, the alternative hypothesis specifies that the proportion
-					is less than "
-		message <- paste0(note, options$testValue, ".")
-		.addFootnote(footnotes, symbol="<em>Note.</em>", text=message)
-		
+	  
+	  return(list(results=results, status="complete", state=state))	
+	  
 	}
 	
-	if (perform == "run" && !is.null(variables)) {
-
-		for (var in variables) {
-
-			d <- dataset[[.v(var)]]
-			d <- d[!is.na(d)]
-			
-			levels <- levels(d)
-			n <- length(d)
-						
-			for (lev in levels) {
-				
-				counts <- sum(d == lev)
-				prop <- counts/n
-				
-				r <- stats::binom.test(counts, n, p = options$testValue, 
-				                       alternative = hyp, 
-				                       conf.level = options$confidenceIntervalInterval)
-				
-				p <- r$p.value
-				cil <- r$conf.int[1]
-				ciu <- r$conf.int[2]
-				
-				if (p == FALSE) {
-					p <- 0
-				} else if (p == TRUE) {
-					p <- 1
-				}
-				
-				row <- list(case=var, level=lev, counts=.clean(counts), total=.clean(n), 
-				            proportion=.clean(prop), p=.clean(p), lowerCI = .clean(cil),
-				            upperCI = .clean(ciu))
-				
-				if (lev == levels[1]) {
-					row[[".isNewGroup"]] <- TRUE
-				} else {
-					row[[".isNewGroup"]] <- FALSE
-				}
-				
-				data[[length(data) + 1]] <- row
-			}
-		}
-		
-	} else {
-		
-		if (is.null(variables))
-			variables <- ""
+}
 	
-		for (var in variables)
-			data[[length(data) + 1]] <- list(case=var, level=".", counts=".", 
-			                                 total=".",  proportion=".", p=".",
-			                                 lowerCI=".", upperCI=".")
-		
-	}
+.binomialTable <- function(r, options, variables, perform){
+  
+  table <- list()
+  table[["title"]] <- "Binomial Test"
+  
+  fields <- list(
+    list(name="case", title="", type="string", combine=TRUE),
+    list(name="level", title="Level", type="string"),
+    list(name="counts", title="Counts", type="integer"),
+    list(name="total", title="Total", type="integer"),
+    list(name="proportion", title="Proportion", type="number", 
+         format="sf:4;dp:3"),
+    list(name="p", title="p", type="number", format="dp:3;p:.001")
+  )
+  
+  if (options$confidenceInterval) {
+    
+    interval <- 100 * options$confidenceIntervalInterval
+    title <- paste0(interval, "% Confidence Interval")
+    
+    fields[[length(fields)+1]] <- list(name = "lowerCI",
+                                       type = "number",
+                                       format = "sf:4;dp:3", title = "Lower",
+                                       overTitle = title)
+    fields[[length(fields)+1]] <- list(name = "upperCI", 
+                                       type = "number",
+                                       format = "sf:4;dp:3", title = "Upper",
+                                       overTitle = title)
+  }
+  
+  table[["schema"]] <- list(fields = fields)
+  
+  
+  if (!is.null(r)){
+    
+    table[["data"]] <- r$data
+    table[["status"]] <- "complete"
+    
+    footnotes <- .newFootnotes()
+    .addFootnote(footnotes, symbol="<em>Note.</em>", text=r$message)
+    table[["footnotes"]] <- as.list(footnotes)
+    
+  } else {
+    
+    data <- list()
+    
+    if (is.null(variables)){
+      variables <- ""
+    } 
+    
+    for (var in variables){
+      data[[length(data) + 1]] <- list(case=var, level=".", counts=".", 
+                                       total=".",  proportion=".", p=".",
+                                       lowerCI=".", upperCI=".")
+    }
+    
+    table[["data"]] <- data
+    
+  }
+  
+  return(table)
+}
 
-	table[["data"]] <- data
-	
-	table[["footnotes"]] <- as.list(footnotes)
 
-	results[["binomial"]] <- table
 
-	results
-} 
+
+.binomialTest <- function(dataset, options, variables, perform){
+  if (perform == "run" && !is.null(variables)) {
+    
+    binomResults <- list()
+    
+    if (options$hypothesis == "notEqualToTestValue") {
+      
+      hyp <- "two.sided"
+      message <- paste0("Proportions tested against value: ", options$testValue, 
+                        ".")
+      
+    } else if (options$hypothesis == "greaterThanTestValue") {
+      
+      hyp <- "greater"
+      note <- "For all tests, the alternative hypothesis specifies that the 
+      proportion	is greater than "
+      message <- paste0(note, options$testValue, ".")
+      
+    } else {
+      
+      hyp <- "less"
+      note <- "For all tests, the alternative hypothesis specifies that the
+      proportion is less than "
+      message <- paste0(note, options$testValue, ".")
+      
+    }
+    
+    binomResults[["message"]] <- message
+    
+    
+    data <- list()
+    
+    for (var in variables) {
+      
+      d <- dataset[[.v(var)]]
+      d <- d[!is.na(d)]
+      
+      levels <- levels(d)
+      n <- length(d)
+      
+      # !! Test each level in each variable against test value !!
+      for (lev in levels) {
+        
+        counts <- sum(d == lev)
+        prop <- counts/n
+        
+        r <- stats::binom.test(counts, n, p = options$testValue, 
+                               alternative = hyp, 
+                               conf.level = options$confidenceIntervalInterval)
+        
+        p <- r$p.value
+        cilo <- r$conf.int[1]
+        ciup <- r$conf.int[2]
+        
+        if (p == FALSE) {
+          p <- 0
+        } else if (p == TRUE) {
+          p <- 1
+        }
+        
+        row <- list(case = var, level = lev, counts = .clean(counts), 
+                    total = .clean(n), proportion = .clean(prop), p = .clean(p), 
+                    lowerCI = .clean(cilo), upperCI = .clean(ciup))
+        
+        if (lev == levels[1]) {
+          row[[".isNewGroup"]] <- TRUE
+        } else {
+          row[[".isNewGroup"]] <- FALSE
+        }
+        
+        data[[length(data)+1]] <- row
+        
+      }
+      
+    }
+    
+    binomResults[["data"]] <- data
+    
+  } else {
+    
+    binomResults <- NULL
+      
+  }
+  
+  return(binomResults)
+      
+}
+
+
+
