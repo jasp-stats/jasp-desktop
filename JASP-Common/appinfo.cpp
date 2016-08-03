c0e73a04c25bd62b5b184b5a874e39ef974eb12b
bruno boutin <b.boutin@uva.nl>
2016-08-03 13:03:54 +0200

    Set version to 0.8.0 Beta 4

--- JASP-Common/appinfo.cpp
+++ JASP-Common/appinfo.cpp
@@ -18,9 +18,9 @@
 #include "appinfo.h"
 
 
-const Version AppInfo::version = Version(0, 8, 0, 102);
+const Version AppInfo::version = Version(0, 8, 0, 104);
 const std::string AppInfo::name = "JASP";
-const std::string AppInfo::builddate = "Thu Jul 14 2016 13:30:41 GMT+0100 (CET)";
+const std::string AppInfo::builddate = "Thu Aug 03 2016 13:30:41 GMT+0100 (CET)";
 
 std::string AppInfo::getShortDesc()
 {
