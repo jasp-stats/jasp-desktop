08deadecca9b42cb7c5239bef3960c965febf455
FransMeerhoff <F.A.M.Meerhoff@uva.nl>
2016-09-21 14:43:00 +0200

    Fixes #1463 Sumstats issue
    Disable OK button for Summary Statistics
    It s a bit confusing when running SumStats te get an empty panel on OK
    pressed, because no data needed. Fixes ##1463

--- JASP-Common/analysisloader.cpp
+++ JASP-Common/analysisloader.cpp
@@ -46,6 +46,7 @@ Analysis *AnalysisLoader::load(int id, string analysisName, Json::Value *data)
 			perror("malformed analysis definition");
 
 		bool autorun = analysisDesc.get("autorun", false).asBool();
+		bool usedata = analysisDesc.get("usedata", true).asBool();
 		Version version = Version(analysisDesc.get("version", "0.00").asString());
 
 		if (data != NULL)
@@ -53,7 +54,7 @@ Analysis *AnalysisLoader::load(int id, string analysisName, Json::Value *data)
 
 		file.close();
 
-		return new Analysis(id, analysisName, options, version, autorun);
+		return new Analysis(id, analysisName, options, version, autorun, usedata);
 	}
 
 	throw runtime_error("Could not access analysis definition.");
