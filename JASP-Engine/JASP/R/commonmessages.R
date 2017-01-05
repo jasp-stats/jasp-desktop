38b4ad47dfc0954f6569df6b8903074d666bdf9c
TimKDJ <tim_jong@hotmail.com>
2017-01-05 18:08:32 +0100

    Added: grouping levels support, ability to use 'all.', improved the checks

--- JASP-Engine/JASP/R/commonmessages.R
+++ JASP-Engine/JASP/R/commonmessages.R
@@ -2,12 +2,13 @@
   messages <- list()
   # error
   messages$error$opening <- "The following problem(s) occurred while running the analysis:"
+  messages$error$grouping <- 'after grouping on {{groupingVars}}'
   messages$error$infinity <- "Infinity found in {{variables}}"
   messages$error$factorLevels <- "Number of factor levels {{factorLevels.operator}} {{factorLevels.amount}} in {{variables}}"
   messages$error$variance <- "Variance = {{variance.equalTo}} in {{variables}}"
   messages$error$observations <- "Number of observations < {{observations.lessThan}} in {{variables}}"
   messages$error$unexpected <- "This analysis terminated unexpectedly.<br><br>Error in {{error}}<br><br><div class=stack-trace-selector>Stack trace:</div><div class=stack-trace>{{stackTrace}}</div><br>To receive assistence with this problem, please report the message above at: https://jasp-stats.org/bug-reports"
-
+  
   return(messages[[type]][[id]])
 }
 # TODO: add citations, titles, footnotes, additional error messages
