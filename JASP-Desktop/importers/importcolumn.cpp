59c66ac75ae6f4e340454b91b7c72b92b096da84
Patrick Knight <a.g.knight@uva.nl>
2017-01-27 14:03:13 +0100

    Removed ImportColumn::_longName
    Removed ImportColumn::_longName, also all references to it (getters,
    setters etc.)

--- JASP-Desktop/importers/importcolumn.cpp
+++ JASP-Desktop/importers/importcolumn.cpp
@@ -1,6 +1,7 @@
 #include "importcolumn.h"
 
-ImportColumn::ImportColumn(string name, string longName) : _name(name), _longName(longName)
+ImportColumn::ImportColumn(string name)
+	: _name(name)
 {
 }
 
@@ -11,12 +12,4 @@ ImportColumn::~ImportColumn()
 string ImportColumn::getName() const
 {
 	return _name;
-}
-
-string ImportColumn::getLongName() const
-{
-	if (!_longName.empty())
-		return _longName;
-	else
-		return _name;
 }
