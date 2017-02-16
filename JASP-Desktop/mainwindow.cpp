0b1910b56b963534ee2f49fa450d4dd4b7322819
bruno boutin <b.boutin@uva.nl>
2017-02-16 14:15:35 +0100

    Add warning when default spreadsheet editor not found

--- JASP-Desktop/mainwindow.cpp
+++ JASP-Desktop/mainwindow.cpp
@@ -1994,6 +1994,9 @@ void MainWindow::startDataEditor(QString path)
 	}
 	else
 	{
-		QDesktopServices::openUrl(QUrl("file:///" + path, QUrl::TolerantMode));
+		if (!QDesktopServices::openUrl(QUrl("file:///" + path, QUrl::TolerantMode)))
+		{
+			QMessageBox::warning(this, QString("Start Spreadsheet Editor"), QString("No default spreadsheet editor for file ") + fileInfo.completeBaseName() + QString(". Use Preferences to set the right editor."), QMessageBox::Cancel);
+		}
 	}
 }
