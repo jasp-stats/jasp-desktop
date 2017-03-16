b09e227a5e3375770ca39d2ae5b64195c9fffd24
bruno boutin <b.boutin@uva.nl>
2017-03-16 16:53:35 +0100

    Set Version for 0.8.1.1

--- JASP-Common/appinfo.cpp
+++ JASP-Common/appinfo.cpp
@@ -17,7 +17,7 @@
 
 #include "appinfo.h"
 
-const Version AppInfo::version = Version(0, 8, 1, 255);
+const Version AppInfo::version = Version(0, 8, 1, 256);
 const std::string AppInfo::name = "JASP";
 const std::string AppInfo::builddate = __DATE__ " " __TIME__ " (Netherlands)" ;
 
--- Tools/make-dmg.sh
+++ Tools/make-dmg.sh
@@ -1,7 +1,7 @@
 QT_DIR=~/Qt/5.5
 R_FRAMEWORK=~/JASP/Build/Frameworks/R.framework
 JASP_DESKTOP=~/JASP/Build/jasp-desktop
-JASP_VERSION=0.8.1.0
+JASP_VERSION=0.8.1.1
 
 # This script builds the JASP.dmg installer
 # Check that you R.framework is unique (no other test versions).
