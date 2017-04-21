938cfa7d8b16e670814b2187d8ca5f49108d880f
Tim de Jong <tim_jong@hotmail.com>
2017-04-21 22:14:19 +0200

    Revert earlier commit and add R v3.3 functions

--- JASP-Engine/JASP/R/packagecheck.R
+++ JASP-Engine/JASP/R/packagecheck.R
@@ -350,11 +350,14 @@
 }
 
 
-.initPackages <- function(installed) {
+.initPackages <- function(installed.packages) {
 	
-	# First call to BayesFactor initializes its functions and speeds up subsequent calls.
-	if ('BayesFactor' %in% installed) {
-		try(BayesFactor::BFInfo(print=FALSE), silent=TRUE)
+	packages <- c("BayesFactor") # Add any package that needs pre-loading
+	
+	for (package in packages) {
+		if (package %in% installed.packages && base::isNamespaceLoaded(package) == FALSE) {
+			try(base::loadNamespace(package), silent=TRUE)
+		}
 	}
 	
 }
