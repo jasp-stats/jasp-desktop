84ae65f436b7f887a4f866e07bfc7ae2414eb9ed
Erik-Jan van Kesteren <erikjan.vankesteren@gmail.com>
2017-07-11 09:46:46 +0100

    Descriptives split (#1844)
    * Split functionality in frequency plot
    
    * Refactor descriptives
    
    Refactor descriptives, split functionality for all plots. Table split functionality still needed.
    
    * Refactor frequency table for descriptives pt 1
    
    * First working concept of full split functionality
    
    State system does not work yet
    
    * Update: full working version
    
    to do: overtitles for main table
    
    * Add javascript overtitle capability
    
    Now tables with the property `casesAcrossColumns` can also have overtitles when passed the property `overTitle`.
    
    * Bugfixes and basic error handling
    
    * Add small space between lines overTitle
    
    * Properly align split label in UI
    
    * Rename newdescriptives to descriptives
    
    * remove console.log statements

--- JASP-Desktop/html/js/table.js
+++ JASP-Desktop/html/js/table.js
@@ -6,6 +6,7 @@ JASPWidgets.table = Backbone.Model.extend({
 		variables: [],
 		data: [],
 		casesAcrossColumns: false,
+		overTitle: false,
 		formats: null,
 		footnotes: [],
 		citation: null,
@@ -134,12 +135,33 @@ JASPWidgets.tablePrimative = JASPWidgets.View.extend({
 
 	},
 
-	_swapRowsAndColumns: function (columnHeaders, columns) {
+	_swapRowsAndColumns: function (columnHeaders, columns, optOverTitle) {
 
 		var newRowCount = columns.length - 1
 		var newColumnCount = columns[0].length + 1
-
-		var newColumnHeaders = columns[0]
+		
+		if (optOverTitle) {
+			
+			// Transform first column into overtitle, second into title
+			var newColumnHeaders = Array(newColumnCount-1);
+			for (var colNo = 0; colNo < newColumnCount - 1; colNo++) {
+				newColumnHeaders[colNo] = { content: columns[1][colNo].content,
+																		header: true,
+																		overTitle: columns[0][colNo].content,
+																		type: "string" };
+			}
+			// remove the column that became title
+			columns.shift();
+			columnHeaders.shift();
+			newRowCount--;
+			
+		} else {
+			
+			var newColumnHeaders = Array(newColumnCount);
+			var newColumnHeaders = columns[0];
+			
+		}
+		
 		var newColumns = Array(newColumnCount)
 
 		var cornerCell = columnHeaders.shift()
@@ -664,6 +686,7 @@ JASPWidgets.tablePrimative = JASPWidgets.View.extend({
 		var optTitle = this.model.get("title");
 		var optSubtitle = this.model.get("subtitle");
 		var optCasesAcrossColumns = this.model.get("casesAcrossColumns");
+		var optOverTitle = this.model.get("overTitle")
 		var optFootnotes = this.model.get("footnotes");
 		var optCitation = this.model.get("citation");
 		var optStatus = this.model.get("status");
@@ -855,7 +878,7 @@ JASPWidgets.tablePrimative = JASPWidgets.View.extend({
 
 		if (optCasesAcrossColumns) {
 
-			var swapped = this._swapRowsAndColumns(columnHeaders, cells)
+			var swapped = this._swapRowsAndColumns(columnHeaders, cells, optOverTitle)
 			cells = swapped.columns
 			columnHeaders = swapped.columnHeaders;
 			rowCount = swapped.rowCount
@@ -899,21 +922,39 @@ JASPWidgets.tablePrimative = JASPWidgets.View.extend({
 		}
 
 		if (columnHeaders.length > 0) {
-
-			var overTitles = false
-
+			
+			var overTitles = false;
+			var overTitleSpace = false;
+			var overTitlesArray = [];
+			// Find the overTitles
 			for (var i = 0; i < columnHeaders.length; i++) {
-
 				if (columnHeaders[i].overTitle) {
-
-					overTitles = true
-					break;
+					overTitlesArray.push(columnHeaders[i].overTitle);
 				}
+			}
+			
+			if (overTitlesArray.length > 0) {
+				// If we have an overTitle, we should make it
+				overTitles = true;
+			}
+			
+			var uniqueOverTitles = $.unique(overTitlesArray)
+			if (uniqueOverTitles.length > 1) {
+				// If we have more than one unique overTitle, we should make small 
+				// breaks in the line under the overTitle to indicate end of old and 
+				// start of new overTitle. NB: with this option, the line is not copied
+				// to text processor.
+				overTitleSpace = true;
 			}
 
 			if (overTitles) {
-
-				chunks.push('<tr class="over-title">')
+				
+				if (overTitleSpace) {
+					chunks.push('<tr class="over-title-space">')
+				} else {
+					chunks.push('<tr class="over-title">')
+				}
+				
 
 				var span = 1;
 				var oldTitle = columnHeaders[0].overTitle
@@ -933,15 +974,24 @@ JASPWidgets.tablePrimative = JASPWidgets.View.extend({
 						span++
 					}
 					else {
-
-						chunks.push('<th colspan="' + (2 * span) + '">' + oldTitle + '</th>')
+						if (overTitleSpace) {
+							chunks.push('<th colspan="' + (2 * span) + '"><div class="over-title-space">' + oldTitle + '</div></th>');
+						} else {
+							chunks.push('<th colspan="' + (2 * span) + '">' + oldTitle + '</th>');
+						}
 						oldTitle = newTitle
 						span = 1
 					}
 				}
 
-				if (newTitle == oldTitle)
-					chunks.push('<th colspan="' + (2 * span) + '">' + newTitle + '</th>')
+				if (newTitle == oldTitle) {
+					if (overTitleSpace) {
+						chunks.push('<th colspan="' + (2 * span) + '"><div class="over-title-space">' + newTitle + '</div></th>')
+					} else {
+						chunks.push('<th colspan="' + (2 * span) + '">' + newTitle + '</th>')
+					}
+				}
+					
 
 				chunks.push('</tr>')
 			}
