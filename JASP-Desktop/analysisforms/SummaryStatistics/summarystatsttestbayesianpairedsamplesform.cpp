44329bd9533e33626bd18c1e7748bcc40d3c1580
Raoul Grasman <rgrasman@uva.nl>
2017-07-15 13:58:16 +0200

    Added subjectivePrior widget to Summary Stats > Bayesian Paired Samples T-Test and made functional
    Added widget, and modified R-code to use .generalSummaryTtestBF that
    can handle subjective priors.

--- JASP-Desktop/analysisforms/SummaryStatistics/summarystatsttestbayesianpairedsamplesform.cpp
+++ JASP-Desktop/analysisforms/SummaryStatistics/summarystatsttestbayesianpairedsamplesform.cpp
@@ -24,6 +24,9 @@ SummaryStatsTTestBayesianPairedSamplesForm::SummaryStatsTTestBayesianPairedSampl
 	ui(new Ui::SummaryStatsTTestBayesianPairedSamplesForm)
 {
 	ui->setupUi(this);
+
+    // collapse more advanced options widget by default
+    ui->subjectivePriors_2->hide();
 }
 
 SummaryStatsTTestBayesianPairedSamplesForm::~SummaryStatsTTestBayesianPairedSamplesForm()
