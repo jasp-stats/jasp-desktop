bbc4f4b13c0c255796521295c2bb516060912074
vandenman <donvdbergh@hotmail.com>
2017-08-22 10:47:28 +0200

    whitespace cleaning + added functionality for multiple graphs

--- JASP-Engine/JASP/R/networkanalysis.R
+++ JASP-Engine/JASP/R/networkanalysis.R
@@ -31,16 +31,16 @@ NetworkAnalysis <- function (
 
 	## Read Dataset ## ----
 	variables <- unlist(options$variables)
-	
+
 	varsAsFactor <- NULL
 	if (options[["groupingVariable"]] != "")
 		varsAsFactor <- options[["groupingVariable"]]
-	
+
 
 	if (is.null(dataset)) {
 
 		if (perform == "run") {
-			
+
 			dataset <- .readDataSetToEnd(columns.as.numeric = variables, columns.as.factor = varsAsFactor, exclude.na.listwise = NULL)
 
 			if (options[["colorNodesBy"]] != "") # load data from variable that indicates groups
@@ -89,59 +89,59 @@ NetworkAnalysis <- function (
 		network = defArgs,
 		centrality = c(defArgs, "normalizeCentrality"),
 		# depends only on plotting arguments
-		networkPLT = c(defArgs, 
+		networkPLT = c(defArgs,
 					   "plotWidthNetwork", "plotHeightNetwork",
 					   "layout", "edgeColors", "repulsion", "edgeSize", "nodeSize", "colorNodesBy",
 					   "maxEdgeStrength", "minEdgeStrength", "cut", "showDetails", "nodeColors", "showLegend",
 					   "weightedNetwork", "signedNetwork", "missingValues"),
 		# depends only on plotting arguments
-		centralityPLT = c(defArgs, 
+		centralityPLT = c(defArgs,
 						  "plotWidthCentrality", "plotHeightCentrality",
 						  "normalizeCentrality", "weightedNetwork", "signedNetwork", "missingValues")
 
 	)
-	state[["network"]][["centrality"]] <- state[["centrality"]]
+	# state[["network"]][["centrality"]] <- state[["centrality"]]
 
 	## Do Analysis ## ----
 
 	# Sort out whether things are set to defaults or not.
-	if (length(variables) > 2) { 
+	if (length(variables) > 2) {
 
 		# check for errors, but only if there was a change in the data (which implies state[["network"]] is NULL)
 		if (is.null(state[["network"]])) {
-			
+
 			groupingVariable <- NULL
 			if (options[["groupingVariable"]] != "")
 				groupingVariable <- options[["groupingVariable"]]
-			
+
 			checks <- c("infinity", "variance", "observations")
 			anyErrors <- .hasErrors(dataset = dataset, perform = perform,
 									type = checks,
+									variance.target = variables, # otherwise the grouping variable always has variance == 0
 									variance.grouping = groupingVariable,
 									observations.amount = " < 3",
 									observations.grouping = groupingVariable,
 									exitAnalysisIfErrors = TRUE)
 		}
-		
+
 		network <- .doNetworkAnalysis(dataset = dataset, options = options, variables = variables, perform = perform,
 									  oldNetwork = state)
-		
+
 	} else {
-		
+
 		network <- state[["network"]]
-		
+
 	}
 
 	## Create Output ##  ----
-	browser()
 	keep <- NULL
 	results[["generalTB"]] <- .NWgeneralTB(network, dataset, options, perform)
-
+	# browser()
 	if (options[["tableFitMeasures"]]) {
 		results[["fitMeasuresTB"]] <- .fitMeasuresTB(network, options, perform)
 	}
 	if (options[["tableCentrality"]]) {
-		results[["centralityTB"]] <- .centralityTB(network, options, perform)
+		results[["centralityTB"]] <- .networkAnalysisCentralityTB(network, options, perform)
 	}
 	if (options[["plotNetwork"]]) {
 		results[["networkPLT"]] <- .makeNetworkPLT(network, options, perform, oldPlot = state[["networkPLT"]], plotType = "network")
@@ -158,7 +158,7 @@ NetworkAnalysis <- function (
 	# Save State
 	state <- list(
 		options = options,
-		network = network,
+		network = network[["network"]],
 		centrality = network[["centrality"]],
 		networkPLT = results[["networkPLT"]],
 		centralityPLT = results[["centralityPLT"]]
@@ -185,41 +185,45 @@ NetworkAnalysis <- function (
 	# early return if init
 	if (perform != "run")
 		return(NULL)
-	browser()
+
 	if (options[["groupingVariable"]] == "") { # one network
-		
+
 		dataset <- list(dataset)
-		
+
 	} else { # multiple networks
-		
+
 		dataset <- split(dataset, dataset[[.v(options[["groupingVariable"]])]], drop = TRUE)
-		
+
 	}
-	
+
 	# empty list for results
-	networkList <- list()
-	
+	networkList <- list(network = list(), centrality  = list())
+
+	# should never happen?
+	if (length(oldNetwork) != length(dataset))
+		oldNetwork <- NULL
+
 	# for every part of the dataset, do the analysis
-	for (nw in seq_along(dataset)) {	
-		
+	for (nw in seq_along(dataset)) {
+
 		data <- dataset[[nw]]
 		data[[.v(options[["groupingVariable"]])]] <- NULL # grouping variable is not a node in the network
-		network <- oldNetwork[[nw]] # NULL or something usuable
-		
-		if (is.null(network[["graph"]])) { # estimate network
-			
+		network <- oldNetwork[["network"]][[nw]] # NULL or something usuable
+
+		if (is.null(network)) { # estimate network
+
 			# fix input to match bootnet preferences
 			options[["rule"]] <- toupper(options[["rule"]])
 			if (options[["correlationMethod"]] == "auto")
 				options[["correlationMethod"]] <- "cor_auto"
-			
+
 			options[["isingEstimator"]] <- switch(options[["isingEstimator"]],
 												  "pseudoLikelihood" = "pl",
 												  "univariateRegressions" = "uni",
 												  "bivariateRegressions" = "bi",
 												  "logLinear" = "ll"
 			)
-			
+
 			.dots <- list(
 				corMethod   = options[["correlationMethod"]],
 				tuning      = options[["tuningParameter"]],
@@ -233,42 +237,42 @@ NetworkAnalysis <- function (
 				criterion   = options[["criterion"]],
 				sampleSize  = options[["sampleSize"]]
 			)
-			
+
 			# get available arguments for specific network estimation function. Remove unused ones.
 			# FOR FUTURE UPDATING: options[["estimator"]] MUST match name of function in bootnet literally (see ?bootnet::bootnet_EBICglasso).
 			funArgs <- formals(utils::getFromNamespace(paste0("bootnet_", options[["estimator"]]), ns = "bootnet"))
 			nms2keep <- names(funArgs)
 			.dots <- .dots[names(.dots) %in% nms2keep]
-			
+
 			# for safety, when estimator is changed but missing was pairwise (the default).
 			if (!isTRUE("pairwise" %in% eval(funArgs[["missing"]])))
 				.dots[["missing"]] <- "listwise"
-			
+
 			# some manual adjustments for these estimators
 			if (options[["estimator"]] == "huge") {
-				
+
 				if (.dots[["criterion"]] == "cv")
 					.dots[["criterion"]] == "ebic"
-				
+
 			} else if (options[["estimator"]] == "mgm") {
-				
+
 				.dots[["criterion"]] <- toupper(.dots[["criterion"]]) # this function wants capitalized arguments
-				
+
 				if (!(.dots[["criterion"]] %in% eval(funArgs$criterion)))
 					.dots[["criterion"]] <- "EBIC"
-				
+
 			} else if (options[["estimator"]] == "adalasso") {
-				
+
 				if (is.na(.dots[["nFolds"]]) || .dots[["nFolds"]] < 2) # estimator crashes otherwise
 					.dots[["nFolds"]] <- 2
-				
+
 			}
-			
+
 			# Fake png hack -- qgraph::qgraph() has an unprotected call to `par()`. `par()` always opens a new device if there is none.
 			# Perhaps ask Sacha to fix this in qgraph. Line 1119: if (DoNotPlot) par(pty = pty)
 			tempFileName <- getTempFileName()
 			grDevices::png(filename = tempFileName)
-			
+
 			# capture.output to get relevant messages (i.e. from qgraph::cor_auto "variables detected as...")
 			msg <- capture.output(
 				network <- bootnet::estimateNetwork(
@@ -278,65 +282,66 @@ NetworkAnalysis <- function (
 				)
 				, type = "message"
 			)
-			
+
 			dev.off() # close the fake png
-			unlink(tempFileName) # remove the fake png file
-			
+			file.remove(tempFileName) # remove the fake png file
+
 			network[["corMessage"]] <- msg
-			
+
 		}
-		
-		if (is.null(network[["centrality"]])) { # calculate centrality measures
-			
-			network[["centrality"]] <- qgraph::centrality(network[["graph"]], weighted = options[["weightedNetwork"]], signed = options[["signedNetwork"]], all.shortest.paths = FALSE)
-			
+
+		cent <- oldNetwork[["network"]][[nw]]
+		if (is.null(cent)) { # calculate centrality measures
+
+			cent <- qgraph::centrality(network[["graph"]], weighted = options[["weightedNetwork"]], signed = options[["signedNetwork"]], all.shortest.paths = FALSE)
+
 			# note: centrality table is (partially) calculated here so that centralityTable and centralityPlot don't compute the same twice.
-			TBcent <- as.data.frame(network[["centrality"]][c("Betweenness", "Closeness", "InDegree", "OutDegree")])
-			
+			TBcent <- as.data.frame(cent[c("Betweenness", "Closeness", "InDegree", "OutDegree")])
+
 			# adapted from qgraph::centrality_auto
 			wmat <- qgraph::getWmat(network$graph)
 			directedGraph <- ifelse(base::isSymmetric.matrix(object = wmat, tol = 1e-12), FALSE, TRUE)
 			weightedGraph <- ifelse(all(qgraph::mat2vec(wmat) %in% c(0, 1)), FALSE, TRUE)
-			
+
 			if (directedGraph) {
-				
+
 				if (weightedGraph) {
-					
+
 					colnames(TBcent)[3:4] <- c("InStrength", "OutDegree")
-					
+
 				} else { # unweighted
-					
+
 					colnames(TBcent)[3:4] <- c("InDegree", "OutDegree")
-					
+
 				}
-				
+
 			} else { # undirected
-				
+
 				# divide betweenness by 2
-				TBcent[, 1] <- TBcent[, 1] / 2 
+				TBcent[, 1] <- TBcent[, 1] / 2
 				TBcent <- TBcent[c(1:2, 4)]
-				
+
 				if (weightedGraph) {
-					
+
 					colnames(TBcent)[3] <- "Strength"
-					
+
 				} else { # unweighted
-					
+
 					colnames(TBcent)[3] <- "Degree"
-					
+
 				}
-				
+
 			}
-			
+
 			centVars <- apply(TBcent, 2, var)
 			if (options[["normalizeCentrality"]] == "normalized") { # normalize only if variances are nonzero
-				
+
 				for (i in 1:ncol(TBcent)) { # code modified from base::scale.default
-					
+
 					valid <- !is.na(TBcent[, i])
-					
+
 					if (sum(valid) != 0) {
-						
+
 						obs <- TBcent[valid, i]
 						obs <- obs - mean(obs)
 						stdev <- sqrt(sum(obs^2) / max(1, length(obs) - 1))
@@ -346,39 +351,40 @@ NetworkAnalysis <- function (
 							TBcent[valid, i] <- obs / stdev
 						}
 					}
-					
+
 				}
-				
+
 			} else if (options[["normalizeCentrality"]] == "relative") {
-				
+
 				for (i in 1:ncol(TBcent)) { # code modified from qgraph::centralityTable
-					
+
 					mx <- max(abs(TBcent[, i]), na.rm = TRUE)
-					
+
 					if (mx != 0) # avoid division by zero
 						TBcent[, i] <- TBcent[, i] / mx
-					
+
 				}
-				
+
 			} # else raw centrality measures -> do nothing
-			
+
 			TBcent[["node"]] <- .unv(network[["labels"]])
 			nc <- ncol(TBcent)
 			TBcent <- TBcent[c(nc, 1:(nc-1))] # put columns in intended order (of schema).
-			network[["centralityTable"]] <- TBcent
+			cent <- TBcent
 		}
-		
-		networkList[[nw]] <- network
-		
+
+		networkList[["network"]][[nw]] <- network
+		networkList[["centrality"]][[nw]] <- cent
+
 	}
-	
+
 	return(networkList)
 
 }
 
 # wrappers for output ----
 .NWgeneralTB <- function(network, dataset, options, perform) {
-	browser()
+
 	nGraphs <- max(1, length(network))
 	table <- list(
 		title = "Summary of Network",
@@ -388,13 +394,13 @@ NetworkAnalysis <- function (
 		))
 	)
 	for (i in 1:nGraphs) {
-		table[["schema"]][["fields"]][[i+1]] <- 
-			list(name = paste0("value ", i), title = paste0("network ", i), type = "number", format="sf:4;dp:3")
+		table[["schema"]][["fields"]][[i+1]] <-
+			list(name = paste0("value", i), title = paste0("network ", i), type = "number", format="sf:4;dp:3")
 	}
 
 	footnotes <- .newFootnotes()
 	msg <- NULL
-	
+
 	TBcolumns <- list(info = c("Number of nodes", "Number of non-zero edges", "Sparsity"))
 
 	if (perform != "run" || is.null(network)) { # fill in with .
@@ -403,17 +409,17 @@ NetworkAnalysis <- function (
 		table[["status"]] <- "inited"
 
 	} else { # fill in with info from bootnet:::print.bootnet
-		
+
 		for (i in 1:nGraphs) {
-			
-			nw <- network[[i]]
-			
-			TBcolumns[[paste0("network", i)]] <- c(
+
+			nw <- network[["network"]][[i]]
+
+			TBcolumns[[paste0("value", i)]] <- c(
 				nrow(nw[["graph"]]),
 				sum(nw[["graph"]][upper.tri(nw[["graph"]], diag = FALSE)] == 0),
 				mean(nw[["graph"]][upper.tri(nw[["graph"]], diag = FALSE)] == 0)
 			)
-		
+
 		}
 		table[["status"]] <- "complete"
 
@@ -431,8 +437,6 @@ NetworkAnalysis <- function (
 
 	data <- .TBcolumns2TBrows(TBcolumns)
 	table[["data"]] <- data
-	# print("msg")
-	# print(msg)
 	if (!is.null(msg)) {
 
 		msg <- paste(msg, collapse = "\n")
@@ -456,23 +460,38 @@ NetworkAnalysis <- function (
 
 }
 
-.centralityTB <- function(network, options, perform) {
+.networkAnalysisCentralityTB <- function(network, options, perform) {
+	# browser()
+	nGraphs <- max(1, length(network))
 
 	table <- list(
 		title = "Centrality measures per variable",
 		schema = list(fields = list(
-			list(name = "Variable",    title = "Variable",    type = "string"),
-			list(name = "Betweenness", title = "Betweenness", type = "number", format="sf:4;dp:3"),
-			list(name = "Closeness",   title = "Closeness",   type = "number", format="sf:4;dp:3"),
-			list(name = "Strength",    title = "Strength",    type = "number", format="sf:4;dp:3")
+			list(name = "Variable",    title = "Variable",    type = "string")
 		))
 	)
+
+	# shared titles
+	overTitles <- names(network[["network"]])
+	if (is.null(overTitles))
+		overTitles <- paste0("Network", 1:nGraphs)
+
+	# create the fields
+	for (i in seq_along(network)) {
+		# three centrality columns per network
+
+		table[["schema"]][["fields"]][[2 + 3*(i-1)]] <- list(name = paste0("Betweenness", i), title = "Betweenness", type = "number", format="sf:4;dp:3", overTitle = overTitles[i])
+		table[["schema"]][["fields"]][[3 + 3*(i-1)]] <- list(name = paste0("Closeness", i),   title = "Closeness",   type = "number", format="sf:4;dp:3", overTitle = overTitles[i])
+		table[["schema"]][["fields"]][[4 + 3*(i-1)]] <- list(name = paste0("Strength", i),    title = "Strength",    type = "number", format="sf:4;dp:3", overTitle = overTitles[i])
+
+	}
 
 	if (perform != "run") { # make empty table
 
 		if (!is.null(options[["variables"]]) || !(length(options[["variables"]]) > 0)) {
 
 			TBcolumns <- list(".", ".", ".", ".")
+			names(TBcolumns) <- c("Variable", "Betweenness", "Closeness", "Strength")
 
 		} else {
 			# dataframe since reshape2 below returns dataframes
@@ -488,11 +507,23 @@ NetworkAnalysis <- function (
 
 	} else { # fill with results
 
-		TBcolumns <- network[["centralityTable"]]
+		TBcolumns <- NULL
+		for (i in seq_along(network)) {
+
+			toAdd <- network[["centrality"]][[i]]
+			names(toAdd) <- c("Variable", paste0(c("Betweenness", "Closeness", "Strength"), i))
+			if (i == 1) {# if more than 1 network drop additional variable column
+				TBcolumns <- toAdd
+			} else {
+				toAdd <- toAdd[, -1]
+				TBcolumns <- cbind(TBcolumns, toAdd)
+			}
+
+		}
 
 	}
 
-	names(TBcolumns) <- c("Variable", "Betweenness", "Closeness", "Strength")
+	# names(TBcolumns) <- c("Variable", "Betweenness", "Closeness", "Strength")
 	data <- .TBcolumns2TBrows(TBcolumns)
 	table[["data"]] <- data
 
@@ -530,7 +561,7 @@ NetworkAnalysis <- function (
 			groups <- NULL
 			options[["showLegend"]] <- FALSE
 		}
-		
+
 		wMat <- network[["graph"]]
 		if (!options[["weightedNetwork"]]) {
 			wMat <- sign(wMat)
@@ -559,10 +590,17 @@ NetworkAnalysis <- function (
 
 }
 
-.centralityPlot2 <- function(wide) {
+.networkAnalysiscentralityPlot <- function(wide) {
 
-	Long <- reshape2::melt(wide)
-	colnames(Long)[2] <- "measure"
+	wideDf <- Reduce(rbind, wide)
+	if (length(wide) > 1) {
+		wideDf[["graph"]] <- rep(1:length(wide), each = nrow(wideDf) / length(wide))
+		Long <- reshape2::melt(wideDf, id.vars = c("node", "graph"))
+		colnames(Long)[3] <- "measure"
+	} else {
+		Long <- reshape2::melt(wideDf, id.vars = "node")
+		colnames(Long)[2] <- "measure"
+	}
 	Long[["type"]] <- NA
 
 	# code modified from qgraph::centralityPlot()
@@ -617,11 +655,13 @@ NetworkAnalysis <- function (
 	}
 
 	if (perform == "run") {
+		# browser()
+		if (plotType == "network")
+		network <- network[["network"]][[1]]
 
 		plotObjOrFun <- switch(plotType,
 			"network" = .plotFunNetwork,
-			"centrality" = .centralityPlot2(network[["centralityTable"]])#qgraph::centralityPlot(network[["graph"]], labels = .unv(network[["labels"]]), print = FALSE,
-											#	  weighted = options[["weightedNetwork"]], signed = options[["signedNetwork"]])
+			"centrality" = .networkAnalysiscentralityPlot(network[["centrality"]])
 		)
 
 		content <- .writeImage(width = plot[["width"]], height = plot[["height"]], plot = plotObjOrFun)
@@ -632,9 +672,6 @@ NetworkAnalysis <- function (
 		plot[["status"]] <- "complete"
 
 	}
-
-	# should go into .writeImage
-	# grDevices::graphics.off()
 
 	return(plot)
 
@@ -663,13 +700,13 @@ getTempFileName <- function() {
 .TBcolumns2TBrows <- function(infoByCol) do.call(mapply, c(FUN = base::list, infoByCol, SIMPLIFY = FALSE, USE.NAMES = FALSE))
 
 .networkAnalysisCentrality <- function (x, weighted = TRUE, signed = TRUE) {
-	
+
 	# modified version of centrality_auto that does not compute shortest path lengths
-	
+
 	x <- qgraph::getWmat(x)
 	if (is.list(x)) {
-		return(lapply(x, .networkAnalysisCentrality, 
-					  weighted = weighted, 
+		return(lapply(x, .networkAnalysisCentrality,
+					  weighted = weighted,
 					  signed = signed))
 	}
 	if (!isTRUE(weighted)) {
@@ -678,31 +715,31 @@ getTempFileName <- function() {
 	if (!isTRUE(signed)) {
 		x <- abs(x)
 	}
-	if (!is.matrix(x)) 
+	if (!is.matrix(x))
 		stop("the input network must be an adjacency or weights matrix")
 	diag(x) <- 0
 	x <- abs(x)
-	directed.gr <- ifelse(isSymmetric.matrix(object = x, tol = 1e-12), 
+	directed.gr <- ifelse(isSymmetric.matrix(object = x, tol = 1e-12),
 						  FALSE, TRUE)
-	weighted.gr <- ifelse(all(qgraph::mat2vec(x) %in% c(0, 1)), FALSE, 
+	weighted.gr <- ifelse(all(qgraph::mat2vec(x) %in% c(0, 1)), FALSE,
 						  TRUE)
-	
+
 	# pdf trick
 	net_qg <- qgraph(x, diag = FALSE, labels = colnames(x), DoNotPlot = TRUE, minimum = 0)
 	centr <- centrality(net_qg)
-	if (directed.gr & !weighted.gr) 
-		centr1 <- data.frame(cbind(Betweenness = centr$Betweenness, 
-								   Closeness = centr$Closeness, InDegree = centr$InDegree, 
+	if (directed.gr & !weighted.gr)
+		centr1 <- data.frame(cbind(Betweenness = centr$Betweenness,
+								   Closeness = centr$Closeness, InDegree = centr$InDegree,
 								   OutDegree = centr$OutDegree))
-	if (directed.gr & weighted.gr) 
-		centr1 <- data.frame(cbind(Betweenness = centr$Betweenness, 
-								   Closeness = centr$Closeness, InStrength = centr$InDegree, 
+	if (directed.gr & weighted.gr)
+		centr1 <- data.frame(cbind(Betweenness = centr$Betweenness,
+								   Closeness = centr$Closeness, InStrength = centr$InDegree,
 								   OutStrength = centr$OutDegree))
-	if (!directed.gr & !weighted.gr) 
-		centr1 <- data.frame(cbind(Betweenness = centr$Betweenness/2, 
+	if (!directed.gr & !weighted.gr)
+		centr1 <- data.frame(cbind(Betweenness = centr$Betweenness/2,
 								   Closeness = centr$Closeness, Degree = centr$OutDegree))
-	if (!directed.gr & weighted.gr) 
-		centr1 <- data.frame(cbind(Betweenness = centr$Betweenness/2, 
+	if (!directed.gr & weighted.gr)
+		centr1 <- data.frame(cbind(Betweenness = centr$Betweenness/2,
 								   Closeness = centr$Closeness, Strength = centr$OutDegree))
 	row.names(centr1) <- colnames(x)
 	log <- capture.output({
@@ -710,7 +747,7 @@ getTempFileName <- function() {
 	})
 	if (sum(largcomp) < ncol(x)) {
 		x2 <- x[largcomp, largcomp]
-		clos <- centrality(qgraph(x2, diag = FALSE, labels = colnames(x)[largcomp], 
+		clos <- centrality(qgraph(x2, diag = FALSE, labels = colnames(x)[largcomp],
 								  DoNotPlot = TRUE, minimum = 0))$Closeness
 		centr1$Closeness[largcomp] <- clos
 		centr1$Closeness[!largcomp] <- NA
