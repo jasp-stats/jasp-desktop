7f3c9d49285776abbaf4349b20749aa64e799040
akash raj <akashrajkn@gmail.com>
2017-09-05 16:09:25 +0200

    Revamped JASP Tests

--- JASP-Tests/core/main.cpp
+++ JASP-Tests/core/main.cpp
@@ -20,23 +20,16 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+	QApplication a(argc, argv);
+	int failures = AutomatedTests::run(argc, argv);
 
-    int failures = AutomatedTests::run(argc, argv);
+	qDebug() << "##########";
+	if (failures == 0) {
+		qDebug() << "ALL TESTS PASSED";
+	} else {
+		qDebug() << failures << " TESTS FAILED!";
+	}
+	qDebug() << "##########";
 
-    qDebug() << "--------------------------------------------------------------------------------------";
-
-    if (failures == 0)
-    {
-      qDebug() << "ALL TESTS PASSED";
-    }
-    else
-    {
-      qDebug() << failures << " TESTS FAILED!";
-    }
-
-    qDebug() << "--------------------------------------------------------------------------------------";
-
-    return 0;
+	return 0;
 }
-
