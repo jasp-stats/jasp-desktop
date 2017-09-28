ce735aeefdc3c88f61feaf3dfa043b6a23cf5826
bruno boutin <b.boutin@uva.nl>
2017-09-28 19:42:50 +0200

    Anova Repeated Measures Post Hoc factors error
    When setting a Post Hoc factor, hide & show the parameter panel removes
    the factor

--- JASP-Desktop/analysisforms/anovarepeatedmeasuresform.h
+++ JASP-Desktop/analysisforms/anovarepeatedmeasuresform.h
@@ -43,7 +43,7 @@ public:
 	
 private slots:
 	void factorsChanging();
-	void factorsChanged();
+	void factorsChanged(bool changed = true);
 	void termsChanged();
 	void withinSubjectsDesignChanged();
 
