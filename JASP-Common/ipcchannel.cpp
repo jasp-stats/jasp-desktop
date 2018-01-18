de4ff31d9719f7299fb831c6217e28ac3f306938
Joris Goosen <Joris@JorisGoosen.nl>
2018-01-18 16:19:31 +0100

    Postmerge fix
    Including named_semaphore twice is a bit much.

--- JASP-Common/ipcchannel.cpp
+++ JASP-Common/ipcchannel.cpp
@@ -20,7 +20,6 @@
 
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/nowide/convert.hpp>
-#include <boost/interprocess/sync/named_semaphore.hpp>
 
 using namespace std;
 using namespace boost;
