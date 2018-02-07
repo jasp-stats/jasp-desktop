584988df27216efacb1ad0e80b632f8034a67c30
Erik-Jan van Kesteren <erikjan.vankesteren@gmail.com>
2018-02-07 10:47:18 +0100

    Added unit tests for multinomial

---
+++ JASP-Tests/R/tests/testthat/test-multinomialtest.R
@@ -0,0 +1,40 @@
+context("Multinomial Test")
+
+test_that("Main table results match", {
+  options <- jasptools::analysisOptions("MultinomialTest")
+  options$VovkSellkeMPR <- TRUE
+  options$countProp <- "descProps"
+  options$descriptives <- TRUE
+  options$factor <- "facOutlier"
+  options$hypothesis <- "expectedProbs"
+  options$tableWidget <- list(
+    list(
+      levels = list("f1", "f2", "f3", "totallyridiculoussuperlongfactorme"),
+      name = "H1",
+      values = list(50, 42, 5, 3)
+    )
+  )
+  results <- jasptools::run("MultinomialTest",
+                            "test.csv", options, view=FALSE,
+                            quiet=TRUE)
+  maintable <- results[["results"]][["chisq"]][["data"]]
+  desctable <- results[["results"]][["descriptivesTable"]][["data"]]
+  expect_equal_tables(maintable,
+                      list("H1", 5.72, 3, 0.126056548007017, 1.40914224189199))
+  expect_equal_tables(desctable,
+                      list("f1", 0.49, 0.5,
+                           "f2", 0.49, 0.42,
+                           "f3", 0.01, 0.05,
+                           "totallyridiculoussuperlongfactorme", 0.01, 0.03))
+})
+
+test_that("Descriptives plot matches", {
+  options <- jasptools::analysisOptions("MultinomialTest")
+  options$factor <- "facFive"
+  options$descriptivesPlot <- TRUE
+  results <- jasptools::run("MultinomialTest", "test.csv", options,
+                            view=FALSE, quiet=TRUE)
+
+  testPlot <- results[["state"]][["figures"]][[1]]
+  expect_equal_plots(testPlot, "descriptives-1", dir="MultinomialTest")
+})
