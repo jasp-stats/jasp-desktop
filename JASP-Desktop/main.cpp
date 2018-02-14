0ce2ee6d3fb93c0292c8e76c6f94bbc97153b7df
Erik-Jan van Kesteren <erikjan.vankesteren@gmail.com>
2018-02-14 11:30:18 +0100

    support hi-dpi on windows

--- JASP-Desktop/main.cpp
+++ JASP-Desktop/main.cpp
@@ -27,6 +27,7 @@
 
 int main(int argc, char *argv[])
 {
+	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 	QCoreApplication::setOrganizationName("JASP");
 	QCoreApplication::setOrganizationDomain("jasp-stats.org");
 	QCoreApplication::setApplicationName("JASP");
