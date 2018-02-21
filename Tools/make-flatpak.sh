ae581eaee7d3f7ac826a61fd0f7e42aa98d53a77
Joris Goosen <Joris@JorisGoosen.nl>
2018-02-21 10:52:44 +0100

    Name the build-shellscripts consistenly

--- Tools/make-flatpak.sh
+++ Tools/make-flatpak.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 echo "Let's build ourselves a flatpak!"
 orginaldir=$PWD
-cp org.jasp.JASP.json ../../../org.jasp.JASP.json
-cd ../../..
+cp flatpak/org.jasp.JASP.json ../../org.jasp.JASP.json
+cd ../..
 flatpak-builder --gpg-sign=528338C233D5B3D8 --force-clean --ccache --repo=jasp-repo jasp-build org.jasp.JASP.json
 rm org.jasp.JASP.json
 cd $originaldir
