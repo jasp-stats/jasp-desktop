2d0ae4b130577c92586a4a130f6f6ee4c2f64190
kucharssim <kucharssim@gmail.com>
2018-03-13 22:57:39 +0100

    Created Unit Test for AnovaRepeatedMeasures

---
+++ JASP-Tests/R/tests/testthat/test-anovarepeatedmeasures.R
@@ -0,0 +1,273 @@
+context("Repeated Measures ANOVA")
+
+# Does not test:
+#    - type I and type II sum of squares
+#    - Simple effects
+#    - Plots
+#    - Contrasts apart from 'repeated'
+
+initOpts <- function(){
+  options <- jasptools::analysisOptions("anovarepeatedmeasures")
+  
+  options$repeatedMeasuresFactors <- list(
+    list(name = "Drink", levels = c("Beer", "Wine", "Water")),
+    list(name = "Imagery", levels = c("Positive", "Neutral", "Negative"))
+  )
+  
+  options$repeatedMeasuresCells <- c("beerpos", "beerneut", "beerneg",
+                                     "winepos", "wineneut", "wineneg",
+                                     "waterpos", "waterneu", "waterneg")
+  options$withinModelTerms <- list(
+    list(components = "Drink"),
+    list(components = "Imagery"), 
+    list(components = c("Drink", "Imagery"))
+  )
+  
+  options
+}
+
+test_that("Within subjects table results match", {
+  options <- initOpts()
+  options$sphericityCorrections <- TRUE
+
+  results <- jasptools::run(name = "AnovaRepeatedMeasures", dataset = "AnovaRepeatedMeasures.csv",
+                            options = options, view = FALSE, quiet = TRUE)
+  table <- list("Drink", "None", 2092.34444444444, 2, 1046.17222222222, 5.10598105687077,
+                0.0108629307294978, "TRUE", "FALSE", 1, 1, 1, 1, 1, "Drink",
+                "Greenhouse-Geisser", 2092.34444444444, 1.15422864073086, 1812.76427443316,
+                5.10598105687077, 0.0297686804863521, "FALSE", "FALSE", 1, 1,
+                1, 1, 1, "Drink", "Huynh-Feldt", 2092.34444444444, 1.18148845405137,
+                1770.93939197604, 5.10598105687077, 0.028813909529067, "FALSE",
+                "FALSE", 1, 1, 1, 1, 1, "Residual", "None", 7785.87777777778,
+                38, 204.891520467836, "", "", "", "", "", "TRUE", "Residual",
+                "Greenhouse-Geisser", 7785.87777777778, 21.9303441738863, 355.027614525488,
+                "", "", "", "", "", "FALSE", "Residual", "Huynh-Feldt", 7785.87777777778,
+                22.4482806269761, 346.836263638895, "", "", "", "", "", "FALSE",
+                "Imagery", "None", 21628.6777777778, 2, 10814.3388888889, 122.564824909945,
+                2.68019659683571e-17, "TRUE", "FALSE", 1, 1, 1, 1, 1, "Imagery",
+                "Greenhouse-Geisser", 21628.6777777778, 1.49488144635967, 14468.4903478118,
+                122.564824909945, 1.75728558571484e-13, "FALSE", "FALSE", 1,
+                1, 1, 1, 1, "Imagery", "Huynh-Feldt", 21628.6777777778, 1.59368408969683,
+                13571.4963320568, 122.564824909945, 3.14280380271786e-14, "FALSE",
+                "FALSE", 1, 1, 1, 1, 1, "Residual", "None", 3352.87777777778,
+                38, 88.2336257309941, "", "", "", "", "", "TRUE", "Residual",
+                "Greenhouse-Geisser", 3352.87777777778, 28.4027474808338, 118.047656482539,
+                "", "", "", "", "", "FALSE", "Residual", "Huynh-Feldt", 3352.87777777778,
+                30.2799977042398, 110.729129193702, "", "", "", "", "", "FALSE",
+                "Drink <unicode> Imagery", "None", 2624.42222222222, 4, 656.105555555556,
+                17.1549223629789, 4.58904028152479e-10, "TRUE", "FALSE", "Drink <unicode> Imagery",
+                "Greenhouse-Geisser", 2624.42222222222, 3.19359175963514, 821.777615847198,
+                17.1549223629789, 1.90024850184092e-08, "FALSE", "FALSE", "Drink <unicode> Imagery",
+                "Huynh-Feldt", 2624.42222222222, 3.91435133471376, 670.4615906467,
+                17.1549223629789, 6.80963952075043e-10, "FALSE", "FALSE", "Residual",
+                "None", 2906.68888888889, 76, 38.2459064327485, "", "", "",
+                "", "", "TRUE", "Residual", "Greenhouse-Geisser", 2906.68888888889,
+                60.6782434330676, 47.9033130234755, "", "", "", "", "", "FALSE",
+                "Residual", "Huynh-Feldt", 2906.68888888889, 74.3726753595615,
+                39.0827528367944, "", "", "", "", "", "FALSE")
+  
+  expect_equal_tables(results[["results"]][["withinSubjectsEffects"]][["data"]], table)
+})
+
+test_that("Sphericity Assumptions table match", {
+  options <- initOpts()
+  
+  options$sphericityTests <- TRUE
+  
+  results <- jasptools::run(name = "AnovaRepeatedMeasures", dataset = "AnovaRepeatedMeasures.csv",
+                            options = options, view = FALSE, quiet = TRUE)
+  
+  table <- list("Assumption Checks", "Test of Sphericity", "case", "string", "",
+                "W", "number", "sf:4;dp:3", "Mauchly's W", "p", "number", "dp:3;p:.001",
+                "GG", "number", "sf:4;dp:3", "Greenhouse-Geisser <unicode>",
+                "HF", "number", "sf:4;dp:3", "Huynh-Feldt <unicode>", "Drink",
+                0.267241056560857, 6.95230186958065e-06, 0.577114320365429,
+                0.590744227025686, "TRUE", "Imagery", 0.662101262364057, 0.0244523015633462,
+                0.747440723179836, 0.796842044848417, "FALSE", "Drink <unicode> Imagery",
+                0.595043993796251, 0.435658665786593, 0.798397939908785, 0.97858783367844,
+                "FALSE", "complete")
+  
+  expect_equal_tables(results[["results"]][["assumptionsObj"]], table)
+  
+})
+
+test_that("Post-hoc tests match", {
+  options <- initOpts()
+  
+  options$postHocTestsVariables <- c("Drink", "Imagery")
+  options$postHocTestEffectSize <- TRUE
+  options$postHocTestsBonferroni <- TRUE
+  options$postHocTestPooledError <- FALSE
+  
+  results <- jasptools::run(name = "AnovaRepeatedMeasures", dataset = "AnovaRepeatedMeasures.csv",
+                            options = options, view = FALSE, quiet = TRUE)
+  
+  table <- list("Post Hoc Comparisons - Drink", "postHoc_Drink", "<i>Note.</i>",
+                "Cohen's d does not correct for multiple comparisons.", "(I)",
+                "", "string", "TRUE", "(J)", "", "string", "Mean Difference",
+                "number", "sf:4;dp:3", "SE", "number", "sf:4;dp:3", "t", "number",
+                "sf:4;dp:3", "Cohen's d", "Cohen's d", "number", "sf:4;dp:3",
+                "bonferroni", "p<sub>bonf</sub>", "number", "dp:3;p:.001", "complete",
+                "Beer", "Wine", 3.5, 2.84948954082566, 1.22829017262715, 0.274654032208925,
+                "", "", 0.703009687611414, "", "TRUE", "Beer", "Water", 8.31666666666667,
+                3.3351289023547, 2.49365674016224, 0.557598598355329, "", "",
+                0.0660988675936689, "", "FALSE", "Wine", "Water", 4.81666666666667,
+                1.1164571680934, 4.31424223366509, 0.964693890587566, "", "",
+                0.00112213065327869, "", "FALSE", "Post Hoc Comparisons - Imagery",
+                "postHoc_Imagery", "<i>Note.</i>", "Cohen's d does not correct for multiple comparisons.",
+                "(I)", "", "string", "TRUE", "(J)", "", "string", "Mean Difference",
+                "number", "sf:4;dp:3", "SE", "number", "sf:4;dp:3", "t", "number",
+                "sf:4;dp:3", "Cohen's d", "Cohen's d", "number", "sf:4;dp:3",
+                "bonferroni", "p<sub>bonf</sub>", "number", "dp:3;p:.001", "complete",
+                "Positive", "Neutral", 13.2666666666667, 1.11255461788524, 11.9245082024684,
+                2.66640109389732, "", "", 8.64627761186188e-10, "", "TRUE",
+                "Positive", "Negative", 26.85, 1.91462133431161, 14.0236607201777,
+                3.13578586637111, "", "", 5.36180351283324e-11, "", "FALSE",
+                "Neutral", "Negative", 13.5833333333333, 1.97985098972637, 6.86078568731614,
+                1.53411831758965, "", "", 4.54655986206157e-06, "", "FALSE",
+                "Post Hoc Tests")
+  
+  expect_equal_tables(results[["results"]][["posthoc"]], table)
+})
+
+test_that("Descriptives Match", {
+  options <- initOpts()
+  
+  options$descriptives <- TRUE
+  
+  results <- jasptools::run(name = "AnovaRepeatedMeasures", dataset = "AnovaRepeatedMeasures.csv",
+                            options = options, view = FALSE, quiet = TRUE)
+  
+  table <- list("Descriptives", ".Drink", "string", "Drink", "TRUE", ".Imagery",
+                "string", "Imagery", "TRUE", "Mean", "number", "sf:4;dp:3",
+                "SD", "number", "sf:4;dp:3", "N", "number", "dp:0", "Beer",
+                "Positive", 20, 21.05, 13.0079934938807, "TRUE", "Beer", "Neutral",
+                20, 10, 10.295630140987, "FALSE", "Beer", "Negative", 20, 4.45,
+                17.3037111930543, "FALSE", "Wine", "Positive", 20, 25.35, 6.73775692801786,
+                "TRUE", "Wine", "Neutral", 20, 11.65, 6.24310145596511, "FALSE",
+                "Wine", "Negative", 20, -12, 6.18146635643918, "FALSE", "Water",
+                "Positive", 20, 17.4, 7.07404447704126, "TRUE", "Water", "Neutral",
+                20, 2.35, 6.83855170878193, "FALSE", "Water", "Negative", 20,
+                -9.2, 6.8024763292882, "FALSE", "complete")
+  
+  expect_equal_tables(results[["results"]][["descriptivesObj"]][["descriptivesTable"]], table)
+})
+
+
+# Mixed Effects
+initOpts <- function(){
+  options <- jasptools::analysisOptions("anovarepeatedmeasures")
+  
+  options$repeatedMeasuresFactors <- list(
+    list(name = "Looks", levels = c("Attractive", "Average", "Ugly")),
+    list(name = "Charisma", levels = c("High", "Some", "None"))
+  )
+  
+  options$repeatedMeasuresCells <- c("att_high", "att_some", "att_none",
+                                     "av_high", "av_some", "av_none",
+                                     "ug_high", "ug_some", "ug_none")
+  options$withinModelTerms <- list(
+    list(components = "Looks"),
+    list(components = "Charisma"), 
+    list(components = c("Looks", "Charisma"))
+  )
+  
+  options$betweenSubjectFactors <- "gender"
+  options$betweenModelTerms <- list(
+    list(components = "gender")
+  )
+  options
+}
+
+test_that("Between Subjects table match", {
+  options <- initOpts()
+  
+  results <- jasptools::run(name = "AnovaRepeatedMeasures",
+                            dataset = "AnovaMixedEffects.csv", options = options,
+                            view = FALSE, quiet = TRUE)
+  
+  table <- list("gender", 0.200000000000001, 1, 0.200000000000001, 0.00473545746857648,
+                0.945895847556855, "TRUE", "Residual", 760.222222222222, 18,
+                42.2345679012346, "", "", "", "", "", "TRUE")
+  
+  expect_equal_tables(results[["results"]][["betweenSubjectsEffects"]][["data"]],
+                      table)
+})
+
+test_that("Homogeneity tests correct", {
+  options <- initOpts()
+
+  options$homogeneityTests <- TRUE
+  
+  results <- jasptools::run(name = "AnovaRepeatedMeasures",
+                            dataset = "AnovaMixedEffects.csv", options = options,
+                            view = FALSE, quiet = TRUE)
+  
+  table <- list("att_high", 1.13105200239091, 1, 18, 0.301611198987337, "TRUE",
+                "att_some", 0.598562976996908, 1, 18, 0.449169168742317, "FALSE",
+                "att_none", 1.94893878806521, 1, 18, 0.179682774529315, "FALSE",
+                "av_high", 0.101977401129945, 1, 18, 0.753145830077659, "FALSE",
+                "av_some", 1.76314835904338, 1, 18, 0.200826123727507, "FALSE",
+                "av_none", 0.00399511707912524, 1, 18, 0.950298338730636, "FALSE",
+                "ug_high", 0.00491266375545877, 1, 18, 0.944894541517532, "FALSE",
+                "ug_some", 0.123626373626372, 1, 18, 0.729216564281406, "FALSE",
+                "ug_none", 0.0819838056680181, 1, 18, 0.777896246470082, "FALSE")
+  
+  expect_equal_tables(results[["results"]][["assumptionsObj"]][["levene"]][["data"]],
+                      table)
+})
+
+test_that("(Repeated) Contrast table match", {
+  options <- initOpts()
+  
+  options$contrasts <- list(list(contrast = "repeated", variable = "Looks"))
+  
+  results <- jasptools::run(name = "AnovaRepeatedMeasures",
+                            dataset = "AnovaMixedEffects.csv", options = options,
+                            view = FALSE, quiet = TRUE)
+  
+  table <- list("Repeated Contrast - Looks", "RepeatedContrast_Looks", "Comparison",
+                "string", "Estimate", "number", "sf:4;dp:3", "SE", "number",
+                "sf:4;dp:3", "t", "number", "sf:4;dp:3", "p", "number", "dp:3;p:.001",
+                "Attractive - Average", 13.5333333333333, 0.521959482105862,
+                25.9279384651327, 6.94207506292919e-25, "TRUE", "Average - Ugly",
+                12.75, 0.521959482105862, 24.4271834061903, 5.30699268885104e-24,
+                "FALSE", "complete")
+  
+  expect_equal_tables(results[["results"]][["contrasts"]][["collection"]],
+                      table)
+})
+
+
+test_that("Effect Size Calculation correct", {
+  options <- jasptools::analysisOptions("anovarepeatedmeasures")
+  
+  options$repeatedMeasuresFactors <- list(
+    list(name = "Animal", levels = c("Stick", "Kangaroo", "Fish", "Grub"))
+    )
+  options$repeatedMeasuresCells <- c("Stick Insect", "Kangaroo Testicle",
+                                     "Fish Eye", "Witchetty Grub")
+  
+  options$withinModelTerms <- list(
+    list(components = "Animal")
+  )
+  
+  options$effectSizeEstimates <- TRUE
+  options$effectSizeEtaSquared <- TRUE
+  options$effectSizeOmegaSquared <- TRUE
+  
+  results <- jasptools::run(name = "AnovaRepeatedMeasures",
+                            dataset = "AnovaRepeatedMeasuresOneWay.csv",
+                            options = options,
+                            view = FALSE, quiet = TRUE)
+  
+  table <- list("Animal", 83.1249999999999, 3, 27.7083333333333, 3.79380603096984,
+                0.0255702968630395, "TRUE", 1, 1, 1, 1, 1, 0.351479915433403,
+                0.351479915433403, 0.238785176929506, "Residual", 153.375, 21,
+                7.30357142857143, "", "", "", "", "", "TRUE")
+  
+  expect_equal_tables(results[["results"]][["withinSubjectsEffects"]][["data"]], 
+                      table)
+})
+
+
