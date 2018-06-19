fbbe9f84daa58041fb5f39f9e3daef958025f39d
akash raj <akashrajkn@gmail.com>
2018-06-19 16:44:05 +0200

    Fixed multinomial test case

--- JASP-Tests/R/tests/testthat/test-multinomialtest.R
+++ JASP-Tests/R/tests/testthat/test-multinomialtest.R
@@ -19,8 +19,15 @@ test_that("Main table results match", {
                             quiet=TRUE)
   maintable <- results[["results"]][["chisq"]][["data"]]
   desctable <- results[["results"]][["descriptivesTable"]][["data"]]
-  expect_equal_tables(maintable,
-                      list("H1", 5.72, 3, 0.126056548007017, 1.40914224189199))
+
+  expected <- jasptools:::collapseTable(
+      list(list(case = "H\u2080 (a)",
+                chisquare = 5.72,
+                df = 3,
+                p = 0.126056548007017,
+                VovkSellkeMPR = 1.40914224189199)))
+
+  expect_equal_tables(maintable, expected)
   expect_equal_tables(desctable,
                       list("f1", 0.49, 0.5,
                            "f2", 0.49, 0.42,
