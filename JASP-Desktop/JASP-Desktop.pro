527716de749a96c6965debb6053abfbff1cf01bd
FransMeerhoff <F.A.M.Meerhoff@uva.nl>
2018-09-19 12:28:45 +0200

    Update automatic build scripts

--- JASP-Desktop/JASP-Desktop.pro
+++ JASP-Desktop/JASP-Desktop.pro
@@ -13,7 +13,8 @@ DESTDIR = ..
 
 windows:TARGET = JASP
    macx:TARGET = JASP
-  linux: exists(/app/lib/*)	{ TARGET = org.jasp.JASP } else { TARGET = jasp }
+  linux:{ exists(/app/lib/*) {TARGET = org.jasp.JASP } else { TARGET = jasp }}
+
 
 TEMPLATE = app
 
--- Tools/make-clean-osx.sh
+++ Tools/make-clean-osx.sh
@@ -6,74 +6,63 @@ echo "First argument is the SIMPLECRYPTKEY to use! Maybe try -DOLLAR_SIGN-SIMPLE
 exit 1
 fi
 
-CRYPTKEY="$1"
+CRYPTKEY=$1
 
 . qt_dir_finder.sh
 
-STARTDIR="$PWD"
+STARTDIR=`pwd`
 echo "This script is running under the assumption it is based in the Tools folder of your local jasp-desktop clone, if this is not the case it is likely to complain.."
 cd ..
 if [ ! -d "Tools" ]
 then
-  echo "Are you running this script from Tools/?"
+  echo "Are you running this script from Tools ?"
   exit 1
 fi
 cd ..
 
-JASP_ROOT_DIR="$STARTDIR/../.."
-REQUIRED_FILES_DIR=jasp-required-files
+JASP_ROOT_DIR=$STARTDIR/../..
+JASP_REQUIRED_FILES_DIR=jasp-required-files
 JASP_BUILD_DIR=jasp-build
 JASP_GIT_DIR=jasp-desktop
 
-JASP_FULL_BUILD_DIR="$JASP_ROOT_DIR/$JASP_BUILD_DIR"
-JASP_FULL_GIT_DIR="$JASP_ROOT_DIR/$JASP_GIT_DIR"
+JASP_FULL_BUILD_DIR=$JASP_ROOT_DIR/$JASP_BUILD_DIR
+JASP_FULL_GIT_DIR=$JASP_ROOT_DIR/$JASP_GIT_DIR
 
-if [ ! -d "$JASP_GIT_DIR" ]
+if [ ! -d $JASP_GIT_DIR ]
 then
   echo "Missing folder with git clone of repository: $JASP_GIT_DIR\nExiting script"
-  exit 1	
+  exit 1
 fi
 
-if [ ! -d "$REQUIRED_FILES_DIR" ]
+if [ ! -d $JASP_REQUIRED_FILES_DIR ]
 then
-  echo "Missing folder with required binaries etc: $REQUIRED_FILES_DIR\nExiting script"
-  exit 1	
+  echo "Missing folder with required binaries etc: $JASP_REQUIRED_FILES_DIR\nExiting script"
+  exit 1
 fi
 
 echo "Cleaning up previous build folder ($JASP_BUILD_DIR) and creating it if necessary"
+rm -rf $JASP_BUILD_DIR
+mkdir $JASP_BUILD_DIR
 
-if [ ! -d "$JASP_BUILD_DIR" ]
-then
-  mkdir "$JASP_BUILD_DIR"
-fi
-
-cd "$JASP_BUILD_DIR"
-rm -rf *
-cd ..
-
-echo "Copying files from $REQUIRED_FILES_DIR to $JASP_BUILD_DIR"
-cp "$REQUIRED_FILES_DIR/*" "$JASP_BUILD_DIR/"
+echo "Copying files from $JASP_REQUIRED_FILES_DIR to $JASP_BUILD_DIR"
+cp $JASP_REQUIRED_FILES_DIR/* $JASP_BUILD_DIR/
 
 echo "Get the latest version of development from github!"
-cd "$JASP_GIT_DIR"
+cd $JASP_GIT_DIR
 git fetch origin
 git checkout development
 git pull
 
 echo "Running qmake!"
-
-"$QT_KIT_FULL/bin/qmake" -set ENVIRONMENT_CRYPTKEY \"$CRYPTKEY\" || exit 1
-"$QT_KIT_FULL/bin/qmake" -makefile -nocache -o "../$JASP_BUILD_DIR/Makefile" JASP.pro || exit 1
-"$QT_KIT_FULL/bin/qmake" -set ENVIRONMENT_CRYPTKEY \"\" || exit 1
+$QT_KIT_FULL/bin/qmake -set ENVIRONMENT_CRYPTKEY \$CRYPTKEY\ || exit 1
+$QT_KIT_FULL/bin/qmake -makefile -nocache -o ../$JASP_BUILD_DIR/Makefile JASP.pro || exit 1
+$QT_KIT_FULL/bin/qmake -set ENVIRONMENT_CRYPTKEY \"\" || exit 1
 
 echo "Compiling JASP!"
-cd "../$JASP_BUILD_DIR"
-
-make -j `sysctl -n hw.ncpu` || exit 1
-
+cd ../$JASP_BUILD_DIR
+make -j`sysctl -n hw.ncpu` || exit 1
 echo "Compiling finished succesfully!"
-cd "$STARTDIR"
 
 echo "Now making DMG"
+cd $STARTDIR
 . make-dmg.sh
-
