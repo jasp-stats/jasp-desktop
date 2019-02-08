2fcf6b8965209255d6027bf4900f4c557c0136e1
vandenman <donvdbergh@hotmail.com>
2019-02-08 14:05:03 +0100

    added override

--- JASP-R-Interface/jaspResults/src/jaspContainer.h
+++ JASP-R-Interface/jaspResults/src/jaspContainer.h
@@ -43,7 +43,7 @@ public:
 	void		checkDependenciesChildren(Json::Value currentOptions) override;
 
 	void		completeChildren();
-	void		setError(bool error);
+	void		setError(bool error) override;
 
 protected:
 	std::map<std::string, jaspObject*>	_data;
