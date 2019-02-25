8c37fdab538057fa0bf778406b30b4ff3c888cd9
Simon Kucharsky <kucharssim@gmail.com>
2019-02-25 12:13:31 +0100

    andyField logistic regression

--- JASP-Engine/JASP/R/commonglm.R
+++ JASP-Engine/JASP/R/commonglm.R
@@ -155,7 +155,8 @@
       list(name="pvl", title="p", type="number", format="dp:3;p:.001"),
       list(name="fad", title="McFadden R\u00B2", type="number", format="sf:4;dp:3"),
       list(name="nag", title="Nagelkerke R\u00B2", type="number", format="sf:4;dp:3"),
-      list(name="tju", title="Tjur R\u00B2", type="number", format="sf:4;dp:3")
+      list(name="tju", title="Tjur R\u00B2", type="number", format="sf:4;dp:3"),
+      list(name="cas", title="Cox & Snell R\u00B2", type="number", format="sf:4;dp:3")
     )
 
     if (options[["method"]] != "enter") {
@@ -192,7 +193,8 @@
                pvl = .clean(NULL),
                fad = .clean(NULL),
                nag = .clean(NULL),
-               tju = .clean(NULL)),
+               tju = .clean(NULL),
+          		 cas = .clean(NULL)),
           list(mod = "H\u2081",
                dev = .clean(glmObj[[2]][["deviance"]]),
                aic = .clean(glmObj[[2]][["aic"]]),
@@ -202,7 +204,8 @@
                pvl = .clean(lr[["pval"]]),
                fad = .clean(.mcFadden(glmObj[[2]], glmObj[[1]])),
                nag = .clean(.nagelkerke(glmObj[[2]], glmObj[[1]])),
-               tju = .clean(.tjur(glmObj[[2]])))
+               tju = .clean(.tjur(glmObj[[2]])),
+          		 cas = .clean(.coxSnell(glmObj[[2]], glmObj[[1]])))
         )
 
       } else {
@@ -216,9 +219,12 @@
                 options[["method"]] == "stepwise") {
               fadden <- .mcFadden(mObj, glmObj[[1]])
               nagel <- .nagelkerke(mObj, glmObj[[1]])
+              coxSn <- .coxSnell(mObj, glmObj[[1]])
+              
             } else {
               fadden <- -1*.mcFadden(glmObj[[1]], mObj)
               nagel <- -1*.nagelkerke(glmObj[[1]], mObj)
+              coxSn <- -1*.coxSnell(glmObj[[1]], mObj)
             }
 
             lr <- .lrtest(glmObj[[midx]], glmObj[[midx-1]])
@@ -232,7 +238,8 @@
               pvl = .clean(lr[["pval"]]),
               fad = .clean(fadden),
               nag = .clean(nagel),
-              tju = .clean(.tjur(mObj))
+              tju = .clean(.tjur(mObj)),
+              cas = .clean(coxSn)
             )
           } else {
             rows[[midx]] <- list(
@@ -245,7 +252,8 @@
               pvl = .clean(NULL),
               fad = .clean(.mcFadden(mObj, mObj)),
               nag = .clean(.nagelkerke(mObj, mObj)),
-              tju = .clean(.tjur(mObj))
+              tju = .clean(.tjur(mObj)),
+              cas = .clean(.coxSnell(mObj, mObj))
             )
           }
         }
@@ -253,8 +261,8 @@
     } else {
       rows <- list(
         list(mod = "H\u2080", dev = ".", fad = .clean(NULL), nag = .clean(NULL),
-             tju = .clean(NULL), aic = "."),
-        list(mod = "H\u2081", dev = ".", fad = ".", nag = ".", tju = ".", aic = ".")
+             tju = .clean(NULL), cas = .clean(NULL), aic = "."),
+        list(mod = "H\u2081", dev = ".", fad = ".", nag = ".", tju = ".", cas = ".", aic = ".")
       )
     }
 
@@ -271,9 +279,9 @@
     out[["title"]] <- "Coefficients"
 
     if (options[["coeffCIOR"]]) {
-      ciTitle <- paste0(options[["coeffCIInterval"]], "% Confidence interval <br> (odds ratio scale)")
+      ciTitle <- paste0(options[["coeffCIInterval"]]*100, "% Confidence interval <br> (odds ratio scale)")
     } else {
-      ciTitle <- paste0(options[["coeffCIInterval"]], "% Confidence interval")
+      ciTitle <- paste0(options[["coeffCIInterval"]]*100, "% Confidence interval")
     }
 
     if (options[["robustSEOpt"]]) {
@@ -298,7 +306,9 @@
       list(name="std", title = "Standardized\u207A", type="number", format="dp:3"),
       list(name="or", title = "Odds Ratio", type="number", format="sf:4;dp:3"),
       list(name="zval", title = "z", type="number", format="sf:4;dp:3"),
-      list(name="pval", title = "p", type="number", format="dp:3;p:.001"),
+      list(name="waldsta", title = "Wald Statistic", type="number", format="sf:4;dp:3", overTitle = "Wald Test"),
+      list(name="walddf", title = "df", type="integer", overTitle = "Wald Test"),
+      list(name="pval", title = "p", type="number", format="dp:3;p:.001", overTitle = "Wald Test"),
       list(name="vsmpr", title = "VS-MPR\u002A", type="number", format="sf:4;dp:3"),
       list(name="cilo", title = "Lower bound", type="number", format="dp:3", overTitle=ciTitle),
       list(name="ciup", title = "Upper bound", type="number", format="dp:3", overTitle=ciTitle)
@@ -307,7 +317,8 @@
 
     # then determine which ones we need
     selectFields <- with(options, c(multimod, TRUE, TRUE, TRUE, stdCoeff,
-                                    oddsRatios, TRUE, TRUE, VovkSellkeMPR,
+                                    oddsRatios, TRUE, TRUE, TRUE, TRUE,
+    																VovkSellkeMPR,
                                     coeffCI, coeffCI))
 
     out[["schema"]] <- list(fields=fields[selectFields])
@@ -345,7 +356,9 @@
         s <- summary(glmObj[[2]])[["coefficients"]]
         rn <- rownames(s)
         rn[which(rn == "(Intercept)")] <- .v("(Intercept)")
-        c <- qnorm(1 - (100 - options[["coeffCIInterval"]]) / 200)
+        # `c` should be renamed to something better
+        # c <- qnorm(1 - (100 - options[["coeffCIInterval"]]) / 200)
+        c <- qnorm(1 - (1 - options[['coeffCIInterval']])/2)
         beta <- .stdEst(glmObj[[2]], type = "X") # stand. X continuous vars
 
         # Confidence intervals on the odds ratio scale
@@ -363,6 +376,7 @@
             s[3] <- s[1]/s[2] # new z
             s[4] <- 2*pnorm(-abs(s[3])) # new p
           }
+          waldtest <- mdscore::wald.test(glmObj[[2]], term = 1)
           rows[[1]] <- list(param = .clean(.formatTerm(rn, glmObj[[2]])),
                             est = .clean(s[1]),
                             se = .clean(s[2]),
@@ -370,6 +384,8 @@
                             or = .clean(exp(s[1])),
                             zval = .clean(s[3]),
                             pval = .clean(s[4]),
+          									waldsta = .clean(as.numeric(waldtest$W)),
+          									walddf = .clean(as.numeric(1)),
                             vsmpr = .clean(.VovkSellkeMPR(s[4])),
                             cilo = .clean(expon(s[1] - c * s[2])),
                             ciup = .clean(expon(s[1] + c * s[2])))
@@ -381,6 +397,8 @@
           }
           for (i in seq_along(rn)) {
 
+          	waldtest <- mdscore::wald.test(glmObj[[2]], term = i)
+          	
             rows[[i]] <- list(param = .clean(.formatTerm(rn[i], glmObj[[2]])),
                               est = .clean(s[i,1]),
                               se = .clean(s[i,2]),
@@ -388,6 +406,8 @@
                               or = .clean(exp(s[i,1])),
                               zval = .clean(s[i,3]),
                               pval = .clean(s[i,4]),
+            									waldsta = .clean(as.numeric(waldtest$W)),
+            									walddf = .clean(as.numeric(1)),
                               vsmpr = .clean(.VovkSellkeMPR(s[i,4])),
                               cilo = .clean(expon(s[i,1] - c * s[i,2])),
                               ciup = .clean(expon(s[i,1] + c * s[i,2])))
@@ -400,7 +420,9 @@
           s <- summary(mObj)[["coefficients"]]
           rn <- rownames(s)
           rn[which(rn == "(Intercept)")] <- .v("(Intercept)")
-          c <- qnorm(1 - (100 - options[["coeffCIInterval"]]) / 200)
+          # `c` should be renamed to something better
+          # c <- qnorm(1 - (100 - options[["coeffCIInterval"]]) / 200)
+          c <- qnorm(1 - (1-options[['coeffCIInterval']])/2)
           beta <- .stdEst(mObj, type = "X") # stand. X continuous vars
 
           # Confidence intervals on the odds ratio scale
@@ -418,6 +440,8 @@
               s[3] <- s[1]/s[2] # new z
               s[4] <- 2*pnorm(-abs(s[3])) # new p
             }
+            waldtest <- mdscore::wald.test(mObj, term = 1)
+            
             rows[[length(rows)+1]] <- list(
               model = as.character(midx),
               param = .clean(.formatTerm(rn, mObj)),
@@ -427,6 +451,8 @@
               or = .clean(exp(s[1])),
               zval = .clean(s[3]),
               pval = .clean(s[4]),
+              waldsta = .clean(as.numeric(waldtest$W)),
+              walddf = .clean(as.numeric(1)),
               vsmpr = .clean(.VovkSellkeMPR(s[4])),
               cilo = .clean(expon(s[1] - c * s[2])),
               ciup = .clean(expon(s[1] + c * s[2])),
@@ -439,6 +465,9 @@
               s[,4] <- 2*pnorm(-abs(s[,3])) # new p
             }
             for (i in seq_along(rn)) {
+            	
+            	waldtest <- mdscore::wald.test(mObj, term = i)
+            	
               row <- list(
                 model = as.character(midx),
                 param = .clean(.formatTerm(rn[i], mObj)),
@@ -448,6 +477,8 @@
                 or = .clean(exp(s[i,1])),
                 zval = .clean(s[i,3]),
                 pval = .clean(s[i,4]),
+                waldsta = .clean(as.numeric(waldtest$W)),
+                walddf = .clean(as.numeric(1)),
                 vsmpr = .clean(.VovkSellkeMPR(s[i,4])),
                 cilo = .clean(expon(s[i,1] - c * s[i,2])),
                 ciup = .clean(expon(s[i,1] + c * s[i,2]))
@@ -468,7 +499,8 @@
     } else {
       rows <- list(
         list(param = ".", est = ".", se = ".", std = ".", or = ".",
-             zval = ".", pval = ".", vsmpr = ".", cilo = ".", ciup = ".")
+             zval = ".", pval = ".", waldsta = ".", walddf = ".", 
+        		 vsmpr = ".", cilo = ".", ciup = ".")
       )
     }
 
@@ -914,6 +946,21 @@
   ys <- glmModel[["y"]]
   return(abs(mean(ps[ys])-mean(ps[-ys])))
 }
+
+.coxSnell <- function(glmModel, nullModel) {
+	rightSide <- deparse(glmModel[["formula"]][[3]])
+	if (length(rightSide == 1) && rightSide %in% c("1", "0")) {
+		# intercept-only model needs fix because of computer precision limits
+		return(NULL)
+	} else {
+		l0 <- -0.5*nullModel[["deviance"]]
+		lm <- as.numeric(logLik(glmModel))
+		n <- length(glmModel[["y"]])
+		coxSnell <- 1 - exp(l0 - lm)^(2 / n)
+		return(max(c(0,coxSnell)))
+	}
+}
+
 
 .bic <- function(glmModel) {
   return(log(length(glmModel[["y"]]))*length(coef(glmModel))+glmModel[["deviance"]])
---
+++ JASP-Tests/R/tests/testthat/test-regressionlogistic.R
@@ -0,0 +1,164 @@
+context("Logistic Regression")
+
+# Below are the unit tests for Andy Field's book
+
+# Chapter 10
+test_that("Fields Book - Chapter 10 results match", {
+  options <- jasptools::analysisOptions("RegressionLogistic")
+  options$dependent <- "delivered"
+  options$factors <- c("treat")
+  options$modelTerms <- list(
+    list(components="treat", isNuisance=FALSE)
+  )
+  options$factorDescriptivesOpt <- TRUE
+  options$oddsRatios <- TRUE
+  options$coeffCI <- TRUE
+  options$coeffCIOR <- TRUE
+  results <- jasptools::run("RegressionLogistic", dataset = "santas_log.csv", options, view=FALSE, quiet=TRUE)
+  output1 <- results[["results"]][["factorDescriptives"]][["data"]]
+  expect_equal_tables(output1,
+                      list(0, 178, "TRUE",
+                           1, 222, "FALSE")
+  )
+  output2 <- results[["results"]][["modelSummary"]][["data"]]
+  expect_equal_tables(output2,
+  										list("H<unicode>", 529.250590526386, 531.250590526386, 535.242055073494,
+  												 399, "", "", "", "", "", "", 
+  												 "H<unicode>", 460.494488544053,
+  												 464.494488544053, 472.477417638269, 398, 68.7561019823323, 1.11022302462516e-16,
+  												 0.129912187559297, 0.215249724800182, 0.174987016994499, 0.157928418719378)
+  )
+  output3 <- results[["results"]][["estimatesTable"]][["data"]]
+  expect_equal_tables(output3,
+                      list("(Intercept)", 1.678431, 0.2058631, 1.678431, 5.357143, 8.15314, 3.54595e-16, 66.47369, 1, 2.916226e+13, 3.57851, 8.019813,
+                           "treat (1)", -1.877282, 0.2461223, -1.877282, 0.1530055, -7.627436, 2.394692e-14, 58.17778, 1, 4.89823e+11, 0.09445116, 0.2478601)
+  )
+  options <- jasptools::analysisOptions("RegressionLogistic")
+  options$dependent <- "delivered"
+  options$factors <- c("treat")
+  options$covariates <- c("quantity")
+  options$modelTerms <- list(
+    list(components="treat", isNuisance=FALSE),
+    list(components="quantity", isNuisance=FALSE),
+    list(components=list("treat", "quantity"), isNuisance=FALSE)
+  )
+  options$oddsRatios <- TRUE
+  options$coeffCI <- TRUE
+  options$coeffCIOR <- TRUE
+  results <- jasptools::run("RegressionLogistic", dataset = "santas_log.csv", options, view=FALSE, quiet=TRUE)
+  output4 <- results[["results"]][["modelSummary"]][["data"]]
+  expect_equal_tables(output4,
+  										list("H<unicode>", 529.250590526386, 531.250590526386, 535.242055073494,
+  												 399, "", "", "", "", "", "", 
+  												 "H<unicode>", 390.185034282251,
+  												 398.185034282251, 414.150892470683, 396, 139.065556244135, 0,
+  												 0.262759378512591, 0.400251224473979, 0.543597009594121, 0.293663757434995)
+  )
+  output5 <- results[["results"]][["estimatesTable"]][["data"]]
+  expect_equal_tables(output5,
+                      list("(Intercept)", 1.829715, 0.3810035, 1.671349, 6.232108, 4.802356, 1.568094e-06, 23.06263, 1, 17552.68, 2.953413, 13.1506,
+                           "treat (1)", 0.199482, 0.520007, -0.09976397, 1.22077, 0.3836141, 0.7012645, 0.1471597, 1, 1, 0.4405581, 3.382709,
+                           "quantity", -0.08100546, 0.1678705, -1.809561, 0.9221887, -0.4825472, 0.6294173, 0.2328518, 1, 1, 0.6636332, 1.281479,
+                           "treat (1) * quantity", -1.027643, 0.2309776, -1.265616, 0.3578493, -4.449103, 8.622966e-06, 19.79452, 1, 3658.559, 0.2275578, 0.5627412)
+  )
+  
+  options <- jasptools::analysisOptions("RegressionLogistic")
+  options$dependent <- "delivered"
+  options$covariates <- c("quantity")
+  options$modelTerms <- list(
+    list(components="quantity", isNuisance=FALSE)
+  )
+  options$oddsRatios <- TRUE
+  options$coeffCI <- TRUE
+  options$coeffCIOR <- TRUE
+  options$estimatesPlotsOpt <- TRUE
+  options$showPoints <- FALSE
+  results <- jasptools::run("RegressionLogistic", dataset = "santas_log_subset_treat0.csv", options, view=FALSE, quiet=TRUE)
+  output6 <- results[["results"]][["estimatesTable"]][["data"]]
+  expect_equal_tables(output6,
+                      list("(Intercept)", 1.829715, 0.3810035, 1.681811, 6.232108, 4.802356, 1.568094e-06, 23.06263, 1, 17552.68, 2.953413, 13.1506,
+                           "quantity", -0.08100546, 0.1678705, -0.09960282, 0.9221887, -0.4825472, 0.6294173, 0.2328518, 1, 1, 0.6636332, 1.281479)
+  )
+  unnumberedFigureA <- results[["state"]][["estimatesPlots"]][["collection"]][[1]]
+  #expect_equal_plots(unnumberedFigureA, "?", dir="Ancova") # This command needs to be updated
+  
+  options <- jasptools::analysisOptions("RegressionLogistic")
+  options$dependent <- "delivered"
+  options$covariates <- c("quantity")
+  options$modelTerms <- list(
+    list(components="quantity", isNuisance=FALSE)
+  )
+  options$oddsRatios <- TRUE
+  options$coeffCI <- TRUE
+  options$coeffCIOR <- TRUE
+  options$estimatesPlotsOpt <- TRUE
+  options$showPoints <- FALSE
+  results <- jasptools::run("RegressionLogistic", dataset = "santas_log_subset_treat1.csv", options, view=FALSE, quiet=TRUE)
+  output7 <- results[["results"]][["estimatesTable"]][["data"]]
+  expect_equal_tables(output7,
+                      list("(Intercept)", 2.029197, 0.3538977, -0.2530217, 7.607972, 5.73385, 9.817603e-09, 32.87704, 1, 2032173, 3.802162, 15.22324,
+                           "quantity", -1.108649, 0.158651, -1.359324, 0.3300046, -6.987972, 2.788889e-12, 48.83175, 1, 4957980522, 0.241811, 0.4503643)
+  )
+  unnumberedFigureB <- results[["state"]][["estimatesPlots"]][["collection"]][[1]]
+  #expect_equal_plots(unnumberedFigureB, "?", dir="Ancova") # This command needs to be updated
+  
+  options <- jasptools::analysisOptions("RegressionLogistic")
+  options$dependent <- "delivered"
+  options$factors <- c("treat")
+  options$covariates <- c("quantity")
+  options$modelTerms <- list(
+    list(components="treat", isNuisance=FALSE),
+    list(components="quantity", isNuisance=FALSE),
+    list(components=list("treat", "quantity"), isNuisance=FALSE)
+  )
+  options$casewiseDiagnostics <- TRUE
+  options$casewiseDiagnosticsResidualZ <- 2
+  options$coeffEstimatesBootstrapping <- TRUE
+  options$coeffEstimatesBootstrappingReplicates <- 1000
+  set.seed(1) # For Bootstrapping Unit Tests
+  results <- jasptools::run("RegressionLogistic", dataset = "santas_log.csv", options, view=FALSE, quiet=TRUE)
+  output9 <- results[["results"]][["casewiseDiagnosticsTable"]][["data"]]
+  expect_equal_tables(output9,
+                      list(18, 0, 0.8517899, 1, -0.8517899, -2.397327, 0.01212395,
+                           40, 0, 0.8412694, 1, -0.8412694, -2.302169, 0.007636277,
+                           51, 0, 0.8617277, 1, -0.8617277, -2.496419, 0.02790573,
+                           64, 0, 0.8517899, 1, -0.8517899, -2.397327, 0.01212395,
+                           78, 0, 0.8301511, 1, -0.8301511, -2.210788, 0.01337234,
+                           90, 0, 0.8412694, 1, -0.8412694, -2.302169, 0.007636277,
+                           93, 1, 0.08276198, 0, 0.917238, 3.32909, 0.02726264,
+                           96, 0, 0.8301511, 1, -0.8301511, -2.210788, 0.01337234,
+                           98, 0, 0.818422, 1, -0.818422, -2.123034, 0.02915053,
+                           105, 0, 0.8617277, 1, -0.8617277, -2.496419, 0.02790573,
+                           111, 0, 0.8617277, 1, -0.8617277, -2.496419, 0.02790573,
+                           112, 0, 0.8412694, 1, -0.8412694, -2.302169, 0.007636277,
+                           113, 0, 0.8517899, 1, -0.8517899, -2.397327, 0.01212395,
+                           138, 0, 0.8517899, 1, -0.8517899, -2.397327, 0.01212395,
+                           162, 0, 0.818422, 1, -0.818422, -2.123034, 0.02915053,
+                           170, 0, 0.8412694, 1, -0.8412694, -2.302169, 0.007636277,
+                           188, 1, 0.08276198, 0, 0.917238, 3.32909, 0.02726264,
+                           195, 0, 0.8517899, 1, -0.8517899, -2.397327, 0.01212395,
+                           214, 0, 0.8838286, 1, -0.8838286, -2.758255, 0.02510005,
+                           215, 0, 0.8301511, 1, -0.8301511, -2.210788, 0.01337234,
+                           219, 0, 0.8517899, 1, -0.8517899, -2.397327, 0.01212395,
+                           222, 0, 0.818422, 1, -0.818422, -2.123034, 0.02915053,
+                           249, 0, 0.8412694, 1, -0.8412694, -2.302169, 0.007636277,
+                           258, 0, 0.8301511, 1, -0.8301511, -2.210788, 0.01337234,
+                           265, 1, 0.08276198, 0, 0.917238, 3.32909, 0.02726264,
+                           270, 0, 0.818422, 1, -0.818422, -2.123034, 0.02915053,
+                           285, 0, 0.8301511, 1, -0.8301511, -2.210788, 0.01337234,
+                           288, 0, 0.8517899, 1, -0.8517899, -2.397327, 0.01212395,
+                           301, 0, 0.8517899, 1, -0.8517899, -2.397327, 0.01212395,
+                           307, 0, 0.8838286, 1, -0.8838286, -2.758255, 0.02510005,
+                           335, 0, 0.8517899, 1, -0.8517899, -2.397327, 0.01212395,
+                           370, 0, 0.8517899, 1, -0.8517899, -2.397327, 0.01212395,
+                           374, 0, 0.8301511, 1, -0.8301511, -2.210788, 0.01337234,
+                           382, 0, 0.8838286, 1, -0.8838286, -2.758255, 0.02510005)
+  )
+  output10 <- results[["results"]][["estimatesTableBootstrapping"]][["data"]]
+  expect_equal_tables(output10,
+                      list(2, "(Intercept)", 1.829715, 0.04991356, 0.408753, 1.109881, 2.705956, "TRUE",
+                           2, "treat (1)", 0.199482, -0.006766021, 0.5457141, -1.015603, 1.165043, "FALSE",
+                           2, "quantity", -0.08100546, -0.01073049, 0.176387, -0.4214278, 0.2660843, "FALSE",
+                           2, "treat (1) * quantity", -1.027643, -0.01150906, 0.2457343, -1.504704, -0.524605, "FALSE")
+  )
+})
