364f12346a090a099edccb769ade3e2a36b04004
Joris Goosen <Joris@JorisGoosen.nl>
2019-03-20 12:55:55 +0100

    Some update to uploadscripts

--- Tools/upload-dmg.sh
+++ Tools/upload-dmg.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #Uploads the JASP.dmg to our server with the name: JASP-nightly-*branch*-*commit*, repository isn't mentioned because i figured the name was long enough already.
-scp ../../jasp-build/JASP.dmg nachtjapon@static.jasp-stats.org:~/Nightlies/MacOS/JASP-nightly-`git rev-parse --abbrev-ref HEAD`-`git rev-parse --verify HEAD`.dmg
+DEST_FILE=JASP-nightly-`git rev-parse --abbrev-ref HEAD`-`git rev-parse --verify HEAD`.dmg
+echo Copying JASP.dmg to jasp-static with name $DEST_FILE
+scp ../../jasp-build/JASP.dmg nachtjapon@static.jasp-stats.org:~/Nightlies/MacOS/$DEST_FILE
--- Tools/upload-msi.cmd
+++ Tools/upload-msi.cmd
@@ -4,7 +4,7 @@ SETLOCAL EnableDelayedExpansion
 
 SET ARCH=%1
 
-REM ---------------------- Check arguments -------------------------------
+REM - Check arguments -
 if NOT "%1"=="32" (
 if NOT "%1"=="64" (
 echo "First argument could be architecture, i.e 32 or 64 bits, to upload"
@@ -12,7 +12,10 @@ echo "But for now the default of 64 will be assumed."
 SET ARCH=64
 ))
 
-FOR /F "tokens=*" %g IN ('git rev-parse --abbrev-ref HEAD') do (SET GIT_BRANCH=%g)
-FOR /F "tokens=*" %g IN ('git rev-parse --verify HEAD')     do (SET GIT_COMMIT=%g)
+FOR /F "tokens=*" %%g IN ('git rev-parse --abbrev-ref HEAD') do (SET GIT_BRANCH=%%g)
+FOR /F "tokens=*" %%h IN ('git rev-parse --verify HEAD')     do (SET GIT_COMMIT=%%h)
 
-scp ../../jasp-wix-installer-%ARCH%/JASP.dmg nachtjapon@static.jasp-stats.org:~/Nightlies/Windows/JASP-nightly-%GIT_BRANCH%-%GIT_COMMIT%.msi
+SET DEST_FILE=JASP-nightly-%GIT_BRANCH%-%GIT_COMMIT%.msi
+echo "Copying MSI to jasp-static with filename : %DEST_FILE%"
+scp ..\..\jasp-wix-installer-%ARCH%\JASP.msi nachtjapon@static.jasp-stats.org:~/Nightlies/Windows/%DEST_FILE%
+endlocal
