0cf82391089adad338097bded3bac65e52c9a042
Joris Goosen <Joris@JorisGoosen.nl>
2019-03-25 10:22:48 +0100

    NA in R should become empty string in jaspTable not NaN because otherwise you cannot add empty cells easily.. Fixes issue  https://github.com/jasp-stats/INTERNAL-jasp/issues/155

--- JASP-R-Interface/jaspResults/src/jaspJson.h
+++ JASP-R-Interface/jaspResults/src/jaspJson.h
@@ -109,23 +109,24 @@ protected:
 	Json::Value _json;
 };
 
-template<> inline Json::Value jaspJson::RVectorEntry_to_JsonValue<INTSXP>(Rcpp::Vector<INTSXP> obj, int row)					{ return (int)(obj[row]) == NA_INTEGER	? "NaN" : Json::Value((int)(obj[row]));			}
-template<> inline Json::Value jaspJson::RMatrixColumnEntry_to_JsonValue<INTSXP>(Rcpp::MatrixColumn<INTSXP> obj, int row)		{ return (int)(obj[row]) == NA_INTEGER	? "NaN" : Json::Value((int)(obj[row]));			}
+template<> inline Json::Value jaspJson::RVectorEntry_to_JsonValue<INTSXP>(Rcpp::Vector<INTSXP> obj, int row)					{ return (int)(obj[row]) == NA_INTEGER	? "" : Json::Value((int)(obj[row]));			}
+template<> inline Json::Value jaspJson::RMatrixColumnEntry_to_JsonValue<INTSXP>(Rcpp::MatrixColumn<INTSXP> obj, int row)		{ return (int)(obj[row]) == NA_INTEGER	? "" : Json::Value((int)(obj[row]));			}
 
-template<> inline Json::Value jaspJson::RVectorEntry_to_JsonValue<LGLSXP>(Rcpp::Vector<LGLSXP> obj, int row)					{ return (bool)(obj[row]) == NA_LOGICAL	? "NaN" : Json::Value((bool)(obj[row]));			}
-template<> inline Json::Value jaspJson::RMatrixColumnEntry_to_JsonValue<LGLSXP>(Rcpp::MatrixColumn<LGLSXP> obj, int row)		{ return (bool)(obj[row]) == NA_LOGICAL	? "NaN" : Json::Value((bool)(obj[row]));			}
+template<> inline Json::Value jaspJson::RVectorEntry_to_JsonValue<LGLSXP>(Rcpp::Vector<LGLSXP> obj, int row)					{ return (bool)(obj[row]) == NA_LOGICAL	? "" : Json::Value((bool)(obj[row]));			}
+template<> inline Json::Value jaspJson::RMatrixColumnEntry_to_JsonValue<LGLSXP>(Rcpp::MatrixColumn<LGLSXP> obj, int row)		{ return (bool)(obj[row]) == NA_LOGICAL	? "" : Json::Value((bool)(obj[row]));			}
 
-template<> inline Json::Value jaspJson::RVectorEntry_to_JsonValue<STRSXP>(Rcpp::Vector<STRSXP> obj, int row)					{ return obj[row] == NA_STRING			? "NaN" : Json::Value((std::string)(obj[row]));	}
-template<> inline Json::Value jaspJson::RMatrixColumnEntry_to_JsonValue<STRSXP>(Rcpp::MatrixColumn<STRSXP> obj, int row)		{ return obj[row] == NA_STRING			? "NaN" : Json::Value((std::string)(obj[row]));	}
+template<> inline Json::Value jaspJson::RVectorEntry_to_JsonValue<STRSXP>(Rcpp::Vector<STRSXP> obj, int row)					{ return obj[row] == NA_STRING			? "" : Json::Value((std::string)(obj[row]));	}
+template<> inline Json::Value jaspJson::RMatrixColumnEntry_to_JsonValue<STRSXP>(Rcpp::MatrixColumn<STRSXP> obj, int row)		{ return obj[row] == NA_STRING			? "" : Json::Value((std::string)(obj[row]));	}
 
 
 #define TO_INFINITY_AND_BEYOND																					\
 {																												\
 	double val = static_cast<double>(obj[row]);																	\
-	return	R_IsNA(val) || R_IsNaN(val) ? "NaN" :																\
-				val == std::numeric_limits<double>::infinity() ? "\u221E" :										\
-					val == -1 * std::numeric_limits<double>::infinity() ? "-\u221E"  :							\
-						Json::Value((double)(obj[row]));														\
+	return	R_IsNA(val) ? "" :																					\
+				R_IsNaN(val) ? "NaN" :																			\
+					val == std::numeric_limits<double>::infinity() ? "\u221E" :									\
+						val == -1 * std::numeric_limits<double>::infinity() ? "-\u221E"  :						\
+							Json::Value((double)(obj[row]));													\
 }
 
 template<> inline Json::Value jaspJson::RVectorEntry_to_JsonValue<REALSXP>(Rcpp::Vector<REALSXP> obj, int row)				TO_INFINITY_AND_BEYOND
