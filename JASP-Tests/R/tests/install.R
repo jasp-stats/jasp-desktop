632d76c0f18dc4c69dd84c490c01fab1bd3459e6
TimKDJ <tim_jong@hotmail.com>
2019-04-04 13:58:07 +0200

    force pkg installs from binaries in unit tests
    This reduces the chance of the unit tests breaking when package dependencies are updated

--- JASP-Tests/R/tests/install.R
+++ JASP-Tests/R/tests/install.R
@@ -3,17 +3,18 @@
 repos <- getOption("repos")
 repos["CRAN"] <- "https://cloud.r-project.org"
 options(repos = repos)
+options(pkgType="binary")
 
 install.packages("devtools")
 
 jaspResultsPath <- file.path("..", "..", "..", "JASP-R-Interface", "jaspResults")
-devtools::install_deps(jaspResultsPath, upgrade="never")
+devtools::install_deps(jaspResultsPath, upgrade=FALSE)
 install.packages(jaspResultsPath, type="source", repos=NULL)
 
 jasptoolsPath <- file.path("..", "..", "..", "Tools", "jasptools")
-devtools::install_deps(jasptoolsPath, upgrade="never")
+devtools::install_deps(jasptoolsPath, upgrade=FALSE)
 install.packages(jasptoolsPath, type="source", repos=NULL)
 
 JASPgraphsPath <- file.path("..", "..", "..", "JASP-Engine", "JASPgraphs")
-devtools::install_deps(JASPgraphsPath, upgrade="never")
+devtools::install_deps(JASPgraphsPath, upgrade=FALSE)
 install.packages(JASPgraphsPath, type="source", repos=NULL)
