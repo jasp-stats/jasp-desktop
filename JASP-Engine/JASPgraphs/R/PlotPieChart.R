5795be173fe74f4c47073a8c70303eb9ddabc656
vandenman <donvdbergh@hotmail.com>
2019-06-21 16:47:32 +0200

    moved examples and now uses colorspace::qualitative_hcl

--- JASP-Engine/JASPgraphs/R/PlotPieChart.R
+++ JASP-Engine/JASPgraphs/R/PlotPieChart.R
@@ -1,9 +1,10 @@
 #' Make a Pie Chart
 #'
-#' @param value the value for each group.
-#' @param group character of factor that indicates which value belongs to which group.
+#' @param value The value for each group.
+#' @param group Character of factor that indicates which value belongs to which group.
 #' @param legendName Character, title of the legend.
-#' @param legendLabels character vector with names for the legend.
+#' @param legendLabels Character vector with names for the legend.
+#' @param legendColors Character vector with colors.
 #' @param showAxisText Logical, should the axis text be shown?
 #' @param asPercentages Logical, should value be transformed to percentages? Recommended to be true.
 #' @param ... Arguments to be passed to \code{\link{themeJasp}}.
@@ -11,35 +12,13 @@
 #' @return a ggplot object.
 #' @export
 #'
-#' @examples
-#' value <- c(25, 25, 50)
-#' gg <- letters[1:3]
-#' ga <- letters[4:6]
+#' @example inst/examples/ex-PlotPieChart.R
 #'
-#' g <- plotPieChart(value, gg)
-#' print(g)
-#' plotPieChart(value, gg, ga)
-#' plotPieChart(value, gg, ga, showAxisTicks = FALSE)
-#' plotPieChart(value, gg, ga, showAxisTicks = FALSE, legend.position = "none") # hide the legend
-#'
-#' # axis can still be modified
-#' print(g + scale_y_continuous(breaks = seq(0, 100, 10)))
-#' cm <- c(0, cumsum(value))
-#' breaks <- 100 - (cm[-1] + cm[-length(cc)]) / 2
-#' print(g + scale_y_continuous(breaks = breaks, labels = gg))
-#'
-#' # something more extreme:
-#' value <- rpois(25, 10)
-#' g <- as.character(seq_len(25))
-#' plotPieChart(value, g)
-#'
-#'
-#'
-#'
-#' @importFrom ggplot2 ggplot geom_bar coord_polar scale_fill_discrete labs element_text element_line element_blank
+#' @importFrom ggplot2 ggplot geom_bar coord_polar scale_fill_manual labs element_text element_line element_blank
 plotPieChart <- function(value, group,
                          legendName = deparse(substitute(group)),
                          legendLabels = if (is.factor(group)) levels(group) else unique(group),
+                         legendColors = colorspace::qualitative_hcl(length(unique(group))),
                          showAxisText = TRUE, showAxisTicks = showAxisText, asPercentages = TRUE,
                          ...) {
 
@@ -65,7 +44,7 @@ plotPieChart <- function(value, group,
   graph <- ggplot(df, aes(x = "", y=value, fill=group))+
     geom_bar(width = 1, stat = "identity") +
     coord_polar("y", start = 0) +
-    scale_fill_discrete(name = legendName, breaks = legendLabels) +
+    scale_fill_manual(values = legendColors, name = legendName, breaks = legendLabels) +
     labs(x = "", y = "")
 
   return(do.call(themeJasp, c(list(graph = graph), dots)) + theme(
