1a9342dbc67b4fe3446fd685fdb76a0b85803b55
TimKDJ <tim_jong@hotmail.com>
2019-06-25 10:16:13 +0200

    Always order the plots in the state that jasptools returns

--- JASP-Tests/R/tests/testthat/test-regressionlinear.R
+++ JASP-Tests/R/tests/testthat/test-regressionlinear.R
@@ -183,7 +183,8 @@ test_that("Residuals vs. Dependent plot matches", {
   )
   options$plotResidualsDependent <- TRUE
   results <- jasptools::run("RegressionLinear", "test.csv", options)
-  testPlot <- results[["state"]][["figures"]][[1]][["obj"]]
+
+  testPlot <- results[["state"]][["figures"]][[2]][["obj"]]
   expect_equal_plots(testPlot, "residuals-dependent", dir="RegressionLinear")
 })
 
