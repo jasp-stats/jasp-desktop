4456072521e08468c09137bd0020f147b7f86d2a
Joris Goosen <Joris@JorisGoosen.nl>
2019-07-12 23:35:14 +0200

    - Setting up flatpak for v0.10.1.0 - Fixed https://github.com/jasp-stats/jasp-issues/issues/399 by adding i386 mode

--- Tools/make-flatpak.sh
+++ Tools/make-flatpak.sh
@@ -7,8 +7,11 @@ echo "Moving org.jaspstats.JASP.json to flatpak-builder-folder"
 cp flatpak/org.jaspstats.JASP.json ../flatpak-builder-folder/org.jaspstats.JASP.json
 cd ../flatpak-builder-folder
 
-echo "Starting flatpak builder"
+echo "Starting flatpak builder x64"
 flatpak-builder --install-deps-from=flathub --gpg-sign=528338C233D5B3D8 --force-clean --ccache --repo=jasp-repo jasp-build org.jaspstats.JASP.json
+
+echo "Starting flatpak builder x86"
+flatpak-builder --arch=i386 --install-deps-from=flathub --gpg-sign=528338C233D5B3D8 --force-clean --ccache --repo=jasp-repo jasp-build org.jaspstats.JASP.json
 
 echo "Update Repository"
 flatpak build-update-repo jasp-repo --gpg-sign=528338C233D5B3D8 --generate-static-deltas
