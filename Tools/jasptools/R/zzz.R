b813d151ac0c37175e11764a7ad7da533e6d7418
TimKDJ <tim_jong@hotmail.com>
2019-07-25 16:00:09 +0200

    Notify users when there is a new version of jasptools

--- Tools/jasptools/R/zzz.R
+++ Tools/jasptools/R/zzz.R
@@ -7,6 +7,7 @@
     if (isJaspDesktopDir(path)) {
       packageStartupMessage(sprintf("Using jasp-desktop at %s", path))
       .jasptoolsInit(path)
+      .checkVersion(utils::packageVersion("jasptools"), path)
       return(NULL)
     } else {
       packageStartupMessage("Path of jasp-desktop is corrupted! Please use develop(path_to_jasp_desktop).")
@@ -16,4 +17,13 @@
   }
   .jasptoolsInit(NULL)
   invisible()
+}
+
+.checkVersion <- function(installedVersion, jaspDir) {
+  devVersion <- try(silent=TRUE, { utils::packageVersion("jasptools", file.path(jaspDir, "Tools")) })
+  if (inherits(devVersion, "try-error") || installedVersion >= devVersion)
+    return()
+    
+  jasptoolsPath <- file.path(jaspDir, "Tools", "jasptools")
+  packageStartupMessage(paste0("*** There is a newer version available (", devVersion,"). To update run `devtools::install(\"", jasptoolsPath, "\")`"))
 }
