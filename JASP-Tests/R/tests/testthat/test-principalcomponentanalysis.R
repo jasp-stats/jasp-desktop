5574cd77cdec0db12e6dab4dff1483cbdc5ea727
Erik-Jan van Kesteren <erikjanvankesteren@pm.me>
2019-08-28 16:05:03 +0200

    Skip scree plot test due to non-determinism

--- JASP-Tests/R/tests/testthat/test-principalcomponentanalysis.R
+++ JASP-Tests/R/tests/testthat/test-principalcomponentanalysis.R
@@ -41,6 +41,7 @@ test_that("Path Diagram plot matches", {
 })
 
 test_that("Scree plot matches", {
+  skip("Scree plot check does not work because some data is simulated (non-deterministic).")
   plotName <- results[["results"]][["modelContainer"]][["collection"]][["modelContainer_scree"]][["data"]]
   testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
   expect_equal_plots(testPlot, "scree-plot", dir = "PrincipalComponentAnalysis")
