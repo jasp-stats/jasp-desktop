e06ff1992e505dfcbb9bf5ffe8114d492393169e
vandenman <donvdbergh@hotmail.com>
2019-09-03 11:16:55 +0200

    geom_rangeframe now functional
    combined plot now working

--- JASP-Engine/JASPgraphs/R/printJASPgraphs.R
+++ JASP-Engine/JASPgraphs/R/printJASPgraphs.R
@@ -1,24 +1,3 @@
-# print.JASPgraphs <- function(x, newpage = is.null(vp), vp = NULL, ...) {
-#
-#   tmp <- mget(x = c("x_custom", "y_custom", "element_grob.element_custom_x", "element_grob.element_custom_y"),
-#               envir = .GlobalEnv, ifnotfound = rep(list(NULL), 4))
-#
-#   assign("x_custom", JASPgraphs:::x_custom, envir = .GlobalEnv)
-#   assign("y_custom", JASPgraphs:::y_custom, envir = .GlobalEnv)
-#   assign("element_grob.element_custom_x", JASPgraphs:::element_grob.element_custom_x, envir = .GlobalEnv)
-#   assign("element_grob.element_custom_y", JASPgraphs:::element_grob.element_custom_y, envir = .GlobalEnv)
-#
-#   x <- ggplot2:::print.ggplot(x, newpage = newpage, vp = vp, ...)
-#
-#   assign("x_custom", tmp[[1L]], envir = .GlobalEnv)
-#   assign("y_custom", tmp[[2L]], envir = .GlobalEnv)
-#   assign("element_grob.element_custom_x", tmp[[3L]], envir = .GlobalEnv)
-#   assign("element_grob.element_custom_y", tmp[[4L]], envir = .GlobalEnv)
-#
-#   invisible(x)
-#
-# }
-
 #' @method print JASPgraphs
 #' @export
 print.JASPgraphs <- function(x, ...) {
@@ -40,4 +19,17 @@ print.JASPgraphs <- function(x, ...) {
 
 #' @method plot JASPgraphs
 #' @export
-plot.JASPgraphs <- function(x, ...) print.JASPgraphs(x)
+plot.JASPgraphs <- function(x, ...) print.JASPgraphs(x, ...)
+
+#' @method print JASPgraphsPlot
+#' @export
+print.JASPgraphsPlot <- function(x, ...) {
+
+  x$plot(...)
+  return(invisible(TRUE))
+}
+
+#' @method plot JASPgraphsPlot
+#' @export
+plot.JASPgraphsPlot <- function(x, ...) print.JASPgraphsPlot(x, ...)
+
--- JASP-Tests/R/tests/testthat/helper-plots.R
+++ JASP-Tests/R/tests/testthat/helper-plots.R
@@ -6,8 +6,25 @@ expect_equal_plots <- function(test, name, dir) {
   if (length(test) == 0)
       stop("The new plot has no data. Please check your unit test; is the index path to the plot specified correctly?", call.=FALSE)
 
+  skip_if_grob(test)
+
+  if (inherits(test, "JASPgraphsPlot")) {
+    subplots <- test$subplots
+    nms <- names(subplots)
+    if (is.null(nms))
+      nms <- seq_along(subplots)
+
+    for (i in seq_along(subplots)) {
+      skip_if_grob(subplots[[i]]) # This means if one out of 20 plots is a grob, we skip the test!
+      vdiffr::expect_doppelganger(paste(dir, name, "subplot", nms[i], sep="-"), subplots[[i]], path=dir)
+    }
+
+  } else {
+    vdiffr::expect_doppelganger(paste(dir, name, sep="-"), test, path=dir)
+  }
+}
+
+skip_if_grob <- function(test) {
   if (inherits(test, "grob"))
     skip("Cannot reliably test matrix plots (they fail Windows <-> OSX)")
-
-  vdiffr::expect_doppelganger(paste(dir, name, sep="-"), test, path=dir)
 }
