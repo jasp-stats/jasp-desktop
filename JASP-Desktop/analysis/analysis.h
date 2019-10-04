a1cd407f8dcb7ed68dc02a240add4eecdcbfeac7
bruno boutin <b.boutin@uva.nl>
2019-10-04 12:47:10 +0200

    Model terms are wrong in Duplicate analyses

--- JASP-Desktop/analysis/analysis.h
+++ JASP-Desktop/analysis/analysis.h
@@ -102,6 +102,8 @@ public:
 	void setUserData(Json::Value userData)				{ _userData = userData;							}
 	void setRefreshBlocked(bool block)					{ _refreshBlocked = block;						}
 	void setUsesJaspResults(bool usesJaspResults)		{ _useJaspResults = usesJaspResults;			}
+	void incrementRevision()							{ _revision++;									}
+
 
 	bool checkAnalysisEntry();
 	
@@ -119,7 +121,6 @@ public:
 			bool				usesJaspResults()	const	{ return _useJaspResults;					}
 			Status				status()			const	{ return _status;							}
 			int					revision()			const	{ return _revision;							}
-			void				incrementRevision()			{ _revision++;								}
 			bool				isRefreshBlocked()	const	{ return _refreshBlocked;					}
 			QString				helpFile()			const	{ return _helpFile;							}
 	const	Json::Value		&	getSaveImgOptions()	const	{ return _saveImgOptions;					}
@@ -127,6 +128,7 @@ public:
 			DataSet			*	getDataSet()		const;
 	Modules::DynamicModule	*	dynamicModule()		const	{ return _dynamicModule;					}
 			AnalysisForm	*	form()				const	{ return _analysisForm;						}
+			bool				isDuplicate()		const	{ return _isDuplicate;						}
 
 			void		refresh();
 			void		reload();
