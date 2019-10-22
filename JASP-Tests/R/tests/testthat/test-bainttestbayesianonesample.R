44234c779e834e95d0677a05dfb9c50f2a5e4674
Koen Derks <koen-derks@hotmail.com>
2019-10-22 12:36:55 +0200

    Add units tests for Bain

---
+++ JASP-Tests/R/tests/testthat/test-bainttestbayesianonesample.R
@@ -0,0 +1,39 @@
+context("Bain One Sample T-Test")
+
+options <- jasptools::analysisOptions("BainTTestBayesianOneSample")
+options$bayesFactorPlot <- TRUE
+options$bayesFactorType <- "BF01"
+options$descriptives <- TRUE
+options$descriptivesPlots <- TRUE
+options$hypothesis <- "allTypes"
+options$testValue <- 51
+options$variables <- list("age")
+set.seed(1)
+results <- jasptools::run("BainTTestBayesianOneSample", "sesame.csv", options)
+
+
+test_that("BF plot matches", {
+  plotName <- results[["results"]][["BFplots"]][["collection"]][["BFplots_age"]][["data"]]
+  testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
+  expect_equal_plots(testPlot, "BF age", dir="BainTTestBayesianOneSample")
+})
+
+test_that("Bain One Sample T-test table results match", {
+  table <- results[["results"]][["bainTable"]][["data"]]
+  expect_equal_tables(table,
+                      list("", 15.1135035681881, 15.8743731825382, "age", 0.885613607789322,
+                           0.0585975054555463, 0.0557888867551317, "H0: Equal", "H1: Bigger",
+                           "H2: Smaller"))
+})
+
+test_that("Descriptives plot matches", {
+  plotName <- results[["results"]][["descriptivesPlots"]][["collection"]][["descriptivesPlots_age"]][["data"]]
+  testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
+  expect_equal_plots(testPlot, "descritptives age", dir="BainTTestBayesianOneSample")
+})
+
+test_that("Descriptive Statistics table results match", {
+  table <- results[["results"]][["descriptivesTable"]][["data"]]
+  expect_equal_tables(table,
+                      list(240, 50.212, 51.013, 6.292, 0.406, 51.813, "age"))
+})
