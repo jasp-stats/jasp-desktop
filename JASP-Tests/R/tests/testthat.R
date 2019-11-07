e26388de801d7c8d833525426f3e5516629ec2ed
vandenman <donvdbergh@hotmail.com>
2019-11-07 15:20:15 +0100

    changed upper bound of testthat fails

--- JASP-Tests/R/tests/testthat.R
+++ JASP-Tests/R/tests/testthat.R
@@ -6,6 +6,8 @@ library(testthat)
 develop(path = file.path("..", "..", ".."))
 setPkgOption("pkgs.dir", "~/pkgs/Frameworks/R.framework/Versions/3.6/Resources/library")
 
+options("testthat.progress.max_fails" = 1E3L)
+
 result <- test_dir("testthat")
 result <- as.data.frame(result)
 
