ee0dfc60876add59ac7f2f0f97e15c97869f8a21
Joris Goosen <Joris@JorisGoosen.nl>
2020-01-16 17:57:07 +0100

    R translation bain (#3732)
    * Gettext'ed bain stuff:
    - ANCOVA
    - ANOVA
    - Linear Regression
    - Independent Samples ttest (welch)
    - ttest one & paired sample(s)
    
    * Fix bug found by unittest
    
    * Requested change fixed

--- JASP-Engine/JASP/R/bainancovabayesian.R
+++ JASP-Engine/JASP/R/bainancovabayesian.R
@@ -55,14 +55,14 @@ BainAncovaBayesian	 <- function(jaspResults, dataset, options, ...) {
 	variables <- c(options[["dependent"]], options[["fixedFactors"]], unlist(options[["covariates"]]))
 	bainTable <- createJaspTable("Bain ANCOVA")
 
-	bainTable$addColumnInfo(name="hypotheses", 		type="string", title="")
-	bainTable$addColumnInfo(name="BF", 				type="number", title= "BF.c")
-	bainTable$addColumnInfo(name="PMP1", 			type="number", title= "PMP a")
-	bainTable$addColumnInfo(name="PMP2", 			type="number", title= "PMP b")
+  bainTable$addColumnInfo(name="hypotheses", type="string", title="")
+  bainTable$addColumnInfo(name="BF", 				 type="number", title= gettext("BF.c"))
+  bainTable$addColumnInfo(name="PMP1", 			 type="number", title= gettext("PMP a"))
+  bainTable$addColumnInfo(name="PMP2", 			 type="number", title= gettext("PMP b"))
 	bainTable$position <- position
 
-	message <-  "BF.c denotes the Bayes factor of the hypothesis in the row versus its complement.
-				Posterior model probabilities (a: excluding the unconstrained hypothesis, b: including the unconstrained hypothesis) are based on equal prior model probabilities."
+  message <-  gettext("BF.c denotes the Bayes factor of the hypothesis in the row versus its complement.\
+Posterior model probabilities (a: excluding the unconstrained hypothesis, b: including the unconstrained hypothesis) are based on equal prior model probabilities.")
 	bainTable$addFootnote(message=message)
 
 	bainTable$addCitation(.bainGetCitations())
@@ -75,9 +75,9 @@ BainAncovaBayesian	 <- function(jaspResults, dataset, options, ...) {
 	if (any(variables %in% missingValuesIndicator)) {
 		i <- which(variables %in% missingValuesIndicator)
 		if (length(i) > 1) {
-			bainTable$addFootnote(message= paste0("The variables ", paste(variables[i], collapse = ", "), " contain missing values, the rows containing these values are removed in the analysis."), symbol="<b>Warning.</b>")
+      bainTable$addFootnote(message= gettextf("The variables %s contain missing values, the rows containing these values are removed in the analysis.", paste(variables[i], collapse = ", ")), symbol=gettext("<b>Warning.</b>"))
 		} else if (length(i) == 1) {
-			bainTable$addFootnote(message= paste0("The variable ", variables[i], " contains missing values, the rows containing these values are removed in the analysis."), symbol="<b>Warning.</b>")
+      bainTable$addFootnote(message= gettextf("The variable %s contains missing values, the rows containing these values are removed in the analysis.", variables[i]), symbol=gettext("<b>Warning.</b>"))
 		}
 	}
 
@@ -85,7 +85,7 @@ BainAncovaBayesian	 <- function(jaspResults, dataset, options, ...) {
 	varLevels <- levels(groupCol)
 
 	if (length(varLevels) > 15) {
-		bainTable$setError("The fixed factor has too many levels for a Bain analysis.")
+    bainTable$setError(gettext("The fixed factor has too many levels for a Bain analysis."))
 		return()
 	}
 
@@ -103,15 +103,15 @@ BainAncovaBayesian	 <- function(jaspResults, dataset, options, ...) {
 	})
 
 	if (isTryError(p)) {
-		bainContainer$setError(paste0("An error occurred in the analysis:<br>", .extractErrorMessage(p), "<br><br>Please double check your variables and model constraints."))
+    bainContainer$setError(gettextf("An error occurred in the analysis:<br>%s<br><br>Please double check your variables and model constraints.", .extractErrorMessage(p)))
 		return()
 	}
 
 	for (i in 1:(length(bainResult$fit$BF)-1)) {
-		row <- list(hypotheses = paste0("H",i), BF = bainResult$fit$BF[i], PMP1 = bainResult$fit$PMPa[i], PMP2 = bainResult$fit$PMPb[i])
+    row <- list(hypotheses = gettextf("H%i",i), BF = bainResult$fit$BF[i], PMP1 = bainResult$fit$PMPa[i], PMP2 = bainResult$fit$PMPb[i])
 		bainTable$addRows(row)
 	}
-	row <- list(hypotheses = "Hu", BF = "", PMP1 = "", PMP2 = bainResult$fit$PMPb[length(bainResult$fit$BF)])
+  row <- list(hypotheses = gettext("Hu"), BF = "", PMP1 = "", PMP2 = bainResult$fit$PMPb[length(bainResult$fit$BF)])
 	bainTable$addRows(row) 
 }
 
@@ -119,21 +119,22 @@ BainAncovaBayesian	 <- function(jaspResults, dataset, options, ...) {
 
 	if (!is.null(bainContainer[["bayesFactorMatrix"]]) || !options[["bayesFactorMatrix"]]) return()
 
-	bayesFactorMatrix <- createJaspTable("Bayes Factor Matrix")
+  bayesFactorMatrix <- createJaspTable(gettext("Bayes Factor Matrix"))
 	bayesFactorMatrix$position <- position
 
 	if (type == "regression")
 		bayesFactorMatrix$dependOn(options = c("bayesFactorMatrix", "standardized", "seed"))
+
 	if (type == "ancova" || type == "anova")
 		bayesFactorMatrix$dependOn(options = c("bayesFactorMatrix", "seed"))
 		
-	bayesFactorMatrix$addColumnInfo(name = "hypothesis", title = "", type = "string")
-	bayesFactorMatrix$addColumnInfo(name = "H1", type = "number")
+  bayesFactorMatrix$addColumnInfo(name = "hypothesis",  title = "",             type = "string")
+  bayesFactorMatrix$addColumnInfo(name = "H1",          title = gettext("H1"),  type = "number")
 	
 	bainContainer[["bayesFactorMatrix"]] <- bayesFactorMatrix
 
 	if (!ready || bainContainer$getError()) {
-		row <- data.frame(hypothesis = "H1", H1 = ".")
+    row <- data.frame(hypothesis = gettext("H1"), H1 = ".")
 		bayesFactorMatrix$addRows(row)
 		return()
 	}
@@ -143,11 +144,11 @@ BainAncovaBayesian	 <- function(jaspResults, dataset, options, ...) {
 
 	if (nrow(BFmatrix) > 1) {
 		for (i in 2:nrow(BFmatrix))
-			bayesFactorMatrix$addColumnInfo(name = paste0("H", i), type = "number")
+      bayesFactorMatrix$addColumnInfo(name = paste0("H", i), title = gettextf("H%i", i), type = "number")
 	}
 
 	for (i in 1:nrow(BFmatrix)) {
-		tmp <- list(hypothesis = paste0("H", i))
+    tmp <- list(hypothesis = gettextf("H%i", i))
 		for (j in 1:ncol(BFmatrix)) {
 			tmp[[paste0("H", j)]] <- BFmatrix[i,j]
 		}
@@ -179,12 +180,12 @@ BainAncovaBayesian	 <- function(jaspResults, dataset, options, ...) {
 
 	if (!is.null(jaspResults[["legendTable"]])) return() #The options for this table didn't change so we don't need to rebuild it
 
-		legendTable <- createJaspTable("Hypothesis Legend")
+    legendTable <- createJaspTable(gettext("Hypothesis Legend"))
 		legendTable$dependOn(options =c("model", "fixedFactors"))
 		legendTable$position <- position
 
-		legendTable$addColumnInfo(name="number", type="string", title="")
-		legendTable$addColumnInfo(name="hypothesis", type="string", title="Hypothesis")
+    legendTable$addColumnInfo(name="number"    , type="string", title="")
+    legendTable$addColumnInfo(name="hypothesis", type="string", title=gettext("Hypothesis"))
 		
 		jaspResults[["legendTable"]] <- legendTable
 
@@ -192,7 +193,7 @@ BainAncovaBayesian	 <- function(jaspResults, dataset, options, ...) {
 			rest.string <- .bainCleanModelInput(options[["model"]])
 			hyp.vector <- unlist(strsplit(rest.string, "[;]"))
 				for (i in 1:length(hyp.vector)) {
-					row <- list(number = paste0("H",i), hypothesis = hyp.vector[i])
+          row <- list(number = gettextf("H%i",i), hypothesis = hyp.vector[i])
 					legendTable$addRows(row)
 				}
 		} else if (options[["fixedFactors"]] != "") {
@@ -200,7 +201,7 @@ BainAncovaBayesian	 <- function(jaspResults, dataset, options, ...) {
 			fact <- dataset[, .v(factor)]
 			levels <- levels(fact)
 			string <- paste(paste(factor, levels, sep = ""), collapse = " = ")
-			row <- list(number = "H1", hypothesis = string)
+      row <- list(number = gettext("H1"), hypothesis = string)
 			legendTable$addRows(row)
 		}
 }
@@ -210,9 +211,9 @@ BainAncovaBayesian	 <- function(jaspResults, dataset, options, ...) {
   PMPa <- na.omit(x$fit$PMPa)
   PMPb <- x$fit$PMPb
   numH <- length(PMPa)
-  P_lables <- paste("H", 1:numH, sep = "")
+  P_lables <- paste(gettext("H"), 1:numH, sep = "")
   ggdata1 <- data.frame(lab = P_lables, PMP = PMPa)
-  ggdata2 <- data.frame(lab = c(P_lables, "Hu"), PMP = PMPb)
+  ggdata2 <- data.frame(lab = c(P_lables, gettext("Hu")), PMP = PMPb)
   
   if (numH == 1) {
     
@@ -222,7 +223,7 @@ BainAncovaBayesian	 <- function(jaspResults, dataset, options, ...) {
           ggplot2::coord_polar(theta = "y", direction = -1) +
           ggplot2::labs(x = "", y = "", title = "") + 
           ggplot2::theme(panel.grid = ggplot2::element_blank(), legend.position = "none") + 
-          ggplot2::scale_y_continuous(breaks = cumsum(rev(PMPb)) - rev(PMPb)/2, labels = rev(c(P_lables, "Hu"))) +
+          ggplot2::scale_y_continuous(breaks = cumsum(rev(PMPb)) - rev(PMPb)/2, labels = rev(c(P_lables, gettext("Hu")))) +
           ggplot2::theme(panel.background = ggplot2::element_blank(), 
                          axis.text=ggplot2::element_text(size=17, color = "black"),
                           plot.title = ggplot2::element_text(size=18, hjust = .5),
@@ -237,7 +238,7 @@ BainAncovaBayesian	 <- function(jaspResults, dataset, options, ...) {
           ggplot2::geom_bar(stat = "identity", width = 1e10, color = "black", size = 1) +
           ggplot2::geom_col() + 
           ggplot2::coord_polar(theta = "y", direction = -1) +
-          ggplot2::labs(x = "", y = "", title = "Excluding Hu", size = 30) +
+          ggplot2::labs(x = "", y = "", title = gettext("Excluding Hu"), size = 30) +
           ggplot2::theme(panel.grid = ggplot2::element_blank(), legend.position = "none") +
           ggplot2::scale_y_continuous(breaks = cumsum(rev(PMPa)) - rev(PMPa)/2, labels = rev(P_lables)) +
           ggplot2::theme(panel.background = ggplot2::element_blank(),
@@ -250,9 +251,9 @@ BainAncovaBayesian	 <- function(jaspResults, dataset, options, ...) {
           ggplot2::geom_bar(stat = "identity", width = 1e10, color = "black", size = 1) +
           ggplot2::geom_col() + 
           ggplot2::coord_polar(theta = "y", direction = -1) +
-          ggplot2::labs(x = "", y = "", title = "Including Hu", size = 30) +
+          ggplot2::labs(x = "", y = "", title = gettext("Including Hu"), size = 30) +
           ggplot2::theme(panel.grid = ggplot2::element_blank(), legend.position = "none") +
-          ggplot2::scale_y_continuous(breaks = cumsum(rev(PMPb)) - rev(PMPb)/2, labels = rev(c(P_lables, "Hu"))) +
+          ggplot2::scale_y_continuous(breaks = cumsum(rev(PMPb)) - rev(PMPb)/2, labels = rev(c(P_lables, gettext("Hu")))) +
           ggplot2::theme(panel.background = ggplot2::element_blank(),
                           axis.text=ggplot2::element_text(size=17, color = "black"),
                           plot.title = ggplot2::element_text(size=18, hjust = .5),
