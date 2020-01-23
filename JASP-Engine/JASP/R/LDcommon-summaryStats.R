65cd6563836e3d91af7eb2d7824bb61bd38fc185
Simon Kucharsky <kucharssim@gmail.com>
2020-01-23 15:36:28 +0100

    add drawing samples

--- JASP-Engine/JASP/R/LDcommon-summaryStats.R
+++ JASP-Engine/JASP/R/LDcommon-summaryStats.R
@@ -1,5 +1,21 @@
+#
+# Copyright (C) 2013-2020 University of Amsterdam
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 .simulateData <- function(jaspResults, options, as = "scale"){
-  return()
   if(is.null(jaspResults[['simdata']])){
     sample <- do.call(options[['rFun']], c(options[['pars']], n = options[['sampleSize']]))
     jaspResults[['simdata']] <- createJaspState(sample)
