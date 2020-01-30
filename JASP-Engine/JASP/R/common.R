527d16c984195c7ccd0f16a83f1d941629ca7b05
FBartos <38475991+FBartos@users.noreply.github.com>
2020-01-30 17:20:12 +0100

    summary stats fix

--- JASP-Engine/JASP/R/common.R
+++ JASP-Engine/JASP/R/common.R
@@ -3023,9 +3023,8 @@ postProcessModuleInstall <- function(moduleLibraryPath)
   if (is.list(options) && c("setSeed", "seed") %in% names(options)) {
     if (isTRUE(options[["setSeed"]]))
       set.seed(options[["seed"]])
-  } else {
-    stop(paste(".setSeedJASP was called with an incorrect argument.",
-               "The argument options should be the options list from QML.",
-               "Ensure that the SetSeed{} QML component is present in the QML file for this analysis."))
   }
+  #  stop(paste(".setSeedJASP was called with an incorrect argument.",
+  #             "The argument options should be the options list from QML.",
+  #             "Ensure that the SetSeed{} QML component is present in the QML file for this analysis."))
 }
