918fe97e6cfa2c2cc40317a0522e9ae18b2999c2
Joris Goosen <Joris@JorisGoosen.nl>
2020-06-16 13:31:43 +0200

    No queued connection please (#4133)
    - Fixes https://github.com/jasp-stats/INTERNAL-jasp/issues/899 (No topnote)

--- JASP-Desktop/mainwindow.cpp
+++ JASP-Desktop/mainwindow.cpp
@@ -319,7 +319,7 @@ void MainWindow::makeConnections()
 	connect(_analyses,				&Analyses::unselectAnalysisInResults,				_resultsJsInterface,	&ResultsJsInterface::unselect								);
 	connect(_analyses,				&Analyses::analysisImageEdited,						_resultsJsInterface,	&ResultsJsInterface::analysisImageEditedHandler				);
 	connect(_analyses,				&Analyses::analysisRemoved,							_resultsJsInterface,	&ResultsJsInterface::removeAnalysis							);
-	connect(_analyses,				&Analyses::setResultsMeta,							_resultsJsInterface,	&ResultsJsInterface::setResultsMeta,						Qt::QueuedConnection);
+	connect(_analyses,				&Analyses::setResultsMeta,							_resultsJsInterface,	&ResultsJsInterface::setResultsMeta							);
 	connect(_analyses,				&Analyses::moveAnalyses,							_resultsJsInterface,	&ResultsJsInterface::moveAnalyses							);
 	connect(_analyses,				&Analyses::developerMode,							_preferences,			&PreferencesModel::developerMode							);
 	connect(_analyses,				&Analyses::somethingModified,						[&](){					if(_package) _package->setModified(true); }					);
