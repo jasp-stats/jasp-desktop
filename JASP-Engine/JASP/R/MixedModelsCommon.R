c82282ab2474fa9fa0bc0e20aefbb188223ce683
FBartos <38475991+FBartos@users.noreply.github.com>
2020-06-23 17:39:08 +0200

    fixes: #862
    A fitted model was deleted after setting an error withing the summary table. However, next ioption refreshed the model and it wasn't reset to null since the summary table wasn't called - the other output options could've been accessed without a proper model.
    Fixed by creating a state that checks whether the model contained error. Doesn't allow to proceed unless a proper model was fitted.

--- JASP-Engine/JASP/R/MixedModelsCommon.R
+++ JASP-Engine/JASP/R/MixedModelsCommon.R
@@ -40,7 +40,7 @@
   if(type %in% c("BLMM", "BGLMM")).mmSummaryStanova(jaspResults, dataset, options, type)
 
   
-  if (!is.null(jaspResults[["mmModel"]])) {
+  if (!is.null(jaspResults[["mmModel"]]) && is.null(jaspResults[["hasERROR"]])) {
     
     # show fixed / random effects summary
     if (options$showFE){
@@ -491,18 +491,25 @@
       if (any(attr(model, "class") %in% c("std::runtime_error", "C++Error", "error"))) {
         if (model$message == "(maxstephalfit) PIRLS step-halvings failed to reduce deviance in pwrssUpdate") {
           ANOVAsummary$setError(
-            gettext("The optimizer failed to find solution. Probabably due to quasi-separation in the data. Try removing some of the predictors.")
+            gettext("The optimizer failed to find a solution. Probabably due to quasi-separation in the data. Try removing some of the predictors.")
           )
         } else if (model$message == "cannot find valid starting values: please specify some") {
           # currently no solution to this, it seems to be a problem with synthetic data only.
           # I will try silving it once someone actually has problem with real data.
-          ANOVAsummary$setError(gettext("Invalid starting values."))
+          ANOVAsummary$setError(gettext("The optimizer failed to find a solution due to invalid starting values. (JASP currently does not support specifying different starting values.)"))
         } else{
           ANOVAsummary$setError(model$message)
         }
         
-        jaspResults[["ANOVAsummary"]] <- ANOVAsummary
-        jaspResults[["mmModel"]]      <- NULL
+        
+        hasERROR <- createJaspState()
+        hasERROR$object <- TRUE
+        hasERROR$dependOn(c(dependencies, seed_dependencies))
+        jaspResults[["hasERROR"]] <- hasERROR
+        
+        
+        jaspResults[["ANOVAsummary"]]   <- ANOVAsummary
+
         return()
       }
       
