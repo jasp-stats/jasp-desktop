f49f747af123ff40e6ddbf98d74f71a2d793d916
vandenman <donvdbergh@hotmail.com>
2020-10-02 16:32:34 +0200

    select correct arguments depending on whether Bayesian or frequentist analysis is run

--- JASP-Engine/JASP/R/commonAnovaBayesian.R
+++ JASP-Engine/JASP/R/commonAnovaBayesian.R
@@ -1439,8 +1439,8 @@
                                   betweenvars = betweenSubjectFactors,
                                   withinvars = repeatedMeasuresFactors,
                                   idvar = .BANOVAsubjectName,
-                                  conf.interval = options$confidenceIntervalInterval,
-                                  na.rm=TRUE, .drop = FALSE, errorBarType = options$errorBarType,
+                                  conf.interval = conf.interval,
+                                  na.rm=TRUE, .drop = FALSE, errorBarType = errorBarType,
                                   usePooledSE = usePooledSE)
   }
 
