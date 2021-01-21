136b547a11f230dd05fa665c11bba49164fb063d
Joris Goosen <Joris@JorisGoosen.nl>
2021-01-21 15:54:01 +0100

    Instead of having a "selection" column you now singleclick for select and double for edit. - Uses a painful amount of timers... - should be done better in development

--- JASP-Desktop/data/labelmodel.h
+++ JASP-Desktop/data/labelmodel.h
@@ -3,6 +3,7 @@
 
 
 #include "datasettableproxy.h"
+#include <QTimer>
 
 class LabelModel : public DataSetTableProxy
 {
@@ -16,11 +17,10 @@ class LabelModel : public DataSetTableProxy
 	Q_PROPERTY(float	filterColWidth	READ filterColWidth		WRITE setFilterColWidth		NOTIFY filterColWidthChanged	)
 	Q_PROPERTY(float	valueColWidth	READ valueColWidth		WRITE setValueColWidth		NOTIFY valueColWidthChanged		)
 	Q_PROPERTY(float	labelColWidth	READ labelColWidth		WRITE setLabelColWidth		NOTIFY labelColWidthChanged		)
-	Q_PROPERTY(float	selectColWidth	READ selectColWidth		WRITE setSelectColWidth		NOTIFY selectColWidthChanged	)
 
 public:
 	///These are also used directly (aka without this enum) in DataSetPackage::data
-	enum class	Column { Filter=0, Value=1, Label=2, Selection=3 };
+	enum class	Column { Filter=0, Value=1, Label=2 };
 
 				LabelModel();
 
@@ -39,26 +39,27 @@ public:
 	Q_INVOKABLE void moveSelectionUp();
 	Q_INVOKABLE void moveSelectionDown();
 	Q_INVOKABLE void resetFilterAllows();
+	
+	Q_INVOKABLE void singleClickForSelect(			int row);
+	Q_INVOKABLE void doubleClickSoonAfterSelect(	int row);
 
 	std::vector<bool>			filterAllows(size_t col);
 	std::vector<std::string>	labels(size_t col);
 
-
 	float filterColWidth() const { return getColumnWidth(0); }
 	float valueColWidth()  const { return getColumnWidth(1); }
 	float labelColWidth()  const { return getColumnWidth(2); }
-	float selectColWidth() const { return getColumnWidth(3); }
 
 public slots:
 	void filteredOutChangedHandler(int col);
 	void setVisible(bool visible);
+	void toggleSelected(int row);
 	void columnAboutToBeRemoved(int column);
 	void columnDataTypeChanged(std::string colName);
 
 	void setFilterColWidth(float colWidth) { if(setColumnWidth(0, colWidth)) emit filterColWidthChanged(filterColWidth());	}
 	void setValueColWidth (float colWidth) { if(setColumnWidth(1, colWidth)) emit valueColWidthChanged(valueColWidth());	}
 	void setLabelColWidth (float colWidth) { if(setColumnWidth(2, colWidth)) emit labelColWidthChanged(labelColWidth());	}
-	void setSelectColWidth(float colWidth) { if(setColumnWidth(3, colWidth)) emit selectColWidthChanged(selectColWidth());	}
 
 	void onChosenColumnChanged();
 
@@ -71,7 +72,6 @@ signals:
 	void filterColWidthChanged(float filterColWidth);
 	void valueColWidthChanged( float valueColWidth);
 	void labelColWidthChanged( float labelColWidth);
-	void selectColWidthChanged(float selectColWidth);
 
 private:
 	bool				setColumnWidth(int col, float width);
@@ -81,8 +81,10 @@ private:
 
 private:
 	bool				_visible		= false;
-	std::vector<float>	_colWidths		= { 60, 120, 400, 60} ;
+	std::vector<float>	_colWidths		= { 60, 120, 400} ;
 	std::set<QString>	_selected;
+	
+	std::map<int, QTimer*>	_toggleSelectTimers;
 };
 Q_DECLARE_METATYPE(LabelModel::Column)
 
