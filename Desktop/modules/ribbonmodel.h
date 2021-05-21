74ac062b84052c45a7d36e34c869a7e4825c9687
Joris Goosen <Joris@JorisGoosen.nl>
2021-05-21 18:58:55 +0200

    Implements a data mode - can enter data manually - copy/paste to itself and to from spreadsheet editors - Arrow + shift now also changes selection - Allow for changing columnnames - Make sure a "New Data"-set can be closed - Make sure handle can't be seen when in datamode (like results+analyses) - When switching to back the analysesmode, last edit should be absorbed - Make ribbonbuttons behave - First attempts at restoring external data editing but commented it out. Im not sure if and how this should be done - Some general cleaning up - select all and cut and include columnnames in all that - Also add a rightclick menu - Don't show edit data etc in modules menu - Adds support for more context-menu options: -- insert. delete and select columns & rows - Turn BOM workaround on by default for SKF - Fix after rebase/merge before release 0.15

--- Desktop/modules/ribbonmodel.h
+++ Desktop/modules/ribbonmodel.h
@@ -33,7 +33,9 @@
 class RibbonModel : public QAbstractListModel
 {
 	Q_OBJECT
-	Q_PROPERTY(int highlightedModuleIndex READ highlightedModuleIndex WRITE setHighlightedModuleIndex NOTIFY highlightedModuleIndexChanged)
+	Q_PROPERTY(int	highlightedModuleIndex	READ highlightedModuleIndex WRITE setHighlightedModuleIndex NOTIFY highlightedModuleIndexChanged)
+	Q_PROPERTY(int	currentRow				READ currentRow				WRITE setCurrentRow				NOTIFY currentRowChanged			)
+	Q_PROPERTY(bool dataMode				READ dataMode												NOTIFY dataModeChanged				)
 
 public:
 	enum {
@@ -51,27 +53,32 @@ public:
 		SpecialRole
 	};
 
+	enum class RowType { Analyses = 0, Data};
+
 	RibbonModel();
 	~RibbonModel() { _singleton = nullptr; } 
 	
 	static RibbonModel * singleton() { return _singleton; }
 	
 
-	int								rowCount(const QModelIndex & = QModelIndex())				const override	{	return int(_moduleNames.size());	}
+	int								rowCount(const QModelIndex & = QModelIndex())				const override	{	return int(_buttonNames[_currentRow].size());	}
 	QVariant						data(const QModelIndex &index, int role = Qt::DisplayRole)	const override;
 	virtual QHash<int, QByteArray>	roleNames()													const override;
 
 	void						loadModules(std::vector<std::string> commonModulesToLoad = {}, std::vector<std::string> extraModulesToLoad = {});
 
-	void						addRibbonButtonModelFromDynamicModule(Modules::DynamicModule * module);
-	void						addRibbonButtonRPrompt();
+	void						addSpecialRibbonButtonsEarly();
+	void						addSpecialRibbonButtonsLate();
+	Q_INVOKABLE void			showData()			{ setCurrentRow(int(RowType::Data));		}
+	Q_INVOKABLE void			showStatistics()	{ setCurrentRow(int(RowType::Analyses));	}
+	void						genShowEmptyData()	{ generateEmptyData(); showData(); }
 
 	void						removeRibbonButtonModel(std::string moduleName);
 
 
 	bool						isModuleName(std::string name)						const	{ return _buttonModelsByName.count(name) > 0; }
-	QString						moduleName(size_t index)							const	{ return QString::fromStdString(_moduleNames[index]);}
-	RibbonButton*				ribbonButtonModelAt(size_t index)					const	{ return ribbonButtonModel(_moduleNames[index]); }
+	QString						moduleName(size_t index)							const	{ return QString::fromStdString(_buttonNames[_currentRow][index]);}
+	RibbonButton*				ribbonButtonModelAt(size_t index)					const	{ return ribbonButtonModel(		_buttonNames[_currentRow][index]); }
 	RibbonButton*				ribbonButtonModel(std::string moduleName)			const;
 	int							ribbonButtonModelIndex(RibbonButton * model)		const;
 
@@ -79,10 +86,15 @@ public:
 	Q_INVOKABLE void			setModuleEnabled(int ribbonButtonModelIndex, bool enabled);
 	QStringList					getModulesEnabled() const;
 
-	int							highlightedModuleIndex() const { return _highlightedModuleIndex; }
-	Modules::AnalysisEntry*		getAnalysis(std::string moduleName, const std::string & analysisName);
+	int							highlightedModuleIndex()							const { return _highlightedModuleIndex; }
+	int							currentRow()										const { return _currentRow;				}
+	bool						dataMode()											const {	return _currentRow == size_t(RowType::Data);	}
+	Modules::AnalysisEntry*		getAnalysis(std::string moduleName,					const std::string & analysisName);
 
 	std::string					getModuleNameFromAnalysisName(const std::string & analysisName);
+
+
+
 
 signals:
 				void currentButtonModelChanged();
@@ -90,27 +102,38 @@ signals:
 				void highlightedModuleIndexChanged(int highlightedModuleIndex);
 				void showRCommander();
 				void invalidateFilterModel();
+				void currentRowChanged();
+				void dataLoadedChanged(bool loaded);
+				void generateEmptyData();
+				void dataModeChanged(bool dataMode);
+				void startExternalEdit();
+				void stopExternalEdit();
+				void resizeData();
+				void finishCurrentEdit();
+
 
 public slots:
-	void addDynamicRibbonButtonModel(Modules::DynamicModule * module)	{ addRibbonButtonModelFromDynamicModule(module);		}
+	void addRibbonButtonModelFromDynamicModule(Modules::DynamicModule * module);
 	void removeDynamicRibbonButtonModel(QString moduleName)				{ removeRibbonButtonModel(moduleName.toStdString());				}
 	void setHighlightedModuleIndex(int highlightedModuleIndex);
 	//void moduleLoadingSucceeded(const QString & moduleName);
 	void analysisClicked(QString analysisFunction, QString analysisQML, QString analysisTitle, QString module);
+	void setCurrentRow(int currentRow);
 
 private slots:
-	void dynamicModuleChanged(Modules::DynamicModule * module);
+	void dynamicModuleChanged(	Modules::DynamicModule * module);
+	void dynamicModuleReplaced(	Modules::DynamicModule * oldModule, Modules::DynamicModule * module);
 	void ribbonButtonModelChanged(RibbonButton* model);
-	void dynamicModuleReplaced(Modules::DynamicModule * oldModule, Modules::DynamicModule * module);
 
 private: // functions
-	void addRibbonButtonModel(RibbonButton* model);
+	void addRibbonButtonModel(RibbonButton* model, size_t row);
 
 private: // fields
 	std::map<std::string, RibbonButton*>	_buttonModelsByName;
-	std::vector<std::string>				_moduleNames;
+	std::vector<stringvec>					_buttonNames; //Can be multiple rows, originally [ { Analyses }, { Data Mode } ]
 	int										_highlightedModuleIndex = -1;
 	std::vector<std::string>				_commonModulesToLoad;
+	size_t									_currentRow				= size_t(RowType::Analyses);
 	
 	static RibbonModel * _singleton;
 };
