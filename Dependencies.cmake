98936fdfe343142366cd88e731dd58d07d656ecc
Amir Masoud Abdol <amirmasoudabdol@icloud.com>
2021-11-19 19:48:01 +0100

    Fixed a few things:
    - Better installation of JAGS,
    - Integration of JAGS with the Module Installer.
       - I still expect this to fail because our current jags build tree is not standard, and maybe, the correct paths cause some problem, or it might not.
    - CPM somehow messes up the build tree, so, I still need to get that fixed. It still works but for some reasons it shuffles _everything_ around into a linux-like build tree which I don't think we want all the time.

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -30,52 +30,82 @@ set(CPM_USE_LOCAL_PACKAGES ON)
 #   GIT_TAG
 #   "1.9.5")
 
-cpmaddpackage(
-  NAME
-  LibArchive
-  VERSION
-  3.5.2
-  OPTIONS
-  "ENABLE_TEST OFF"
-  "JSONCPP_WITH_POST_BUILD_UNITTEST OFF"
-  GITHUB_REPOSITORY
-  "libarchive/libarchive"
-  GIT_TAG
-  "v3.5.2")
+# cpmaddpackage(
+#   NAME
+#   LibArchive
+#   VERSION
+#   3.5.2
+#   OPTIONS
+#   "ENABLE_TEST OFF"
+#   "JSONCPP_WITH_POST_BUILD_UNITTEST OFF"
+#   GITHUB_REPOSITORY
+#   "libarchive/libarchive"
+#   GIT_TAG
+#   "v3.5.2")
 
-cpmaddpackage(
-  NAME
-  ZLIB
-  VERSION
-  1.2.11
-  GITHUB_REPOSITORY
-  "madler/zlib"
-  GIT_TAG
-  "v1.2.11")
+# cpmaddpackage(
+#   NAME
+#   ZLIB
+#   VERSION
+#   1.2.11
+#   GITHUB_REPOSITORY
+#   "madler/zlib"
+#   GIT_TAG
+#   "v1.2.11")
 
-# find_program(MAKE NAMES gmake nmake make)
-# find_program(ACLOCAL NAMES aclocal)
-# find_program(AUTOCONF NAMES autoconf)
-# find_program(AUTORECONF NAMES autoreconf)
+find_program(MAKE NAMES gmake nmake make)
+find_program(ACLOCAL NAMES aclocal)
+find_program(AUTOCONF NAMES autoconf)
+find_program(AUTORECONF NAMES autoreconf)
 
-# externalproject_add(
-#   readstat
-#   PREFIX ExternalProjects/readstat
-#   GIT_REPOSITORY "https://github.com/WizardMac/ReadStat"
-#   GIT_TAG "v1.1.7"
-#   BUILD_IN_SOURCE ON
-#   CONFIGURE_COMMAND ./autogen.sh && autoupdate && ./configure
-#                     --prefix=${CMAKE_CURRENT_BINARY_DIR}
-#   BUILD_COMMAND ${MAKE})
+# Installing ReadStat
+#
+externalproject_add(
+  readstat
+  PREFIX Dependencies/readstat
+  GIT_REPOSITORY "https://github.com/WizardMac/ReadStat"
+  GIT_TAG "v1.1.7"
+  BUILD_IN_SOURCE ON
+  # LOG_CONFIGURE ON
+  # LOG_BUILD ON
+  # LOG_INSTALL ON
+  # LOG_OUTPUT_ON_FAILURE ON
+  CONFIGURE_COMMAND ./autogen.sh
+  COMMAND autoupdate
+  COMMAND
+    ./configure
+    --prefix=${CMAKE_CURRENT_BINARY_DIR}/Dependencies/readstat/src/readstat-build
+  BUILD_COMMAND ${MAKE})
 
-# # JAGS needs GNU Bison v3, https://www.gnu.org/software/bison.
-# externalproject_add(
-#   jags
-#   PREFIX ExternalProjects/jags
-#   HG_REPOSITORY "http://hg.code.sf.net/p/mcmc-jags/code-0"
-#   HG_TAG "release-4_3_0"
-#   BUILD_IN_SOURCE ON
-#   CONFIGURE_COMMAND
-#     ${ACLOCAL} && ${AUTORECONF} -fi && ./configure --disable-dependency-tracking
-#     --prefix=${CMAKE_CURRENT_BINARY_DIR}
-#   BUILD_COMMAND ${MAKE})
+externalproject_get_property(readstat SOURCE_DIR)
+set(readstat_SOURCE_DIR ${SOURCE_DIR})
+set(readstat_BUILD_DIR ${readstat_SOURCE_DIR}/../readstat-build)
+set(readstat_INCLUDES_DIR ${readstat_BUILD_DIR}/include)
+set(readstat_LIBRARIES_DIR ${readstat_BUILD_DIR}/lib)
+
+# Installing JAGS
+#
+# - JAGS needs GNU Bison v3, https://www.gnu.org/software/bison.
+# - With this, we can build JAGS, and link it, or even place it inside the the `R.framework`
+#
+externalproject_add(
+  jags
+  PREFIX Dependencies/jags
+  HG_REPOSITORY "http://hg.code.sf.net/p/mcmc-jags/code-0"
+  HG_TAG "release-4_3_0"
+  BUILD_IN_SOURCE ON
+  # LOG_CONFIGURE ON
+  # LOG_BUILD ON
+  # LOG_INSTALL ON
+  # LOG_OUTPUT_ON_FAILURE ON
+  CONFIGURE_COMMAND ${ACLOCAL}
+  COMMAND ${AUTORECONF} -fi
+  COMMAND ./configure --disable-dependency-tracking
+          --prefix=${CMAKE_CURRENT_BINARY_DIR}/Dependencies/jags/src/jags-build
+  BUILD_COMMAND ${MAKE})
+
+externalproject_get_property(jags SOURCE_DIR)
+set(JAGS_SOURCE_DIR ${SOURCE_DIR})
+set(JAGS_BUILD_DIR ${JAGS_SOURCE_DIR}/../jags-build)
+set(JAGS_INCLUDES_DIR ${JAGS_BUILD_DIR}/include)
+set(JAGS_LIBRARIES_DIR ${JAGS_BUILD_DIR}/lib)
