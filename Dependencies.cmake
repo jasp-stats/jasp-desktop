2be4868ec903601ea9e1cc454be984ce8cae03ae
Amir Masoud Abdol <amirmasoudabdol@icloud.com>
2021-11-19 16:27:51 +0100

    JAGS Integration
    - Downloading, building and installing JAGS during JASP's build process
    - Better handling of `libarchive`, `zlib`, and `openssl`. Since these are usually part of the toolchain, CMake tries to search for them first, and then start looking into the local. For instance, with this change, CMake first ask Xcode and VS.
    - [ ] The integration of the local dependencies is not yet complete but I'm slowly adding them one by one
    - [ ] I still also need to make sure that I look for dependencies once. There are duplicates around at the moment, which I don't really like

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -5,34 +5,34 @@ include(Tools/cmake/CPM.cmake)
 
 set(CPM_USE_LOCAL_PACKAGES ON)
 
-cpmaddpackage(
-  NAME
-  Boost
-  VERSION
-  1.77.0
-  OPTIONS
-  "BUILD_TESTING OFF"
-  GITHUB_REPOSITORY
-  "boostorg/boost"
-  GIT_TAG
-  "boost-1.77.0")
+# cpmaddpackage(
+#   NAME
+#   Boost
+#   VERSION
+#   1.77.0
+#   OPTIONS
+#   "BUILD_TESTING OFF"
+#   GITHUB_REPOSITORY
+#   "boostorg/boost"
+#   GIT_TAG
+#   "boost-1.77.0")
+
+# cpmaddpackage(
+#   NAME
+#   jsoncpp
+#   VERSION
+#   1.9.5
+#   OPTIONS
+#   "JSONCPP_WITH_TESTS OFF"
+#   "JSONCPP_WITH_POST_BUILD_UNITTEST OFF"
+#   GITHUB_REPOSITORY
+#   "open-source-parsers/jsoncpp"
+#   GIT_TAG
+#   "1.9.5")
 
 cpmaddpackage(
   NAME
-  jsoncpp
-  VERSION
-  1.9.5
-  OPTIONS
-  "JSONCPP_WITH_TESTS OFF"
-  "JSONCPP_WITH_POST_BUILD_UNITTEST OFF"
-  GITHUB_REPOSITORY
-  "open-source-parsers/jsoncpp"
-  GIT_TAG
-  "1.9.5")
-
-cpmaddpackage(
-  NAME
-  libarchive
+  LibArchive
   VERSION
   3.5.2
   OPTIONS
@@ -45,7 +45,7 @@ cpmaddpackage(
 
 cpmaddpackage(
   NAME
-  zlib
+  ZLIB
   VERSION
   1.2.11
   GITHUB_REPOSITORY
@@ -53,18 +53,29 @@ cpmaddpackage(
   GIT_TAG
   "v1.2.11")
 
-find_program(MAKE NAMES gmake nmake make)
-externalproject_add(
-  readstat
-  PREFIX ExternalProjects/readstat
-  # SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/readstat
-  GIT_REPOSITORY "https://github.com/WizardMac/ReadStat"
-  GIT_TAG "v1.1.7"
-  BUILD_IN_SOURCE ON
-  LOG_CONFIGURE ON
-  LOG_BUILD ON
-  LOG_INSTALL ON
-  LOG_OUTPUT_ON_FAILURE ON
-  CONFIGURE_COMMAND ./autogen.sh && ./configure
-                    --prefix=${CMAKE_CURRENT_BINARY_DIR}
-  BUILD_COMMAND ${MAKE})
+# find_program(MAKE NAMES gmake nmake make)
+# find_program(ACLOCAL NAMES aclocal)
+# find_program(AUTOCONF NAMES autoconf)
+# find_program(AUTORECONF NAMES autoreconf)
+
+# externalproject_add(
+#   readstat
+#   PREFIX ExternalProjects/readstat
+#   GIT_REPOSITORY "https://github.com/WizardMac/ReadStat"
+#   GIT_TAG "v1.1.7"
+#   BUILD_IN_SOURCE ON
+#   CONFIGURE_COMMAND ./autogen.sh && autoupdate && ./configure
+#                     --prefix=${CMAKE_CURRENT_BINARY_DIR}
+#   BUILD_COMMAND ${MAKE})
+
+# # JAGS needs GNU Bison v3, https://www.gnu.org/software/bison.
+# externalproject_add(
+#   jags
+#   PREFIX ExternalProjects/jags
+#   HG_REPOSITORY "http://hg.code.sf.net/p/mcmc-jags/code-0"
+#   HG_TAG "release-4_3_0"
+#   BUILD_IN_SOURCE ON
+#   CONFIGURE_COMMAND
+#     ${ACLOCAL} && ${AUTORECONF} -fi && ./configure --disable-dependency-tracking
+#     --prefix=${CMAKE_CURRENT_BINARY_DIR}
+#   BUILD_COMMAND ${MAKE})
