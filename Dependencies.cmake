93b792eb525c1a24d4bbfd8777d62b0776b16f1e
Amir Masoud Abdol <amirmasoudabdol@icloud.com>
2021-11-25 13:57:25 +0100

    👌🏼 Linking `jaspMetaAnalysis` using PkgConfig to the locally build JAGS!

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -77,53 +77,53 @@ set(CPM_USE_LOCAL_PACKAGES ON)
 # This is rather slow because it has to download all the submodules,
 # when the final version of 1.78.0 is released, we can replace it
 # with the .tar.gz to have a faster download.
-cpmaddpackage(
-  NAME
-  Boost
-  VERSION
-  1.78.0
-  OPTIONS
-  "BUILD_TESTING OFF"
-  GITHUB_REPOSITORY
-  "boostorg/boost"
-  GIT_TAG
-  "boost-1.78.0.beta1")
+# cpmaddpackage(
+#   NAME
+#   Boost
+#   VERSION
+#   1.78.0
+#   OPTIONS
+#   "BUILD_TESTING OFF"
+#   GITHUB_REPOSITORY
+#   "boostorg/boost"
+#   GIT_TAG
+#   "boost-1.78.0.beta1")
 
-cpmaddpackage(
-  NAME
-  jsoncpp
-  VERSION
-  1.9.5
-  OPTIONS
-  "JSONCPP_WITH_TESTS OFF"
-  "JSONCPP_WITH_POST_BUILD_UNITTEST OFF"
-  GITHUB_REPOSITORY
-  "open-source-parsers/jsoncpp"
-  GIT_TAG
-  "1.9.5")
+# cpmaddpackage(
+#   NAME
+#   jsoncpp
+#   VERSION
+#   1.9.5
+#   OPTIONS
+#   "JSONCPP_WITH_TESTS OFF"
+#   "JSONCPP_WITH_POST_BUILD_UNITTEST OFF"
+#   GITHUB_REPOSITORY
+#   "open-source-parsers/jsoncpp"
+#   GIT_TAG
+#   "1.9.5")
 
-cpmaddpackage(
-  NAME
-  LibArchive
-  VERSION
-  3.5.2
-  OPTIONS
-  "ENABLE_TEST OFF"
-  "JSONCPP_WITH_POST_BUILD_UNITTEST OFF"
-  GITHUB_REPOSITORY
-  "libarchive/libarchive"
-  GIT_TAG
-  "v3.5.2")
+# cpmaddpackage(
+#   NAME
+#   LibArchive
+#   VERSION
+#   3.5.2
+#   OPTIONS
+#   "ENABLE_TEST OFF"
+#   "JSONCPP_WITH_POST_BUILD_UNITTEST OFF"
+#   GITHUB_REPOSITORY
+#   "libarchive/libarchive"
+#   GIT_TAG
+#   "v3.5.2")
 
-cpmaddpackage(
-  NAME
-  ZLIB
-  VERSION
-  1.2.11
-  GITHUB_REPOSITORY
-  "madler/zlib"
-  GIT_TAG
-  "v1.2.11")
+# cpmaddpackage(
+#   NAME
+#   ZLIB
+#   VERSION
+#   1.2.11
+#   GITHUB_REPOSITORY
+#   "madler/zlib"
+#   GIT_TAG
+#   "v1.2.11")
 
 find_program(MAKE NAMES gmake nmake make)
 find_program(ACLOCAL NAMES aclocal)
@@ -189,3 +189,4 @@ set(jags_DOWNLOAD_DIR ${DOWNLOAD_DIR})
 set(jags_BUILD_DIR ${jags_DOWNLOAD_DIR}/jags-build)
 set(jags_INCLUDE_DIRS ${jags_DOWNLOAD_DIR}/jags-install/include)
 set(jags_LIBRARY_DIRS ${jags_DOWNLOAD_DIR}/jags-install/lib)
+set(jags_PKG_CONFIG_PATH ${jags_DOWNLOAD_DIR}/jags-install/lib/pkgconfig/)
