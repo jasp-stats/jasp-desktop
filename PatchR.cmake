53eae8131a1b3e70b69590d29919fcbe44953638
Amir Masoud Abdol <amirmasoudabdol@icloud.com>
2021-12-17 14:22:30 +0100

    Patching `opt/R/arm64` in `etc/Markconf`

--- PatchR.cmake
+++ PatchR.cmake
@@ -25,4 +25,13 @@ macro(patch_r r_pkg_r_home)
       "s/LIBR =.*/LIBR = -F$(R_HOME)\\/..\\/..\\/..\\/..\\/ -framework R/g"
       Makeconf)
 
+  # If this fails, I might need to have a quote aruond "$(R_HOME)", 
+  # or the whole line including it. I'm not sure yet.
+  execute_process(
+    WORKING_DIRECTORY ${r_pkg_r_home}/etc
+    COMMAND
+      sed -i.bak -e
+      "s/\\/opt\\/R\\/arm64/$(R_HOME)\\/opt\\/R\\/arm64/g"
+      Makeconf)
+
 endmacro()
