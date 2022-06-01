e978b1cc79a39adfe2122142c08ccec330bdf979
Joris Goosen <Joris@JorisGoosen.nl>
2022-06-01 16:00:03 +0200

    This makes dynamic modules and engines behave themselves much much better (#4726)

--- Common/ipcchannel.cpp
+++ Common/ipcchannel.cpp
@@ -20,6 +20,7 @@
 
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include "log.h"
+#include "utils.h"
 
 using namespace std;
 using namespace boost;
@@ -34,34 +35,98 @@ IPCChannel::IPCChannel(std::string name, size_t channelNumber, bool isSlave)
 	  _channelNumber(	channelNumber								),
 	  _isSlave(			isSlave										)
 {
-	_memoryControl			= new interprocess::managed_shared_memory(interprocess::open_or_create, _baseName.c_str(), 4096);
+	Log::log() << "IPCChannel(" << name << ", " << channelNumber << ", " << (isSlave ? "slave" : "master") << ");" << std::endl;
 
-	_sizeMtoS				= _memoryControl->find_or_construct<size_t>("sizeMasterToSlave")(1024 * 1024 * 8);
-	_sizeStoM				= _memoryControl->find_or_construct<size_t>("sizeSlaveToMaster")(1024 * 1024 * 8);
+	Log::log() << (!_isSlave ? "Creating control memory" : "Opening control memory") << std::endl;
 
-	_memoryMasterToSlave	= new interprocess::managed_shared_memory(interprocess::open_or_create, _nameMtS.c_str(), *_sizeMtoS);
-	_memorySlaveToMaster	= new interprocess::managed_shared_memory(interprocess::open_or_create, _nameStM.c_str(), *_sizeStoM);
+	_memoryControl			= !isSlave ? new interprocess::managed_shared_memory(interprocess::open_or_create,	_baseName.c_str(), 4096)
+									   : new interprocess::managed_shared_memory(interprocess::open_only,		_baseName.c_str());
 
-	TempFiles::addShmemFileName(_baseName);
-	TempFiles::addShmemFileName(_nameMtS);
-	TempFiles::addShmemFileName(_nameStM);
+	if(!_isSlave)
+	{
+		_sizeMtoS				= _memoryControl->find_or_construct<size_t>("sizeMasterToSlave")(1024 * 1024 * 8);
+		_sizeStoM				= _memoryControl->find_or_construct<size_t>("sizeSlaveToMaster")(1024 * 1024 * 8);
+	}
+	else
+		catchAndRepeat("Finding sizes if communication channels", [&]()
+		{
+			auto foundSizeMtoS  = _memoryControl->find<size_t>("sizeMasterToSlave");
+			auto foundSizeStoM  = _memoryControl->find<size_t>("sizeSlaveToMaster");
+
+			if(foundSizeMtoS.first == nullptr)	throw std::runtime_error("Couldn't find sizeMasterToSlave for IPCChannel...");
+			if(foundSizeStoM.first == nullptr)	throw std::runtime_error("Couldn't find sizeSlaveToMaster for IPCChannel...");
+
+			_sizeMtoS = foundSizeMtoS.first;  //It could actually be an array, but of course, this would just take the first one and we ignore the rest.
+			_sizeStoM = foundSizeStoM.first;
+
+			if(foundSizeMtoS.second > 1 || foundSizeStoM.second > 1)
+				Log::log() << "More than 1 (_sizeMtoS: " << foundSizeMtoS.second << " _sizeStoM: " << foundSizeStoM.second << ") data String found in IPCChannel startup on engine." << std::endl;
+		});
+
+	if(!_isSlave)
+	{
+		Log::log() << "Creating communication memory" << std::endl;
+		_memoryMasterToSlave	= new interprocess::managed_shared_memory(interprocess::open_or_create, _nameMtS.c_str(), *_sizeMtoS);
+		_memorySlaveToMaster	= new interprocess::managed_shared_memory(interprocess::open_or_create, _nameStM.c_str(), *_sizeStoM);
+	}
+	else
+		catchAndRepeat("Opening communications memory", [&]()
+		{
+
+			Log::log() << "Opening M->S" << std::endl;
+			_memoryMasterToSlave	= new interprocess::managed_shared_memory(interprocess::open_only, _nameMtS.c_str());
+			Log::log() << "Opening S->M" << std::endl;
+			_memorySlaveToMaster	= new interprocess::managed_shared_memory(interprocess::open_only, _nameStM.c_str());
+		});
+
 
 	generateNames();
 
-	_mutexIn  = _memoryControl->find_or_construct<interprocess::interprocess_mutex>(_mutexInName.c_str())();
-	_mutexOut = _memoryControl->find_or_construct<interprocess::interprocess_mutex>(_mutexOutName.c_str())();
+	Log::log() << "Finding/constructing mutexes" << std::endl;
 
-	_memoryIn  = _isSlave ? _memoryMasterToSlave : _memorySlaveToMaster;
-	_memoryOut = _isSlave ? _memorySlaveToMaster : _memoryMasterToSlave;
+	if(!_isSlave)	findConstructMutexes();
+	else
+		catchAndRepeat("Finding mutexes", [&]()
+		{
+			auto mutexesIn  = _memoryControl->find<interprocess::interprocess_mutex>(_mutexInName.c_str());
+			auto mutexesOut = _memoryControl->find<interprocess::interprocess_mutex>(_mutexOutName.c_str());
 
-	_sizeIn  = _isSlave ? _sizeMtoS : _sizeStoM;
-	_sizeOut = _isSlave ? _sizeStoM : _sizeMtoS;
+			if(mutexesIn.first  == nullptr)	throw std::runtime_error("Couldn't find mutex in for IPCChannel...");
+			if(mutexesOut.first == nullptr)	throw std::runtime_error("Couldn't find mutex out for IPCChannel...");
 
-	_previousSizeIn = *_sizeIn;
+			_mutexIn	= mutexesIn.first;
+			_mutexOut	= mutexesOut.first;
+		});
+
+	_memoryIn	= _isSlave ? _memoryMasterToSlave : _memorySlaveToMaster;
+	_memoryOut	= _isSlave ? _memorySlaveToMaster : _memoryMasterToSlave;
+
+	_sizeIn		= _isSlave ? _sizeMtoS : _sizeStoM;
+	_sizeOut	= _isSlave ? _sizeStoM : _sizeMtoS;
+
+	_previousSizeIn  = *_sizeIn;
 	_previousSizeOut = *_sizeOut;
 
-	_dataIn		= _memoryIn->find_or_construct<String>(_dataInName.c_str())		(_memoryIn->get_segment_manager());
-	_dataOut	= _memoryOut->find_or_construct<String>(_dataOutName.c_str())	(_memoryOut->get_segment_manager());
+	if(!_isSlave)
+		findConstructDataStrings();
+	else
+		catchAndRepeat("Finding communication strings", [&]()
+		{
+			Log::log() << "Opening " << _dataInName << std::endl;
+			auto foundDataIn  = _memoryIn ->find<String>(_dataInName.c_str());
+
+			Log::log() << "Opening " << _dataOutName << std::endl;
+			auto foundDataOut = _memoryOut->find<String>(_dataOutName.c_str());
+
+			if(foundDataIn.first == nullptr)	throw std::runtime_error("Couldn't find data in for IPCChannel...");
+			if(foundDataOut.first == nullptr)	throw std::runtime_error("Couldn't find data out for IPCChannel...");
+
+			_dataIn	 = foundDataIn.first;  //It could actually be an array, but of course, this would just take the first one and we ignore the rest.
+			_dataOut = foundDataOut.first;
+
+			if(foundDataIn.second > 1 || foundDataOut.second > 1)
+				Log::log() << "More than 1 (in: " << foundDataIn.second << " out: " << foundDataOut.second << ") data String found in IPCChannel startup on engine." << std::endl;
+		});
 
 #ifdef __APPLE__
 	_semaphoreIn  = sem_open(_mutexInName.c_str(),  O_CREAT, S_IWUSR | S_IRGRP | S_IROTH, 0);
@@ -112,15 +177,83 @@ IPCChannel::IPCChannel(std::string name, size_t channelNumber, bool isSlave)
 
 
 #endif
+
+	//Log::log() << "IPCChannel init done" << std::endl;
+}
+
+void IPCChannel::findConstructSizes()
+{
+	_sizeMtoS	= _memoryControl->find_or_construct<size_t>("sizeMasterToSlave")(1024 * 1024 * 8);
+	_sizeStoM	= _memoryControl->find_or_construct<size_t>("sizeSlaveToMaster")(1024 * 1024 * 8);
+}
+
+
+void IPCChannel::findConstructMutexes()
+{
+	_mutexIn  = _memoryControl->find_or_construct<interprocess::interprocess_mutex>(_mutexInName.c_str())();
+	_mutexOut = _memoryControl->find_or_construct<interprocess::interprocess_mutex>(_mutexOutName.c_str())();
+}
+
+
+void IPCChannel::findConstructDataStrings()
+{
+	Log::log() << "Finding/constructing communication strings" << std::endl;
+
+	Log::log() << "Creating " << _dataInName << std::endl;
+	_dataIn		= _memoryIn ->find_or_construct<String>(_dataInName.c_str())	(_memoryIn ->get_segment_manager());
+
+	Log::log() << "Creating " << _dataOutName << std::endl;
+	_dataOut	= _memoryOut->find_or_construct<String>(_dataOutName.c_str())	(_memoryOut->get_segment_manager());
+}
+
+void IPCChannel::findConstructAllAgain()
+{
+	Log::log() << "Finding/constructing all relevant shared memory objects again." << std::endl;
+	findConstructSizes();
+	findConstructMutexes();
+	findConstructDataStrings();
+}
+
+void IPCChannel::catchAndRepeat(const std::string & taskDescription, std::function<void()> doThis)
+{
+	const long	now		= Utils::currentMillis(),
+				wait	= 2000,
+				sleep	= 200;
+
+	bool worked = false;
+
+	Log::log() << taskDescription << " for at least " << wait << "ms" << std::endl;
+
+
+	while(!worked && Utils::currentMillis() < now + wait)
+		try
+		{
+			doThis();
+
+			worked = true;
+		}
+		catch(std::exception e)
+		{
+			Log::log() << "Had exception: " << e.what() << std::endl;
+			Log::log() << "Will sleep for " << sleep << "ms and try again." << std::endl;
+
+			Utils::sleep(sleep);
+		}
+
+	if(!worked)
+	{
+		static std::string failure = taskDescription + " failed...";
+		Log::log() << failure << std::endl;
+		throw std::runtime_error(failure);
+	}
 }
 
 IPCChannel::~IPCChannel()
 {
+
 #ifdef JASP_DEBUG
-	Log::log() << "~IPCChannel() of " << (_isSlave ? "Slave" : "Master") << std::endl;
+	Log::log() << "~IPCChannel(#" << _channelNumber << ") of " << (_isSlave ? "Slave" : "Master") << std::endl;
 #endif
-	if(_isSlave)
-		return;
 
 	delete _memoryControl;
 	delete _memoryMasterToSlave;
@@ -129,6 +262,9 @@ IPCChannel::~IPCChannel()
 	_memoryControl			= nullptr;
 	_memoryMasterToSlave	= nullptr;
 	_memorySlaveToMaster	= nullptr;
+
+	if(_isSlave)
+		return;
 
 	interprocess::shared_memory_object::remove(_baseName.c_str());
 	interprocess::shared_memory_object::remove(_nameMtS.c_str());
@@ -241,7 +377,6 @@ retryAfterDoublingMemory:
 
 bool IPCChannel::receive(string &data, int timeout)
 {
-
 	if (tryWait(timeout))
 	{
 		_mutexIn->lock();
