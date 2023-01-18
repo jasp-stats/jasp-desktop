bb3eb21e6b0302afb79ba272dc353c6241ef3be4
boutinb <b.boutin@uva.nl>
2023-01-18 12:27:33 +0100

    JAGS checks too strict
    When running JAGS from the R Script with model
    model{
      theta ~ dbeta(1,1)
    }
    JASP rejects, because the `parameters` parameter is a string instead of an array.
    The check is too strict. Just remove it

--- QMLComponents/boundcontrols/boundcontroljagstextarea.cpp
+++ QMLComponents/boundcontrols/boundcontroljagstextarea.cpp
@@ -50,7 +50,7 @@ bool BoundControlJAGSTextArea::isJsonValid(const Json::Value &value) const
 	if (!value["modelOriginal"].isString())	return false;
 	if (!value["model"].isString())			return false;
 	//if (!value["columns"].isArray())		return false;
-	if (!value["parameters"].isArray())		return false;
+	//if (!value["parameters"].isArray())		return false;
 
 	return true;
 }
