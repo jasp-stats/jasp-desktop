9ae61001b366ed005f87b4ca15fee95ecc3403a0
Joris Goosen <Joris@JorisGoosen.nl>
2023-04-26 15:24:05 +0200

    Keep <sub> et al in the results for https://github.com/jasp-stats/jasp-issues/issues/129 (#5074)
    Also show richtext in column names and data-items

--- Common/stringutils.h
+++ Common/stringutils.h
@@ -19,72 +19,72 @@ typedef std::vector<stringvec>		stringvecvec;
 class stringUtils
 {
 public:    
-    inline static std::string stripRComments(const std::string & rCode)
-    {
-        std::stringstream out;
+	inline static std::string stripRComments(const std::string & rCode)
+	{
+		std::stringstream out;
 
-        //Fixes https://github.com/jasp-stats/INTERNAL-jasp/issues/72
-        //Gotta do some rudimentary parsing here... A comment starts with # and ends with newline, but if a # is inside a string then it doesn't start a comment...
-        //String are started with ' or "
+		//Fixes https://github.com/jasp-stats/INTERNAL-jasp/issues/72
+		//Gotta do some rudimentary parsing here... A comment starts with # and ends with newline, but if a # is inside a string then it doesn't start a comment...
+		//String are started with ' or "
 
-        enum class status { R, Comment, SingleStr, DoubleStr };
+		enum class status { R, Comment, SingleStr, DoubleStr };
 
-        status curStatus = status::R;
+		status curStatus = status::R;
 
-        for(size_t r=0; r<rCode.size(); r++)
-        {
-            bool pushMe = true;
+		for(size_t r=0; r<rCode.size(); r++)
+		{
+			bool pushMe = true;
 
-            char kar = rCode[r];
+			char kar = rCode[r];
 
-            switch(curStatus)
-            {
-            case status::R:
-                switch(kar)
-                {
-                case '\'':	curStatus = status::SingleStr;	break;
-                case '"':	curStatus = status::DoubleStr;	break;
-                case '#':
-                    curStatus	= status::Comment;
-                    pushMe		= false;
-                    break;
-                }
-                break;
+			switch(curStatus)
+			{
+			case status::R:
+				switch(kar)
+				{
+				case '\'':	curStatus = status::SingleStr;	break;
+				case '"':	curStatus = status::DoubleStr;	break;
+				case '#':
+					curStatus	= status::Comment;
+					pushMe		= false;
+					break;
+				}
+				break;
 
-            case status::Comment:
-                if(kar == '\n')	curStatus	= status::R;
-                else			pushMe		= false;
-                break;
+			case status::Comment:
+				if(kar == '\n')	curStatus	= status::R;
+				else			pushMe		= false;
+				break;
 
-            case status::SingleStr:
-                if(kar == '\'' && rCode[r - 1] != '\\')
-                    curStatus = status::R;
-                break;
+			case status::SingleStr:
+				if(kar == '\'' && rCode[r - 1] != '\\')
+					curStatus = status::R;
+				break;
 
-            case status::DoubleStr:
-                if(kar == '"' && rCode[r - 1] != '\\')
-                    curStatus = status::R;
-                break;
-            }
+			case status::DoubleStr:
+				if(kar == '"' && rCode[r - 1] != '\\')
+					curStatus = status::R;
+				break;
+			}
 
-            if(pushMe)
-                out << kar;
-        }
+			if(pushMe)
+				out << kar;
+		}
 
-        return out.str();
-    }
+		return out.str();
+	}
 
-    inline static std::vector<std::string> splitString(const std::string & str, const char sep = ',')
-    {
-        std::vector<std::string>	vecString;
-        std::string					item;
-        std::stringstream			stringStream(str);
+	inline static std::vector<std::string> splitString(const std::string & str, const char sep = ',')
+	{
+		std::vector<std::string>	vecString;
+		std::string					item;
+		std::stringstream			stringStream(str);
 
-        while (std::getline(stringStream, item, sep))
-            vecString.push_back(item);
+		while (std::getline(stringStream, item, sep))
+			vecString.push_back(item);
 
-        return vecString;
-    }
+		return vecString;
+	}
 
 	inline static std::string toLower(std::string input)
 	{
@@ -94,23 +94,26 @@ public:
 
 	inline static std::string replaceBy(std::string input, const std::string & replaceThis, const std::string & withThis)
 	{
-		size_t	oldLen = replaceThis.size(),
-				newLen = withThis.size();
+		size_t	oldLen = replaceThis.size();
 
-		for(std::string::size_type curPos = input.find_first_of(replaceThis); curPos + oldLen < input.size() && curPos != std::string::npos; curPos = input.find_first_of(replaceThis, curPos))
-		{
+		for	(	std::string::size_type curPos = input.find(replaceThis)
+			;	curPos + oldLen <= input.size() && curPos != std::string::npos
+			;	curPos = input.find(replaceThis)	
+		)
 			input.replace(curPos, oldLen, withThis);
-			curPos += newLen;
-		}
 
 		return input;
 	}
 
 	inline static std::string escapeHtmlStuff(std::string input)
 	{
-		input = replaceBy(input, "&", "&amp;");
-		input = replaceBy(input, "<", "&lt;");
-		input = replaceBy(input, ">", "&gt;");
+		input = replaceBy(input, "&", 				"&amp;"	);
+		input = replaceBy(input, "<", 				"&lt;"	);
+		input = replaceBy(input, ">", 				"&gt;"	);
+		input = replaceBy(input,  "&lt;sub&gt;",	"<sub>"	);
+		input = replaceBy(input, "&lt;/sub&gt;",	"</sub>");
+		input = replaceBy(input,  "&lt;sup&gt;",	"<sup>"	);
+		input = replaceBy(input, "&lt;/sup&gt;",	"</sup>");
 
 		return input;
 	}
@@ -196,7 +199,7 @@ public:
 	}
 
 private:
-    stringUtils();
+	stringUtils();
 };
 
 #endif // STRINGUTILS_H
