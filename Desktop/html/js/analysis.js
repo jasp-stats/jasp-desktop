53ce17993c1e2d235bfc2aaeaaa9d20f4a60239d
Shun Wang <shuonwang@gmail.com>
2023-04-26 18:13:53 +0800

    Add inline formula element

--- Desktop/html/js/analysis.js
+++ Desktop/html/js/analysis.js
@@ -664,15 +664,19 @@ JASPWidgets.AnalysisView = JASPWidgets.View.extend({
 		// Config Katex auto-render to render formula in result page(except JASP note)
 		renderMathInElement(this.el, {
 			delimiters: [
-				{left: "$$", right: "$$", display: true},
-				{left: "\\(", right: "\\)", display: false},
-				{left: "\\begin{equation}", right: "\\end{equation}", display: true},
-				{left: "\\begin{align}", right: "\\end{align}", display: true},
-				{left: "\\begin{alignat}", right: "\\end{alignat}", display: true},
-				{left: "\\begin{gather}", right: "\\end{gather}", display: true},
-				{left: "\\begin{CD}", right: "\\end{CD}", display: true},
-				{left: "\\[", right: "\\]", display: true}
+				{left: "$$", 				right: "$$",				display: true},
+				{left: "$", 				right: "$",					display: false},
+				{left: "\\(", 				right: "\\)",				display: false},
+				{left: "\\begin{equation}", right: "\\end{equation}", 	display: true},
+				{left: "\\begin{align}", 	right: "\\end{align}",		display: true},
+				{left: "\\begin{alignat}",	right: "\\end{alignat}",	display: true},
+				{left: "\\begin{gather}",	right: "\\end{gather}",		display: true},
+				{left: "\\begin{CD}",		right: "\\end{CD}",			display: true},
+				{left: "\\begin{table}",	right: "\\end{table}",		display: true},
+				{left: "\\begin{tabular}",	right: "\\end{tabular}",	display: true},
+				{left: "\\[",				right: "\\]",				display: true}
 			  ],
+			output: "htmlAndMathml", // ["html","mathml","htmlAndMathml"(default)]
 			throwOnError : false
 		});
 
