jasp-stats/jasp-desktop

commit 9ae61001b366ed005f87b4ca15fee95ecc3403a0

Keep <sub> et al in the results for https://github.com/jasp-stats/jasp-issues/issues/129 (#5074)
Also show richtext in column names and data-items

Common/stringutils.h
@@ -19,72 +19,72 @@ typedef std::vector<stringvec>		stringvecvec;
 class stringUtils
 {
 public:    
-    inline static std::string stripRComments(const std::string & rCode)
-    {
-        std::stringstream out;
+	inline static std::string stripRComments(const std::string & rCode)
+	{
+		std::stringstream out;
 
-        //Fixes https://github.com/jasp-stats/INTERNAL-jasp/issues/72
-        //Gotta do some rudimentary parsing here... A comment starts with # and ends with newline, but if a # is inside a string then it doesn't start a comment...
-        //String are started with ' or "
+		//Fixes https://github.com/jasp-stats/INTERNAL-jasp/issues/72
+		//Gotta do some rudimentary parsing here... A comment starts with # and ends with newline, but if a # is inside a string then it doesn't start a comment...
+		//String are started with ' or "
 
-        enum class status { R, Comment, SingleStr, DoubleStr };
+		enum class status { R, Comment, SingleStr, DoubleStr };
 
-        status curStatus = status::R;
+		status curStatus = status::R;
 
-        for(size_t r=0; r<rCode.size(); r++)
-        {
-            bool pushMe = true;
+		for(size_t r=0; r<rCode.size(); r++)
+		{
+			bool pushMe = true;
 
-            char kar = rCode[r];
+			char kar = rCode[r];
 
-            switch(curStatus)
-            {
-            case status::R:
-                switch(kar)
-                {
-                case '\'':	curStatus = status::SingleStr;	break;
-                case '"':	curStatus = status::DoubleStr;	break;
-                case '#':
-                    curStatus	= status::Comment;
-                    pushMe		= false;
-                    break;
-                }
-                break;
+			switch(curStatus)
+			{
+			case status::R:
+				switch(kar)
+				{
+				case '\'':	curStatus = status::SingleStr;	break;
+				case '"':	curStatus = status::DoubleStr;	break;
+				case '#':
+					curStatus	= status::Comment;
+					pushMe		= false;
+					break;
+				}
+				break;
 
-            case status::Comment:
-                if(kar == '\n')	curStatus	= status::R;
-                else			pushMe		= false;
-                break;
+			case status::Comment:
+				if(kar == '\n')	curStatus	= status::R;
+				else			pushMe		= false;
+				break;
 
-            case status::SingleStr:
-                if(kar == '\'' && rCode[r - 1] != '\\')
-                    curStatus = status::R;
-                break;
+			case status::SingleStr:
+				if(kar == '\'' && rCode[r - 1] != '\\')
+					curStatus = status::R;
+				break;
 
-            case status::DoubleStr:
-                if(kar == '"' && rCode[r - 1] != '\\')
-                    curStatus = status::R;
-                break;
-            }
+			case status::DoubleStr:
+				if(kar == '"' && rCode[r - 1] != '\\')
+					curStatus = status::R;
+				break;
+			}
 
-            if(pushMe)
-                out << kar;
-        }
+			if(pushMe)
+				out << kar;
+		}
 
-        return out.str();
-    }
+		return out.str();
+	}
 
-    inline static std::vector<std::string> splitString(const std::string & str, const char sep = ',')
-    {
-        std::vector<std::string>	vecString;
-        std::string					item;
-        std::stringstream			stringStream(str);
+	inline static std::vector<std::string> splitString(const std::string & str, const char sep = ',')
+	{
+		std::vector<std::string>	vecString;
+		std::string					item;
+		std::stringstream			stringStream(str);
 
-        while (std::getline(stringStream, item, sep))
-            vecString.push_back(item);
+		while (std::getline(stringStream, item, sep))
+			vecString.push_back(item);
 
-        return vecString;
-    }
+		return vecString;
+	}
 
 	inline static std::string toLower(std::string input)
 	{
@@ -94,23 +94,26 @@ public:
 
 	inline static std::string replaceBy(std::string input, const std::string & replaceThis, const std::string & withThis)
 	{
-		size_t	oldLen = replaceThis.size(),
-				newLen = withThis.size();
+		size_t	oldLen = replaceThis.size();
 
-		for(std::string::size_type curPos = input.find_first_of(replaceThis); curPos + oldLen < input.size() && curPos != std::string::npos; curPos = input.find_first_of(replaceThis, curPos))
-		{
+		for	(	std::string::size_type curPos = input.find(replaceThis)
+			;	curPos + oldLen <= input.size() && curPos != std::string::npos
+			;	curPos = input.find(replaceThis)	
+		)
 			input.replace(curPos, oldLen, withThis);
-			curPos += newLen;
-		}
 
 		return input;
 	}
 
 	inline static std::string escapeHtmlStuff(std::string input)
 	{
-		input = replaceBy(input, "&", "&amp;");
-		input = replaceBy(input, "<", "&lt;");
-		input = replaceBy(input, ">", "&gt;");
+		input = replaceBy(input, "&", 				"&amp;"	);
+		input = replaceBy(input, "<", 				"&lt;"	);
+		input = replaceBy(input, ">", 				"&gt;"	);
+		input = replaceBy(input,  "&lt;sub&gt;",	"<sub>"	);
+		input = replaceBy(input, "&lt;/sub&gt;",	"</sub>");
+		input = replaceBy(input,  "&lt;sup&gt;",	"<sup>"	);
+		input = replaceBy(input, "&lt;/sup&gt;",	"</sup>");
 
 		return input;
 	}
@@ -196,7 +199,7 @@ public:
 	}
 
 private:
-    stringUtils();
+	stringUtils();
 };
 
 #endif // STRINGUTILS_H

Desktop/components/JASP/Widgets/DataTableView.qml
@@ -38,6 +38,7 @@ FocusScope
 				Text
 				{
 					text:				itemText
+					textFormat:			Text.RichText
 					color:				itemActive ? jaspTheme.textEnabled : jaspTheme.textDisabled
 					font:				jaspTheme.font
 					verticalAlignment:	Text.AlignVCenter
@@ -192,6 +193,7 @@ FocusScope
 					text:			headerText
 					font:			jaspTheme.font
 					color:			jaspTheme.textEnabled
+					textFormat:		Text.RichText
 
 					horizontalAlignment:		Text.AlignHCenter
 

commit 2d7c7feb1ecf9fea58af8bfefaea7241325935c0

fixes replaceBy bug I introduced in https://github.com/jasp-stats/jasp-desktop/commit/9ae61001b366ed005f87b4ca15fee95ecc3403a0 Fixes https://github.com/jasp-stats/INTERNAL-jasp/issues/2337
Implements https://github.com/jasp-stats/INTERNAL-jasp/issues/2335 (bold italic in results)

Common/stringutils.h
@@ -8,6 +8,7 @@
 #include <algorithm>
 #include <locale>
 #include <cctype>
+#include <iostream>
 
 typedef std::set<std::string>		stringset;
 typedef std::vector<std::string>	stringvec;
@@ -94,11 +95,14 @@ public:
 
 	inline static std::string replaceBy(std::string input, const std::string & replaceThis, const std::string & withThis)
 	{
-		size_t	oldLen = replaceThis.size();
+		//std::cout << "replaceBy('" << input << "', '" << replaceThis << "', '" << withThis << "');" << std::endl;
+
+		size_t	oldLen = replaceThis.size(),
+				newLen = withThis.size();
 
 		for	(	std::string::size_type curPos = input.find(replaceThis)
 			;	curPos + oldLen <= input.size() && curPos != std::string::npos
-			;	curPos = input.find(replaceThis)	
+			;	curPos = input.find(replaceThis, curPos+newLen)
 		)
 			input.replace(curPos, oldLen, withThis);
 
@@ -107,13 +111,17 @@ public:
 
 	inline static std::string escapeHtmlStuff(std::string input)
 	{
-		input = replaceBy(input, "&", 				"&amp;"	);
-		input = replaceBy(input, "<", 				"&lt;"	);
-		input = replaceBy(input, ">", 				"&gt;"	);
-		input = replaceBy(input,  "&lt;sub&gt;",	"<sub>"	);
-		input = replaceBy(input, "&lt;/sub&gt;",	"</sub>");
-		input = replaceBy(input,  "&lt;sup&gt;",	"<sup>"	);
-		input = replaceBy(input, "&lt;/sup&gt;",	"</sup>");
+		input = replaceBy(input,	"&", 				"&amp;"	);
+		input = replaceBy(input,	"<", 				"&lt;"	);
+		input = replaceBy(input,	">", 				"&gt;"	);
+		input = replaceBy(input,	"&lt;sub&gt;",		"<sub>"	);
+		input = replaceBy(input,	"&lt;/sub&gt;",		"</sub>");
+		input = replaceBy(input,	"&lt;sup&gt;",		"<sup>"	);
+		input = replaceBy(input,	"&lt;/sup&gt;",		"</sup>");
+		input = replaceBy(input,	"&lt;b&gt;",		"<b>"	);
+		input = replaceBy(input,	"&lt;/b&gt;",		"</b>"	);
+		input = replaceBy(input,	"&lt;i&gt;",		"<i>"	);
+		input = replaceBy(input,	"&lt;/i&gt;",		"</i>"	);
 
 		return input;
 	}

Engine/otoolstuff.cpp
@@ -70,20 +70,23 @@ void _moduleLibraryFixer(const std::string & moduleLibraryPath, bool engineCall,
 	try
 	{
 		// Follow symlinks so that we may fix pkgs installed by renv 
-		// (where the actual files are in cacche and only symlinked in library)
+		// (where the actual files are in cache and only symlinked in library)
 		for(recIt dir(modLibpath, std::filesystem::directory_options::follow_directory_symlink); dir != recIt(); dir++)
 		{
 			path = dir->path();
+
+			/*if(printStuff)
+				std::cout << "- Now checking whether the path at '" << path.string() << "' is one we should fix.\n" << std::flush;*/
 	
 			// We only want files that have dylib or so as extension and don't have dSYM 
 			// anywhere in the path (because those are some kind of debugsymbols)
-			if(	! (	std::filesystem::is_regular_file(path)							&&
+			if(	! (	std::filesystem::is_regular_file(path)						&&
 					(path.extension() == ".dylib" || path.extension() == ".so")	&&
 					path.string().find("dSYM") == std::string::npos				))
 				continue;
 	
 			if(printStuff)
-				std::cout << "- Now checking and fixing otool paths for file '" << path.string() << "'.\n";
+				std::cout << "- Now checking and fixing otool paths for file '" << path.string() << "'.\n" << std::flush;
 	
 			std::string libPath		= stringUtils::replaceBy(path.string(), " ", "\\ "),
 						otoolCmd	= "otool -L " + libPath,