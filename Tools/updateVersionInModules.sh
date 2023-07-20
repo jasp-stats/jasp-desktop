19ec91b0f4914b992b45220d2a7e77d26fd220aa
boutinb <b.boutin@uva.nl>
2023-07-20 10:25:50 +0200

    Add script to set version to all modules

---
+++ Tools/updateVersionInModules.sh
@@ -0,0 +1,20 @@
+export VERSION=0.17.3
+
+function setVersion {
+    BRANCH=`git remote show origin | sed -n "/HEAD branch/s/.*: //p"`
+    echo $sm_path
+    echo $BRANCH
+    sed -i '' -e "s/^Version:.*$/Version: ${VERSION}/g" ./DESCRIPTION
+    sed -i '' -e 's/^\tversion.*$/\tversion\t\t\t: "'${VERSION}'"/g' ./inst/Description.qml || true
+    git add ./DESCRIPTION
+    git add ./inst/Description.qml || true
+    git commit --message "Update version to ${VERSION}"
+    git push origin HEAD:${BRANCH}
+}
+
+export -f setVersion
+
+git submodule update --remote
+git submodule foreach setVersion
+
+
