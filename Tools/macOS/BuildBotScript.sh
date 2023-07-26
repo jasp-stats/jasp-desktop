1d2c5aa95907cbf72adc35c239da486fb7d52f0d
RDOFFERHOFF <rdofferhoff@protonmail.com>
2023-07-26 12:07:24 +0200

    Changes for buildbot

--- Tools/macOS/BuildBotScript.sh
+++ Tools/macOS/BuildBotScript.sh
@@ -9,22 +9,12 @@
 # 	for us, just before calling this script.
 # 	- [ ] Enable the arm64 build, when we can, if necessary.
 
-cmake -E remove_directory build-x86_64
-cmake -E make_directory build-x86_64
+cmake -E remove_directory build
+cmake -E make_directory build
 
-cmake -S . -B build-x86_64 -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES=x86_64 -DCMAKE_PREFIX_PATH=/Users/buildbotworker/Qt/6.2.4/macos 
-cmake --build build-x86_64 --target all
-cmake --build build-x86_64 --target install
-cmake --build build-x86_64 --target dmg
-cmake --build build-x86_64 --target notarise
-cmake --build build-x86_64 --target upload
-
-# cmake -E remove_directory build-arm64
-# cmake -E make_directory build-arm64
-
-# cmake -S . -B build-arm64 -GNinja -DUSE_CONAN=ON -DCMAKE_PREFIX_PATH=@Qt6_DIR@ -DCMAKE_OSX_ARCHITECTURES=arm64
-# cmake --build build-arm64 --target JASP
-# cmake --build build-arm64 --target install
-# cmake --build build-arm64 --target dmg
-# cmake --build build-arm64 --target notarise
-# cmake --build build-arm64 --target upload
+cmake -S . -B build -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$QT_DIR
+cmake --build build --target all
+cmake --build build --target install
+cmake --build build --target dmg
+# cmake --build build --target notarise
+# cmake --build build --target upload
