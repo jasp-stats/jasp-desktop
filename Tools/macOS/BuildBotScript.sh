0b9ed098931ecc5337e5fe0e2f79dbb51e4a983b
RensDofferhoff <20978635+RensDofferhoff@users.noreply.github.com>
2023-07-28 15:32:47 +0200

    Update BuildBotScript.sh

--- Tools/macOS/BuildBotScript.sh
+++ Tools/macOS/BuildBotScript.sh
@@ -13,9 +13,10 @@ cd ../../
 
 cmake -E remove_directory build
 cmake -E make_directory build
-
+env
 source ../env
 export PATH="$QT_DIR/../../Tools/Ninja:$PATH"
+env
 
 cmake -S . -B build -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$QT_DIR -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_C_COMPILER=/usr/bin/clang 
 cmake --build build --target all
