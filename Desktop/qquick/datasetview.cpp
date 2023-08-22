8d5597a55db18273bb08aa2dd75819f788996e84
Joris Goosen <Joris@JorisGoosen.nl>
2023-08-22 13:50:29 +0200

    just a small comment for future bughunting

--- Desktop/qquick/datasetview.cpp
+++ Desktop/qquick/datasetview.cpp
@@ -1295,7 +1295,7 @@ QString DataSetView::columnInsertBefore(int col, bool computed, bool R)
 
 	_model->insertColumn(col, computed, R);
 
-	return tq(DataSetPackage::pkg()->dataSet()->column(col)->name());
+	return tq(DataSetPackage::pkg()->dataSet()->column(col)->name()); // This will break as soon as we start filtering/sorting columns
 }
 
 QString DataSetView::columnInsertAfter(int col, bool computed, bool R)
