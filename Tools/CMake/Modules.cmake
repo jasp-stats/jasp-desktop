jasp-stats/jasp-desktop

commit 9f945a983721a0fb1c52d7684c024f61ce6bd50f

jaspColumn improvements (#5302)
* set analysisId to -1 in database on creation
make errormessage from jaspHtml show
use checkForFix branch of jaspBase

* dont just overwrite things willy nilly

doesnt work yet

* show a button to load analysis instead of a disbled dropdown

* add support for creating/adding columns-from-analysis from R

Implements https://github.com/jasp-stats/INTERNAL-jasp/issues/694 (multiple columns from analysis)

see testModule for example

* add jaspRCPP_getColumnExists

* add show analysis button also for not-computed-yet-added-by-analysis columns
do refresh because otherwise columnmodel does not get updated properly

* make sure labels in rightcolumn in variableswindow are spaced properly

* fix alignment again

* fix show parent analysis button for new virtual column

* better missing values and integrate show workspace better into it

* switch to analyses-mode when trying to show the analysis

* The isComputed property does not get always the right value

* Underlined text in button does not scale

---------

Co-authored-by: boutinb <b.boutin@uva.nl>

Tools/CMake/Modules.cmake
@@ -18,7 +18,15 @@
 
 list(APPEND CMAKE_MESSAGE_CONTEXT Modules)
 
-set(JASP_TEST_BUILD OFF CACHE BOOL "Do a quick build with just descriptives and testmodule")
+set(JASP_COMMON_MODULES
+  "jaspDescriptives"
+	"jaspTTests"
+	"jaspAnova"
+	"jaspMixedModels"
+	"jaspRegression"
+	"jaspFrequencies"
+	"jaspFactor"
+)
 
 if(NOT JASP_TEST_BUILD)
 	set(JASP_COMMON_MODULES
@@ -59,12 +67,14 @@ if(NOT JASP_TEST_BUILD)
 		"jaspVisualModeling"
         )
 else() #it IS a test build
+	message(STATUS "JASP_TEST_BUILD is enabled, building with minimal modules")
 	set(JASP_COMMON_MODULES
 		"jaspDescriptives"
+		"jaspDistributions"
 		"jaspTestModule"
 	)
 endif()
-	
+
 list(
   JOIN
   JASP_COMMON_MODULES
@@ -81,8 +91,7 @@ configure_file(${CMAKE_SOURCE_DIR}/Desktop/modules/activemodules.h.in
                ${CMAKE_SOURCE_DIR}/Desktop/modules/activemodules.h @ONLY)
 message(STATUS "activemodules.h is successfully generated...")
 
-if(("jaspMetaAnalysis" IN_LIST JASP_EXTRA_MODULES) OR ("jaspJags" IN_LIST
-                                                       JASP_EXTRA_MODULES))
+if(("jaspMetaAnalysis" IN_LIST JASP_EXTRA_MODULES) OR ("jaspJags" IN_LIST JASP_EXTRA_MODULES))
   if(LINUX)
 
     if(LINUX_LOCAL_BUILD)

CommonData/column.cpp
@@ -6,8 +6,7 @@
 
 Column::Column(DataSet * data, int id) 
 	: DataSetBaseNode(dataSetBaseNodeType::column, data->dataNode()), _data(data), _id(id)
-{
-}
+{}
 
 void Column::dbCreate(int index)
 {
@@ -21,14 +20,15 @@ void Column::dbLoad(int id, bool getValues)
 {
 	JASPTIMER_SCOPE(Column::dbLoad);
 
-	assert(_id != -1 || id != -1);
+	assert(_id == id || (id != -1 && _id == -1) || _id != -1);
+
 	if(id != -1)
 		_id = id;
 
 	db().transactionReadBegin();
 	
 					db().columnGetBasicInfo(	_id, _name, _title, _description, _type, _revision);
-	_isComputed =   db().columnGetComputedInfo(	_id, _invalidated, _codeType, _rCode, _error, _constructorJson);
+	_isComputed =   db().columnGetComputedInfo(	_id, _analysisId, _invalidated, _codeType, _rCode, _error, _constructorJson);
 
 	db().labelsLoad(this);
 	
@@ -45,7 +45,9 @@ void Column::dbLoadIndex(int index, bool getValues)
 {
 	JASPTIMER_SCOPE(Column::dbLoadIndex);
 
-	dbLoad(db().columnIdForIndex(_data->id(), index), getValues);
+	_id = db().columnIdForIndex(_data->id(), index);
+
+	dbLoad(_id, getValues);
 }
 
 void Column::dbDelete(bool cleanUpRest)
@@ -176,7 +178,7 @@ void Column::setCustomEmptyValues(const stringset& customEmptyValues)
 
 void Column::dbUpdateComputedColumnStuff()
 {
-	db().columnSetComputedInfo(_id, _invalidated, _codeType, _rCode, _error, constructorJsonStr());
+	db().columnSetComputedInfo(_id, _analysisId, _isComputed, _invalidated, _codeType, _rCode, _error, constructorJsonStr());
 	incRevision();
 }
 
@@ -205,9 +207,11 @@ void Column::setCodeType(computedColumnType codeType)
 		_constructorJson["formulas"] = Json::arrayValue;
 	}
 
-	_codeType = codeType;
-	_isComputed = _codeType != computedColumnType::notComputed;
+	if(_codeType == computedColumnType::analysisNotComputed)
+		_analysisId = -1;
 
+	_codeType	= codeType;
+	_isComputed = _codeType != computedColumnType::notComputed && _codeType != computedColumnType::analysisNotComputed;
 	
 	dbUpdateComputedColumnStuff();
 }
@@ -275,18 +279,6 @@ void Column::setAnalysisId(int analysisId)
 		return;
 	
 	_analysisId = analysisId;
-	
-	dbUpdateComputedColumnStuff();
-}
-
-void Column::setIsComputed(bool isComputed)
-{
-	JASPTIMER_SCOPE(Column::setIsComputed);
-
-	if(_isComputed == isComputed)
-		return;
-	
-	_isComputed = isComputed;
 	
 	dbUpdateComputedColumnStuff();
 }
@@ -1010,8 +1002,8 @@ bool Column::overwriteDataWithScale(doublevec scalarData)
 
 	size_t setVals = scalarData.size();
 
-	if(scalarData.size() != rowCount())
-		scalarData.resize(rowCount());
+	if(scalarData.size() != _data->rowCount())
+		scalarData.resize(_data->rowCount());
 
 	for(size_t setThis = setVals; setThis<scalarData.size(); setThis++)
 		scalarData[setThis] = static_cast<double>(std::nanf(""));
@@ -1025,8 +1017,8 @@ bool Column::overwriteDataWithOrdinal(intvec ordinalData, intstrmap levels)
 
 	size_t setVals = ordinalData.size();
 
-	if(ordinalData.size() != rowCount())
-		ordinalData.resize(rowCount());
+	if(ordinalData.size() != _data->rowCount())
+		ordinalData.resize(_data->rowCount());
 
 	for(size_t setThis = setVals; setThis<ordinalData.size(); setThis++)
 		ordinalData[setThis] = std::numeric_limits<int>::lowest();
@@ -1040,8 +1032,8 @@ bool Column::overwriteDataWithOrdinal(intvec ordinalData)
 
 	size_t setVals = ordinalData.size();
 
-	if(ordinalData.size() != rowCount())
-		ordinalData.resize(rowCount());
+	if(ordinalData.size() != _data->rowCount())
+		ordinalData.resize(_data->rowCount());
 
 	for(size_t setThis = setVals; setThis<ordinalData.size(); setThis++)
 		ordinalData[setThis] = std::numeric_limits<int>::lowest();
@@ -1055,8 +1047,8 @@ bool Column::overwriteDataWithNominal(intvec nominalData, intstrmap levels)
 
 	size_t setVals = nominalData.size();
 
-	if(nominalData.size() != rowCount())
-		nominalData.resize(rowCount());
+	if(nominalData.size() != _data->rowCount())
+		nominalData.resize(_data->rowCount());
 
 	for(size_t setThis = setVals; setThis<nominalData.size(); setThis++)
 		nominalData[setThis] = std::numeric_limits<int>::lowest();
@@ -1070,8 +1062,8 @@ bool Column::overwriteDataWithNominal(intvec nominalData)
 
 	size_t setVals = nominalData.size();
 
-	if(nominalData.size() != rowCount())
-		nominalData.resize(rowCount());
+	if(nominalData.size() != _data->rowCount())
+		nominalData.resize(_data->rowCount());
 
 	for(size_t setThis = setVals; setThis<nominalData.size(); setThis++)
 		nominalData[setThis] = std::numeric_limits<int>::lowest();
@@ -1083,8 +1075,8 @@ bool Column::overwriteDataWithNominal(stringvec nominalData)
 {
 	JASPTIMER_SCOPE(Column::overwriteDataWithNominal);
 
-	if(nominalData.size() != rowCount())
-		nominalData.resize(rowCount());
+	if(nominalData.size() != _data->rowCount())
+		nominalData.resize(_data->rowCount());
 
 	bool changedSomething = false;
 	setAsNominalText(nominalData, &changedSomething);
@@ -2190,8 +2182,9 @@ void Column::deserialize(const Json::Value &json)
 	_error				= json["error"].asString();
 	_constructorJson	= json["constructorJson"];
 	_isComputed			= json["isComputed"].asBool();
+	_analysisId			= json["analysisId"].asInt();
 
-	db().columnSetComputedInfo(_id, _invalidated, _codeType, _rCode, _error, constructorJsonStr());
+	db().columnSetComputedInfo(_id, _analysisId, _isComputed, _invalidated, _codeType, _rCode, _error, constructorJsonStr());
 
 
 	_dbls.clear();

CommonData/column.h
@@ -48,7 +48,6 @@ public:
 			bool					setConstructorJson(	const std::string & constructorJson	);
 			void					setAnalysisId(		int					analysisId		);
 			void					setInvalidated(		bool				invalidated		);
-			void					setIsComputed(		bool				isComputed		);
 			void					setCompColStuff(bool   invalidated, computedColumnType   codeType, const	std::string & rCode, const	std::string & error, const	Json::Value & constructorJson);
 			void					setDefaultValues(enum columnType columnType = columnType::unknown);
 
@@ -198,8 +197,8 @@ private:
 			columnType				_type				= columnType::unknown,
 									_preEditType		= columnType::unknown;
 			int						_id					= -1,
-									_analysisId			= -1;
-			bool					_isComputed			= false,
+									_analysisId			= -1;		// Actually initialized in DatabaseInterface::columnInsert
+			bool					_isComputed			= false,	// Actually initialized in DatabaseInterface::columnInsert
 									_invalidated		= false,
 									_batchedLabel		= false;
 			computedColumnType		_codeType			= computedColumnType::notComputed;

CommonData/databaseinterface.cpp
@@ -379,7 +379,7 @@ int DatabaseInterface::columnInsert(int dataSetId, int index, const std::string 
 #endif
 
 	//Create column entry
-	int columnId = runStatementsId("INSERT INTO Columns (dataSet, name, columnType, colIdx, isComputed) VALUES (?, ?, ?, ?, 0) RETURNING id;", [&](sqlite3_stmt * stmt)
+	int columnId = runStatementsId("INSERT INTO Columns (dataSet, name, columnType, colIdx, isComputed, analysisId) VALUES (?, ?, ?, ?, 0, -1) RETURNING id;", [&](sqlite3_stmt * stmt)
 	{
 		sqlite3_bind_int(stmt,	1, dataSetId);
 		sqlite3_bind_text(stmt, 2, name.c_str(), name.length(), SQLITE_TRANSIENT);
@@ -1004,20 +1004,22 @@ void DatabaseInterface::columnSetDescription(int columnId, const std::string & d
 	});
 }
 
-void DatabaseInterface::columnSetComputedInfo(int columnId, bool invalidated, computedColumnType codeType, const std::string & rCode, const std::string & error, const std::string & constructorJsonStr)
+void DatabaseInterface::columnSetComputedInfo(int columnId, int analysisId, bool isComputed, bool invalidated, computedColumnType codeType, const std::string & rCode, const std::string & error, const std::string & constructorJsonStr)
 {
 	JASPTIMER_SCOPE(DatabaseInterface::columnSetComputedInfo);
 
-	runStatements("UPDATE Columns SET isComputed=1, invalidated=?, codeType=?, rCode=?, error=?, constructorJson=? WHERE id=?;", [&](sqlite3_stmt * stmt)
+	runStatements("UPDATE Columns SET isComputed=?, invalidated=?, codeType=?, rCode=?, error=?, constructorJson=?, analysisId=? WHERE id=?;", [&](sqlite3_stmt * stmt)
 	{
 		std::string codeT = computedColumnTypeToString(codeType);
 
-		sqlite3_bind_int(stmt,  1, int(invalidated));
-		sqlite3_bind_text(stmt, 2, codeT.c_str(),				codeT.length(),					SQLITE_TRANSIENT);
-		sqlite3_bind_text(stmt, 3, rCode.c_str(),				rCode.length(),					SQLITE_TRANSIENT);
-		sqlite3_bind_text(stmt, 4, error.c_str(),				error.length(),					SQLITE_TRANSIENT);
-		sqlite3_bind_text(stmt, 5, constructorJsonStr.c_str(),	constructorJsonStr.length(),	SQLITE_TRANSIENT);
-		sqlite3_bind_int(stmt,  6, columnId);
+		sqlite3_bind_int(stmt,  1, int(isComputed));
+		sqlite3_bind_int(stmt,  2, int(invalidated));
+		sqlite3_bind_text(stmt, 3, codeT.c_str(),				codeT.length(),					SQLITE_TRANSIENT);
+		sqlite3_bind_text(stmt, 4, rCode.c_str(),				rCode.length(),					SQLITE_TRANSIENT);
+		sqlite3_bind_text(stmt, 5, error.c_str(),				error.length(),					SQLITE_TRANSIENT);
+		sqlite3_bind_text(stmt, 6, constructorJsonStr.c_str(),	constructorJsonStr.length(),	SQLITE_TRANSIENT);
+		sqlite3_bind_int(stmt,  7, analysisId);
+		sqlite3_bind_int(stmt,  8, columnId);
 	});
 }
 
@@ -1055,7 +1057,7 @@ std::string DatabaseInterface::_wrap_sqlite3_column_text(sqlite3_stmt * stmt, in
 	return !col ? "" : std::string(reinterpret_cast<const char*>(col));	
 }
 
-bool DatabaseInterface::columnGetComputedInfo(int columnId, bool &invalidated, computedColumnType &codeType, std::string &rCode, std::string &error, Json::Value &constructorJson)
+bool DatabaseInterface::columnGetComputedInfo(int columnId, int &analysisId, bool &invalidated, computedColumnType &codeType, std::string &rCode, std::string &error, Json::Value &constructorJson)
 {
 	JASPTIMER_SCOPE(DatabaseInterface::columnGetComputedInfo);
 	bool isComputed = false;
@@ -1069,7 +1071,7 @@ bool DatabaseInterface::columnGetComputedInfo(int columnId, bool &invalidated, c
 	{
 		int colCount = sqlite3_column_count(stmt);
 
-		assert(colCount == 6);
+		assert(colCount == 7);
 
 					isComputed			= sqlite3_column_int(		stmt,	0);
 					invalidated			= sqlite3_column_int(		stmt,	1);
@@ -1077,6 +1079,7 @@ bool DatabaseInterface::columnGetComputedInfo(int columnId, bool &invalidated, c
 					rCode				= _wrap_sqlite3_column_text(stmt,	3);
 					error				= _wrap_sqlite3_column_text(stmt,	4);
 		std::string constructorJsonStr	= _wrap_sqlite3_column_text(stmt,	5);
+					analysisId			= sqlite3_column_int(		stmt,	6);
 
 		codeType = codeTypeStr.empty() ? computedColumnType::notComputed : computedColumnTypeFromString(codeTypeStr);
 
@@ -1084,7 +1087,7 @@ bool DatabaseInterface::columnGetComputedInfo(int columnId, bool &invalidated, c
 		Json::Reader().parse(constructorJsonStr, constructorJson);
 	};
 
-	runStatements("SELECT isComputed, invalidated, codeType, rCode, error, constructorJson FROM Columns WHERE id = ?;", prepare, processRow);
+	runStatements("SELECT isComputed, invalidated, codeType, rCode, error, constructorJson, analysisId FROM Columns WHERE id = ?;", prepare, processRow);
 
 	return isComputed;
 }

CommonData/databaseinterface.h
@@ -126,8 +126,8 @@ public:
 	void		columnSetTitle(				int columnId, const std::string & title);
 	void		columnSetDescription(		int columnId, const std::string & description);
 	void		columnGetBasicInfo(			int columnId,		std::string & name, std::string & title, std::string & description, columnType & colType, int & revision);
-	void		columnSetComputedInfo(		int columnId, bool   invalidated, computedColumnType   codeType, const	std::string & rCode, const	std::string & error, const	std::string & constructorJson);
-	bool		columnGetComputedInfo(		int columnId, bool & invalidated, computedColumnType & codeType,		std::string & rCode,		std::string & error,		Json::Value & constructorJson);
+	void		columnSetComputedInfo(		int columnId, int analysisId, bool isComputed, bool   invalidated, computedColumnType   codeType, const	std::string & rCode, const	std::string & error, const	std::string & constructorJson);
+	bool		columnGetComputedInfo(		int columnId, int &analysisId, bool & invalidated, computedColumnType & codeType,		std::string & rCode,		std::string & error,		Json::Value & constructorJson);
 	void		columnSetValues(			int columnId, const intvec	  & ints);
 	void		columnSetValues(			int columnId, const doublevec & dbls);
 	void		columnSetValue(				int columnId, size_t row, int value);

CommonData/dataset.cpp
@@ -146,6 +146,8 @@ Column * DataSet::newColumn(const std::string &name)
 {
 	assert(_dataSetID > 0);
 	Column * col = new Column(this, db().columnInsert(_dataSetID, -1, name));
+	col->setName(name);
+
 	_columns.push_back(col);
 
 	incRevision();

Desktop/components/JASP/Widgets/DataTableView.qml
@@ -685,8 +685,9 @@ FocusScope
 								filterWindow.open()
 							}
                             
-                            if(computedColumnType == computedColumnTypeAnalysis || computedColumnType == computedColumnTypeAnalysisNotComputed)
-                                computedColumnsInterface.showAnalysisFormForColumn(headerText) //headerText should be columnName
+							//A button in VariablesWindow will do this? in any case, it is kind of annoying to have the analysis always pop up instead of variableswindow...
+							//if(computedColumnType == computedColumnTypeAnalysis || computedColumnType == computedColumnTypeAnalysisNotComputed)
+							//    computedColumnsInterface.showAnalysisFormForColumn(headerText) //headerText should be columnName
                         }
 
 					}

Desktop/components/JASP/Widgets/FileMenu/PrefsData.qml
@@ -221,28 +221,6 @@ ScrollView
 					resetButtonTooltip:			qsTr("Reset missing values with the standard JASP missing values")
 					KeyNavigation.tab:			noBomNative
 				}
-
-				Text
-				{
-					visible:			mainWindow.dataAvailable
-					anchors
-					{
-						top:			parent.top
-						left:			missingValuesList.right
-						leftMargin:		jaspTheme.generalAnchorMargin
-						right:			parent.right
-						rightMargin:	jaspTheme.generalAnchorMargin
-					}
-					text:				qsTr("Show workspace missing values")
-					color:				jaspTheme.blueDarker
-					wrapMode:			Text.Wrap
-					MouseArea
-					{
-						anchors.fill:	parent
-						onClicked:		mainWindowRoot.showWorkspaceMenu()
-						cursorShape:	Qt.PointingHandCursor
-					}
-				}
 			}
 
 			PrefsGroupRect

commit 423739a3e249060e027b44904e1028db7b761b8d

JASP_TEST_BUILD should be defined
Fix bug I introduced in https://github.com/jasp-stats/jasp-desktop/commit/9f945a983721a0fb1c52d7684c024f61ce6bd50f

Tools/CMake/Modules.cmake
@@ -18,15 +18,7 @@
 
 list(APPEND CMAKE_MESSAGE_CONTEXT Modules)
 
-set(JASP_COMMON_MODULES
-  "jaspDescriptives"
-	"jaspTTests"
-	"jaspAnova"
-	"jaspMixedModels"
-	"jaspRegression"
-	"jaspFrequencies"
-	"jaspFactor"
-)
+set(JASP_TEST_BUILD OFF CACHE BOOL "Do a quick build with just descriptives and testmodule")
 
 if(NOT JASP_TEST_BUILD)
 	set(JASP_COMMON_MODULES
@@ -70,7 +62,6 @@ else() #it IS a test build
 	message(STATUS "JASP_TEST_BUILD is enabled, building with minimal modules")
 	set(JASP_COMMON_MODULES
 		"jaspDescriptives"
-		"jaspDistributions"
 		"jaspTestModule"
 	)
 endif()