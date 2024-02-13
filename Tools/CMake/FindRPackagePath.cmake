83074cc6312dfa96c9e4f91be904f938614ef3d8
Joris Goosen <joris@jorisgoosen.nl>
2024-02-13 17:30:12 +0100

    make it build on windows again

--- Tools/CMake/FindRPackagePath.cmake
+++ Tools/CMake/FindRPackagePath.cmake
@@ -1,6 +1,14 @@
 
 # Locate the full paths (so those in the cache, not the symlinks)
-set(FIND_PACKAGE_PATH_SCRIPT		${PROJECT_SOURCE_DIR}/Tools/find_package_path.R)
+
+#Argh
+if("${CMAKE_PROJECT_NAME}" STREQUAL "R-Interface")
+	set(FIND_PACKAGE_PATH_SCRIPT		${CMAKE_SOURCE_DIR}/../Tools/find_package_path.R)
+else()
+
+	set(FIND_PACKAGE_PATH_SCRIPT		${CMAKE_SOURCE_DIR}/Tools/find_package_path.R)
+endif()
+
 macro(find_package_path out libPath packageName)
 	#We are not using Rscript anywhere in CMake because that doesn't work on macOs
 	execute_process(
@@ -8,6 +16,7 @@ macro(find_package_path out libPath packageName)
 		OUTPUT_VARIABLE TEMP_OUT
 		COMMAND_ECHO STDOUT)
 	#R could be printing all sorts of bullshit, so instead we simply take the last line of output
+	message(STATUS "TEMP_OUT is ${TEMP_OUT}")
 	string(REGEX MATCHALL "[^\n\r]+" TEMPLINES ${TEMP_OUT})
 	list(GET TEMPLINES -1 ${out})
 endmacro()
