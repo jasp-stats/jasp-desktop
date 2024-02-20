jasp-stats/jasp-desktop

commit e2328faf30655b65e5fbae1c06de39269ae535ac

Initialize controls only if their dependencies are already initialized (#5371)
* Initialize controls only if their dependencies are already initialized

* Remove ugly code

The Initialization of the available variablesList was quite ugly: a long comment in the bindTo method of the AnalysisForm object tried to explain how this initialization was done, but with this PR, I try to do it in the 'right way'.
The available VariablesLists have no bound value (they don't give values for the analysis), but they must be initialized. For this, as the setInitialized is called even for these controls, just override this method in VaraibelListBase, so that:
. when it has an avalable list model, then it should get its values from its sources.
. when it has an assigned list model, handle the particular case when the addAvailableVariablesToAssigned prperty is true, and it is initialized with no value, then it should take the values of its available list model (which might have default values).

* add context object "this" for lambda to avoid random crashes in the future

QMLComponents/controls/variableslistbase.cpp
@@ -52,7 +52,7 @@ void VariablesListBase::setUp()
 	{
 		for (SourceItem* sourceItem : _sourceItems)
 		{
-			ListModelFactorLevels* factorsModel = dynamic_cast<ListModelFactorLevels*>(sourceItem->listModel());
+			ListModelFactorLevels* factorsModel = dynamic_cast<ListModelFactorLevels*>(sourceItem->sourceListModel());
 			if (!factorsModel)
 				addControlError(tr("Source model of %1 must be from a Factor List").arg(name()));
 			else
@@ -89,6 +89,22 @@ void VariablesListBase::setUp()
 	connect(this,	&VariablesListBase::suggestedColumnsChanged,					this, &VariablesListBase::_setAllowedVariables);
 }
 
+void VariablesListBase::_setInitialized(const Json::Value &value)
+{
+	ListModelAvailableInterface* availableModel = qobject_cast<ListModelAvailableInterface*>(_draggableModel);
+	if (availableModel)
+		availableModel->resetTermsFromSources(false);
+	else if (value == Json::nullValue && addAvailableVariablesToAssigned())
+	{
+		// If addAvailableVariablesToAssigned is true and this is initialized without value,
+		// maybe the availableAssignedList has some default values that must be assigned to this VariablesList
+		ListModelAssignedInterface* assignedModel = qobject_cast<ListModelAssignedInterface*>(_draggableModel);
+		if (assignedModel)
+			assignedModel->initTerms(assignedModel->availableModel()->terms());
+	}
+
+	JASPListControl::_setInitialized(value);
+}
 
 
 ListModel *VariablesListBase::model() const

QMLComponents/models/listmodelinteractionassigned.cpp
@@ -20,6 +20,7 @@
 #include "utilities/qutils.h"
 #include "listmodeltermsavailable.h"
 #include "listmodeltermsassigned.h"
+#include "controls/jasplistcontrol.h"
 
 using namespace std;
 
@@ -151,7 +152,7 @@ void ListModelInteractionAssigned::_addTerms(const Terms& terms, bool combineWit
 
 void ListModelInteractionAssigned::availableTermsResetHandler(Terms termsAdded, Terms termsRemoved)
 {
-	if (termsAdded.size() > 0 && _addNewAvailableTermsToAssignedModel)
+	if (termsAdded.size() > 0 && listView()->addAvailableVariablesToAssigned())
 	{
 		_addTerms(termsAdded, _addInteractionsByDefault);
 		setTerms();

QMLComponents/models/listmodeltermsassigned.cpp
@@ -44,7 +44,7 @@ void ListModelTermsAssigned::initTerms(const Terms &terms, const RowControlsValu
 
 void ListModelTermsAssigned::availableTermsResetHandler(Terms termsAdded, Terms termsRemoved)
 {
-	if (termsAdded.size() > 0 && _addNewAvailableTermsToAssignedModel)
+	if (termsAdded.size() > 0 && listView()->addAvailableVariablesToAssigned())
 	{
 		beginResetModel();
 		_addTerms(termsAdded);

QMLComponents/analysisform.cpp
@@ -275,8 +275,6 @@ void AnalysisForm::_setUp()
 
 	for (JASPControl* control : controls)
 	{
-		BoundControl* boundControl = control->boundControl();
-		if (boundControl) boundControl->setDefaultBoundValue(boundControl->createJson()); // The default value is known only when all controls are setup
 		_dependsOrderedCtrls.push_back(control);
 		connect(control, &JASPControl::helpMDChanged, this, &AnalysisForm::helpMDChanged);
 	}
@@ -357,38 +355,17 @@ void AnalysisForm::_addLoadingError(QStringList wrongJson)
 
 void AnalysisForm::bindTo(const Json::Value & defaultOptions)
 {
-	QVector<ListModelAvailableInterface*> availableModelsToBeReset;
-
 	std::set<std::string> controlsJsonWrong;
 	
 	for (JASPControl* control : _dependsOrderedCtrls)
 	{
-		bool hasOption = false;
 		BoundControl* boundControl = control->boundControl();
-		JASPListControl* listControl = dynamic_cast<JASPListControl *>(control);
-
-		if (listControl && listControl->hasSource())
-		{
-			ListModelAvailableInterface* availableModel = qobject_cast<ListModelAvailableInterface*>(listControl->model());
-			// The availableList control are not bound with options, but they have to be updated from their sources when the form is initialized.
-			// The availableList cannot signal their assigned models now because they are not yet bound (the controls are ordered by dependency)
-			// When the options come from a JASP file, an assigned model needs sometimes the available model (eg. to determine the kind of terms they have).
-			// So in this case resetTermsFromSourceModels has to be called now but with updateAssigned argument set to false.
-			// When the options come from default options (from source models), the availableList needs sometimes to signal their assigned models (e.g. when addAvailableVariablesToAssigned if true).
-			// As their assigned models are not yet bound, resetTermsFromSourceModels (with updateAssigned argument set to true) must be called afterwards.
-			if (availableModel)
-			{
-				if (defaultOptions.size() != 0 || _analysis->isDuplicate())
-					availableModel->resetTermsFromSources(false);
-				else
-					availableModelsToBeReset.push_back(availableModel);
-			}
-		}
-
+		Json::Value optionValue = Json::nullValue;
 		if (boundControl)
 		{
 			std::string name = control->name().toStdString();
-			Json::Value optionValue =  defaultOptions.size() != 0 ? defaultOptions[name] : Json::nullValue;
+			if (defaultOptions.isMember(name))
+				optionValue = defaultOptions[name];
 
 			if (optionValue != Json::nullValue && !boundControl->isJsonValid(optionValue))
 			{
@@ -396,20 +373,10 @@ void AnalysisForm::bindTo(const Json::Value & defaultOptions)
 				control->setHasWarning(true);
 				controlsJsonWrong.insert(name);
 			}
-
-			if (optionValue == Json::nullValue)
-				optionValue = boundControl->createJson();
-			else
-				hasOption = true;
-
-			boundControl->bindTo(optionValue);
 		}
 
-		control->setInitialized(hasOption);
+		control->setInitialized(optionValue);
 	}
-
-	for (ListModelAvailableInterface* availableModel : availableModelsToBeReset)
-		availableModel->resetTermsFromSources(true);
 
 	_addLoadingError(tql(controlsJsonWrong));
 

QMLComponents/components/JASP/Controls/VariablesList.qml
@@ -24,14 +24,16 @@ import JASP				1.0
 
 VariablesListBase
 {
-	id						: variablesList
-	implicitHeight			: maxRows === 1 ? jaspTheme.defaultSingleItemListHeight : jaspTheme.defaultVariablesFormHeight
-	background				: itemRectangle
-	implicitWidth 			: parent.width
-	shouldStealHover		: false
-	innerControl			: itemGridView
-	optionKey				: listViewType === JASP.Interaction ? "components" : "variable"
-	maxRows					: singleVariable ? 1 : -1
+	id								: variablesList
+	implicitHeight					: maxRows === 1 ? jaspTheme.defaultSingleItemListHeight : jaspTheme.defaultVariablesFormHeight
+	background						: itemRectangle
+	implicitWidth					: parent.width
+	shouldStealHover				: false
+	innerControl					: itemGridView
+	optionKey						: listViewType === JASP.Interaction ? "components" : "variable"
+	maxRows							: singleVariable ? 1 : -1
+	addAvailableVariablesToAssigned	: listViewType === JASP.Interaction
+	allowAnalysisOwnComputedColumns	: true
 
 	property alias	label							: variablesList.title
 	property alias	itemGridView					: itemGridView
@@ -53,8 +55,6 @@ VariablesListBase
 	property bool	showVariableTypeIcon			: containsVariables
 	property bool	addInteractionsByDefault		: true
 	property bool	interactionContainLowerTerms	: true
-	property bool	addAvailableVariablesToAssigned	: listViewType === JASP.Interaction
-	property bool	allowAnalysisOwnComputedColumns	: true
 	property bool	allowDuplicatesInMultipleColumns: false // This property is used in the constructor and is not updatable afterwards.
 
 	property int	indexInDroppedListViewOfDraggedItem:	-1

QMLComponents/controls/comboboxbase.cpp
@@ -30,7 +30,7 @@ ComboBoxBase::ComboBoxBase(QQuickItem* parent)
 
 void ComboBoxBase::bindTo(const Json::Value& value)
 {
-	// Do not call BoundControlBase::bindTo now because the value might be changed to the default value.
+	_model->resetTermsFromSources();
 
 	std::vector<std::string> values = _model->getValues();
 	std::string selectedValue = value.asString();

QMLComponents/controls/jaspcontrol.cpp
@@ -157,7 +157,7 @@ void JASPControl::_resetBindingValue()
 	// If a control gets a value from a JASP file, this value may differ from its default value sets by a QML binding:
 	// this QML binding may then change the value during the initialization of the form.
 	// In this case, restore the original value.
-	if (isBound() && hasUserInteractiveValue() && initializedFromJaspFile() && form() && !form()->initialized())
+	if (isBound() && hasUserInteractiveValue() && initializedWithValue() && form() && !form()->initialized())
 		boundControl()->resetBoundValue();
 }
 
@@ -742,16 +742,6 @@ QString JASPControl::humanFriendlyLabel() const
 
 	return label;
 
-		}
-
-void JASPControl::setInitialized(bool byFile)
-{
-	if (!_initialized)
-	{
-		_initialized = true;
-		_initializedFromJaspFile = byFile;
-		emit initializedChanged();
-	}
 }
 
 QVector<JASPControl::ParentKey> JASPControl::getParentKeys()
@@ -820,4 +810,42 @@ void JASPControl::_addExplicitDependency(const QVariant& depends)
 void JASPControl::addExplicitDependency()
 {
 	_addExplicitDependency(_explicitDepends);
+}
+
+bool JASPControl::dependingControlsAreInitialized()
+{
+	bool dependenciesAreInitialized = true;
+
+	for (JASPControl* c : _depends)
+	{
+		if (!c->initialized())
+			dependenciesAreInitialized = false;
+	}
+	return dependenciesAreInitialized;
+}
+
+void JASPControl::setInitialized(const Json::Value &value)
+{
+	if (dependingControlsAreInitialized())
+		_setInitialized(value);
+	else
+	{
+		for (JASPControl* c : _depends)
+			if (!c->initialized())
+				connect(c, &JASPControl::initializedChanged, this, [this, value]() { if (dependingControlsAreInitialized()) _setInitialized(value); });
+	}
+}
+
+void JASPControl::_setInitialized(const Json::Value &value)
+{
+	BoundControl* bControl = boundControl();
+	if (bControl)
+	{
+		bControl->setDefaultBoundValue(bControl->createJson());
+		bControl->bindTo(value == Json::nullValue ? bControl->createJson() : value);
+	}
+
+	_initialized = true;
+	_initializedWithValue = (value != Json::nullValue);
+	emit initializedChanged();
 }

QMLComponents/controls/jaspcontrol.h
@@ -120,7 +120,7 @@ public:
 	bool				indent()					const	{ return _indent;					}
 	bool				isDependency()				const	{ return _isDependency;				}
 	bool				initialized()				const	{ return _initialized;				}
-	bool				initializedFromJaspFile()	const	{ return _initializedFromJaspFile;	}
+	bool				initializedWithValue()		const	{ return _initializedWithValue;		}
 	bool				shouldShowFocus()			const	{ return _shouldShowFocus;			}
 	bool				shouldStealHover()			const	{ return _shouldStealHover;			}
 	bool				debug()						const	{ return _debug;					}
@@ -151,7 +151,6 @@ public:
 	const QVariant&		explicitDepends()			const	{ return _explicitDepends;			}
 
 	QString				humanFriendlyLabel()		const;
-	void				setInitialized(bool byFile = false);
 
 	QVector<JASPControl::ParentKey>	getParentKeys();
 
@@ -159,6 +158,7 @@ public:
 	static QList<JASPControl*>		getChildJASPControls(const QQuickItem* item);
 
 	virtual void					setUp()										{}
+	void							setInitialized(const Json::Value& value = Json::nullValue);
 	virtual void					cleanUp()									{ disconnect(); }
 	virtual BoundControl		*	boundControl();
 	virtual bool					encodeValue()						const	{ return false; }
@@ -172,8 +172,6 @@ public:
 
 	virtual QString					friendlyName() const;
 	void							addExplicitDependency();
-protected:
-	Set								_depends;
 
 public slots:
 	void	setControlType(			ControlType			controlType)		{ _controlType = controlType; }
@@ -266,8 +264,11 @@ protected:
 	bool				eventFilter(QObject *watched, QEvent *event) override;
 	bool				checkOptionName(const QString& name);
 	void				_addExplicitDependency(const QVariant& depends);
+	bool				dependingControlsAreInitialized();
+	virtual void		_setInitialized(const Json::Value &value);
 
 protected:
+	Set						_depends;
 	ControlType				_controlType;
 	AnalysisForm*			_form						= nullptr;
 	QString					_name,
@@ -278,7 +279,7 @@ protected:
 	bool					_isBound					= true,
 							_indent						= false,
 							_initialized				= false,
-							_initializedFromJaspFile	= false,
+							_initializedWithValue	= false,
 							_debug						= false,
 							_parentDebug				= false,
 							_hasError					= false,

commit 24149dd4379e8d5d3a40a99ac05315710d34b7f5

Network analysis crahses
This bug was introduced by https://github.com/jasp-stats/jasp-desktop/pull/5371
Not all assignedModels have a availableModels: this is the case when a AssignedVariablesList has a source (and is not in a VariablesForm).

QMLComponents/controls/variableslistbase.cpp
@@ -99,7 +99,7 @@ void VariablesListBase::_setInitialized(const Json::Value &value)
 		// If addAvailableVariablesToAssigned is true and this is initialized without value,
 		// maybe the availableAssignedList has some default values that must be assigned to this VariablesList
 		ListModelAssignedInterface* assignedModel = qobject_cast<ListModelAssignedInterface*>(_draggableModel);
-		if (assignedModel)
+		if (assignedModel && assignedModel->availableModel())
 			assignedModel->initTerms(assignedModel->availableModel()->terms());
 	}
 

QMLComponents/models/listmodelinteractionassigned.cpp
@@ -197,7 +197,8 @@ Terms ListModelInteractionAssigned::addTerms(const Terms& terms, int , const Row
 		return Terms();
 	
 	Terms dropped;
-	dropped.setSortParent(availableModel()->allTerms());
+	if (availableModel())
+		dropped.setSortParent(availableModel()->allTerms());
 	dropped.set(terms);
 
 	Terms newTerms = dropped.combineTerms(JASPControl::CombinationType::CombinationCross);

QMLComponents/models/listmodeltermsassigned.cpp
@@ -50,7 +50,7 @@ void ListModelTermsAssigned::availableTermsResetHandler(Terms termsAdded, Terms 
 		_addTerms(termsAdded);
 		endResetModel();
 
-		if (!_copyTermsWhenDropped)
+		if (!_copyTermsWhenDropped && availableModel())
 			availableModel()->removeTermsInAssignedList();
 	}
 