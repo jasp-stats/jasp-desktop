304a3fbf71f31f2f9fbce7f273600be32b7d8359
RensDofferhoff <rdofferhoff@protonmail.com>
2024-03-12 22:16:06 +0100

    progress

--- Desktop/utilities/processhelper.h
+++ Desktop/utilities/processhelper.h
@@ -10,7 +10,7 @@ class ProcessHelper
 {
 public:
 	
-    static QProcessEnvironment getProcessEnvironmentForJaspEngine();
+    static QProcessEnvironment getProcessEnvironmentForJaspEngine(bool bootStrap = false);
 	
 private:
 	ProcessHelper(){}
