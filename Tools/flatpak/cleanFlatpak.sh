15b1913c69ac0f4094e9b6f231999b397a655eed
RensDofferhoff <20978635+RensDofferhoff@users.noreply.github.com>
2024-06-07 10:14:32 +0200

    Update cleanFlatpak.sh

--- Tools/flatpak/cleanFlatpak.sh
+++ Tools/flatpak/cleanFlatpak.sh
@@ -2,6 +2,6 @@
 echo "Cleaning up flatpak"
 pwd
 #cant delete read only renv cache stuff. very annoying
-chmod -R +w ./ 
+chmod -R +w _cache/
 #Cant use the cellar post build.
 rm -rf /app/lib64/cellar/
