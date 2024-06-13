1b8538ff8c44e016ffce63e59823ed6d5db94033
RensDofferhoff <20978635+RensDofferhoff@users.noreply.github.com>
2024-06-13 15:25:10 +0200

    Update main.cpp

--- Desktop/main.cpp
+++ Desktop/main.cpp
@@ -65,7 +65,6 @@ bool runJaspEngineJunctionFixer(int argc, char *argv[], bool removeJunctions = f
 	//remove any leftover ModuleDir 
 	QDir modulesDir(AppDirs::bundledModulesDir());
 	if(modulesDir.exists() && AppDirs::bundledModulesDir().contains("Modules", Qt::CaseInsensitive) && DynamicRuntimeInfo::getInstance()->getRuntimeEnvironment() != DynamicRuntimeInfo::ZIP)	{
-	{
 		std::function<void(QDir)> removeDir = [&](QDir x) -> void {
 			for(const auto& entry : x.entryInfoList(QDir::NoDotAndDotDot | QDir::Dirs | QDir::Files))
 			{
