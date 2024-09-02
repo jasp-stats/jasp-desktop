02c51652cb5fd0e651c5e81806b3d9e523e9bb5d
bruno boutin <b.boutin@uva.nl>
2024-09-02 14:14:16 +0200

    Create checkPOFiles.sh

---
+++ Tools/checkPOFiles.sh
@@ -0,0 +1,17 @@
+function checkPO {
+    for LANGUAGE in cs de eo es eu fr gl id ja nl pl pt_BR pt ru zh_Hans hs_Hant
+    do
+        PO_FILE="po/QML-${LANGUAGE}.po"
+        if [ -e ${PO_FILE} ]
+        then
+            if ! grep X-Language ${PO_FILE} >> /dev/null 2>&1
+            then
+                echo "No X-Language for ${LANGUAGE}"
+            fi
+        fi
+    done
+}
+
+export -f checkPO
+
+git submodule foreach checkPO
