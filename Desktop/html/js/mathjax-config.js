41f7120253c8507621a51deb5fc83327de5fcf0f
Shun Wang <shuonwang@gmail.com>
2025-01-21 11:56:58 +0800

    bugfix: tweaks for math expressions in jaspNotes

---
+++ Desktop/html/js/mathjax-config.js
@@ -0,0 +1,10 @@
+MathJax = {
+    options: {
+      menuOptions: {
+        settings: {
+          assistiveMml: false,   // Disable assistive MathML to avoid duplicated copy-past in notes.
+        }
+      }
+    }
+  };
+  
