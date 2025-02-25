b042dd65b4b444c8ad89421b05e78c2422a2460d
Joris Goosen <joris@jorisgoosen.nl>
2025-02-25 16:08:10 +0100

    spacing and formatting of strings

--- QMLComponents/boundcontrols/boundcontrolrlangtextarea.cpp
+++ QMLComponents/boundcontrols/boundcontrolrlangtextarea.cpp
@@ -81,8 +81,9 @@ void BoundControlRlangTextArea::checkSyntax()
 	_prefixedUsedColumnNames.clear();
 	_textEncoded = tq(ColumnEncoder::columnEncoder()->encodeRScript(stringUtils::stripRComments(fq(text)), _prefixedUsedColumnNames, _allowedVarPrefixes));
 
-	if(_prefixedUsedColumnNames.find("") != _prefixedUsedColumnNames.end()) {
-		_noPrefixUsedColumnNames = std::set<std::string>(_prefixedUsedColumnNames[""]);
+	if(_prefixedUsedColumnNames.find("") != _prefixedUsedColumnNames.end()) 
+	{
+		_noPrefixUsedColumnNames = stringset(_prefixedUsedColumnNames[""]);
 		_prefixedUsedColumnNames.erase(_prefixedUsedColumnNames.find("")); //just for clarity remove the noPrefix "" items
 	}
 	else
@@ -103,15 +104,15 @@ void BoundControlRlangTextArea::checkSyntax()
 			encodedColNames.append(", ");
 		}
 	encodedColNames.chop(2); //remove ', '
-	if(encodedColNames.length() > 0) encodedColNames.append(")");
+	
+	if(encodedColNames.length() > 0) 
+		encodedColNames.append(")");
 
 	if(_textEncoded.length() > 0) {
-		QString checkCode = tq(_checkSyntaxRFunctionName())
-			.append("('")
-			.append(_textEncoded)
-			.append("', ")
-			.append(encodedColNames)
-			.append(")");
+		QString checkCode = QString("%1('%2', %3)")
+			.arg(tq(_checkSyntaxRFunctionName()))
+			.arg(_textEncoded)
+			.arg(encodedColNames);
 
 		_textArea->runRScript(checkCode, false);
 	}
@@ -136,7 +137,8 @@ QString BoundControlRlangTextArea::rScriptDoneHandler(const QString & result)
 
 	Json::Value prefixedColumns(Json::objectValue);
 	for(auto& prefixSet : _prefixedUsedColumnNames)
-		for (const std::string& column : prefixSet.second) {
+		for (const std::string& column : prefixSet.second) 
+		{
 			prefixedColumns[prefixSet.first] = Json::Value(Json::arrayValue);
 			prefixedColumns[prefixSet.first].append(ColumnEncoder::columnEncoder()->encode(column));
 		}
