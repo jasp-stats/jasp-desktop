aaf4d9121a753f696640600d5f8967ea0368ff01
Rens <rdofferhoff@protonmail.com>
2025-04-14 20:39:30 +0200

    fix arm64 gfortran

--- Tools/CMake/Patch.cmake
+++ Tools/CMake/Patch.cmake
@@ -357,29 +357,9 @@ else()
           ERROR_QUIET OUTPUT_QUIET
           WORKING_DIRECTORY ${PATH}
           COMMAND
-            install_name_tool -change
-            "${R_HOME_PATH}/opt/R/arm64/gfortran/lib/libgfortran.dylib"
-            "${FRAMEWORK_RESOURCES}/lib/libgfortran.5.dylib"
-            "${FILE}")
-
-        execute_process(
-          # COMMAND_ECHO STDOUT
-          ERROR_QUIET OUTPUT_QUIET
-          WORKING_DIRECTORY ${PATH}
-          COMMAND
             bash ${NAME_TOOL_PREFIX_PATCHER} "${FILE}"
-            "/opt/R/arm64/gfortran/lib"
-            "${FRAMEWORK_RESOURCES}/opt/R/arm64/gfortran/lib"
-        )
-
-        execute_process(
-          # COMMAND_ECHO STDOUT
-          ERROR_QUIET OUTPUT_QUIET
-          WORKING_DIRECTORY ${PATH}
-          COMMAND
-            bash ${NAME_TOOL_PREFIX_PATCHER} "${FILE}"
-            "${R_HOME_PATH}/opt/R/arm64/gfortran/lib"
-            "${FRAMEWORK_RESOURCES}/opt/R/arm64/gfortran/lib"
+            "/opt/gfortran/lib/gcc/aarch64-apple-darwin20.0/14.2.0"
+            "${FRAMEWORK_RESOURCES}/opt/R/arm64/gfortran/lib/gcc/aarch64-apple-darwin20.0/14.2.0"
         )
 
       else()
@@ -389,29 +369,9 @@ else()
           ERROR_QUIET OUTPUT_QUIET
           WORKING_DIRECTORY ${PATH}
           COMMAND
-            install_name_tool -change
-            "${R_HOME_PATH}/opt/local/gfortran/lib/libgfortran.dylib"
-            "${FRAMEWORK_RESOURCES}/lib/libgfortran.5.dylib"
-            "${FILE}")
-
-        execute_process(
-          # COMMAND_ECHO STDOUT
-          ERROR_QUIET OUTPUT_QUIET
-          WORKING_DIRECTORY ${PATH}
-          COMMAND
-            install_name_tool -change
-            "${R_HOME_PATH}/opt/local/gfortran/lib/libquadmath.dylib"
-            "${FRAMEWORK_RESOURCES}/lib/libquadmath.0.dylib"
-            "${FILE}")
-
-        execute_process(
-          # COMMAND_ECHO STDOUT
-          ERROR_QUIET OUTPUT_QUIET
-          WORKING_DIRECTORY ${PATH}
-          COMMAND
             bash ${NAME_TOOL_PREFIX_PATCHER} "${FILE}"
-            "/usr/local/gfortran/lib"
-            "${FRAMEWORK_RESOURCES}/opt/local/gfortran/lib"
+            "/opt/gfortran/lib/gcc/x86_64-apple-darwin20.0/14.2.0"
+            "${FRAMEWORK_RESOURCES}/opt/R/arm64/gfortran/lib/gcc/x86_64-apple-darwin20.0/14.2.0"
         )
         # For whatever reason, the above command cannot replace the prefix of these libraries. I have tried to
         # directly changed their 'id' even, and that didn't help either!
