db4ebaf2d2291cc93e9623db4a601b5df3bb4367
RensDofferhoff <20978635+RensDofferhoff@users.noreply.github.com>
2025-04-14 23:13:21 +0200

    Update Patch.cmake

--- Tools/CMake/Patch.cmake
+++ Tools/CMake/Patch.cmake
@@ -358,8 +358,8 @@ else()
           WORKING_DIRECTORY ${PATH}
           COMMAND
             bash ${NAME_TOOL_PREFIX_PATCHER} "${FILE}"
-            "/opt/gfortran/lib/gcc/aarch64-apple-darwin20.0/14.2.0"
-            "${FRAMEWORK_RESOURCES}/opt/R/arm64/gfortran/lib/gcc/aarch64-apple-darwin20.0/14.2.0"
+            "/opt/gfortran/lib"
+            "${FRAMEWORK_RESOURCES}/opt/R/arm64/gfortran/lib"
         )
 
       else()
@@ -370,8 +370,8 @@ else()
           WORKING_DIRECTORY ${PATH}
           COMMAND
             bash ${NAME_TOOL_PREFIX_PATCHER} "${FILE}"
-            "/opt/gfortran/lib/gcc/x86_64-apple-darwin20.0/14.2.0"
-            "${FRAMEWORK_RESOURCES}/opt/R/x86_64/gfortran/lib/gcc/x86_64-apple-darwin20.0/14.2.0"
+            "/opt/gfortran/lib"
+            "${FRAMEWORK_RESOURCES}/opt/R/x86_64/gfortran/lib"
         )
         # For whatever reason, the above command cannot replace the prefix of these libraries. I have tried to
         # directly changed their 'id' even, and that didn't help either!
