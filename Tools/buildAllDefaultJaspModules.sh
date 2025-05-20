812b550c0f2e4c72c7d658cb5fea64d95a40287b
Joris Goosen <joris@jorisgoosen.nl>
2025-05-20 16:29:58 +0200

    make buildAllDefaultJaspModules.sh work

--- Tools/buildAllDefaultJaspModules.sh
+++ Tools/buildAllDefaultJaspModules.sh
@@ -2,13 +2,14 @@
 
 args = commandArgs(trailingOnly=TRUE)
 
-doTest  <- FALSE
-doClone <- FALSE
+# the args thing doesnt seem to work but the defaults are probably what you want anyway. otherwise plerase commit the fix ;)
+doTest  = FALSE
+doClone = TRUE
 
-sapply(args, function(arg) { if(mod == "test")  doTest <- TRUE})
-sapply(args, function(arg) { if(mod == "clone") doClone <- TRUE})
+sapply(args, function(arg) { if(arg == "test")  { doTest = TRUE; }})
+sapply(args, function(arg) { if(arg == "clone") { doClone = TRUE; }})
 
-modules <- c(
+modules = c(
 "jaspDescriptives",
 "jaspTTests",
 "jaspAnova",
@@ -33,8 +34,13 @@ modules <- c(
 "jaspEsci"
 )
 
-if(doTest) modules <- rbind(modules, "jaspTestModule")
+if(doTest) modules = rbind(modules, "jaspTestModule")
 
+print('modules:')
+print(modules)
+
+workdir     <- './ModuleBundleBuildDir/Modules/'
+dir.create(workdir, recursive = TRUE)
 
 if(doClone) {
     print("Cloning all modules")
@@ -42,10 +48,15 @@ if(doClone) {
     setwd(workdir)
 
     for(module in modules) {
-        system2(paste0("git clone https://github.com/jasp-stats/", module), stdout=TRUE, stderr=TRUE)
+        system2(command="git", args=c("clone", paste0("https://github.com/jasp-stats/", module)))
     }
     setwd(oldwd)
 }
 
+modulePaths <- paste0(workdir, modules)
+
+print('modulePaths:')
+print(modulePaths)
+
 print("Building all modules")
-system2(paste0(./buildModuleBundlesLocally.sh ", paste0(workdir, modules)), stdout=TRUE, stderr=TRUE))
+system2(command="./buildModuleBundlesLocally.sh", args=modulePaths)
