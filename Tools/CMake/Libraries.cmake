25a5b7b9db896d5064cfefe7ee4d407158a4f1d3
Joris Goosen <joris@jorisgoosen.nl>
2025-06-10 15:27:19 +0200

    fix macos build

--- Tools/CMake/Libraries.cmake
+++ Tools/CMake/Libraries.cmake
@@ -250,7 +250,7 @@ if(APPLE)
 
   find_package(Brotli 1.0.9 REQUIRED)
   find_package(freexl 2.0.99 REQUIRED)
-  find_package(librdata 0.0.0.cci.20231003 REQUIRED)
+  find_package(librdata REQUIRED)
 
 endif()
 
