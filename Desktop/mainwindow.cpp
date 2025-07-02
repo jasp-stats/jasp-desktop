80874a6fa79f3d21151b0d0a3eab5a0d7ff9a242
Joris Goosen <Joris@JorisGoosen.nl>
2025-07-02 18:02:52 +0200

    use from LocalFile
    Co-authored-by: Shun Wang <shuonwang@gmail.com>

--- Desktop/mainwindow.cpp
+++ Desktop/mainwindow.cpp
@@ -853,7 +853,7 @@ void MainWindow::showLogFolder() const
 
 void MainWindow::openURLFile(QString fileURLPath)
 {
-	QUrl fileUrl(fileURLPath);
+	QUrl fileUrl = QUrl::fromLocalFile(fileURLPath);
 	if (!fileUrl.isLocalFile())
 	{
 		MessageForwarder::showWarning(tr("Open file"), tr("Cannot access file %1").arg(fileURLPath));
