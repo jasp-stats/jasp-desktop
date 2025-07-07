7940c9a27bed3f0a9038747f86db78ca05e52ceb
RensDofferhoff <20978635+RensDofferhoff@users.noreply.github.com>
2025-07-07 17:25:31 +0200

    Update main.cpp

--- Desktop/container_permission_checker/main.cpp
+++ Desktop/container_permission_checker/main.cpp
@@ -4,7 +4,7 @@
 #include <string>
 
 //quickly check if we have access to dirs/files listed in argv[]. return -1 if permissions need to be fixed by jasp
-int main( int argc, char* argv[] ) {
+int wmain( int argc, wchar_t * argv[] ) {
     if(argc < 2) {
         std::cout << "Please provid a path to a file that list all paths to test" << std::endl;
         return -2;
@@ -12,7 +12,7 @@ int main( int argc, char* argv[] ) {
     
     for(int i = 1; i < argc; i++)
     {
-        std::string line = std::string(argv[i]);
+        std::wstring line = std::wstring(argv[i]);
         std::filesystem::file_status status;
         try {
             status = std::filesystem::status(line);
