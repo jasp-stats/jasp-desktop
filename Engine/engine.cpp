4b3b74aeda69bf7c772e8fed6b9f7f51ff77fa4a
Joris Goosen <joris@jorisgoosen.nl>
2025-07-08 12:21:33 +0200

    no response is not a good response probably?

--- Engine/engine.cpp
+++ Engine/engine.cpp
@@ -430,9 +430,10 @@ void Engine::runRCode(const std::string & rCode, int rCodeRequestId, bool whiteL
 	
 
 	std::string rCodeResult = whiteListed ? rbridge_evalRCodeWhiteListed(rCode.c_str(), true) : jaspRCPP_evalRCode(rCode.c_str(), true);
+	bool		hadError	= rCodeResult == "null" || rCodeResult == "";
 
-	if (rCodeResult == "null")	sendRCodeError(rCodeRequestId);
-	else						sendRCodeResult(rCodeRequestId, rCodeResult);
+	if (hadError)	sendRCodeError(rCodeRequestId);
+	else			sendRCodeResult(rCodeRequestId, rCodeResult);
 
 	_engineState = engineState::idle;
 }
