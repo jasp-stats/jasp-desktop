f292de2d4e2a1a721a9273662206f8038614a932
Joris Goosen <joris@jorisgoosen.nl>
2025-07-09 11:51:14 +0200

    Make sure doubles are converted to labels properly!
    Fixes https://github.com/jasp-stats/INTERNAL-jasp/issues/2917

--- CommonData/column.cpp
+++ CommonData/column.cpp
@@ -1933,17 +1933,13 @@ void Column::upgradeDoublesToLabels()
 	
 	beginBatchedLabelsDB();
 	
-	for(double dbl : dbls)
-		if(!dblToIntsId.count(dbl))
-		{
-			std::string		dblStr	= Column::doubleToDisplayString(dbl);
-			Label		*	label	= labelByValue(dblStr);
-			dblToIntsId		[dbl]	= label ? label->intsId() : labelsAdd(dblStr);
-		}
-	
 	for(size_t row=0; row<_dbls.size(); row++)
-		if(_ints[row] == Label::NO_LABEL && dblToIntsId.count(_dbls[row]))
-			_ints[row] = dblToIntsId[_dbls[row]];
+		if(!std::isnan(_dbls[row]) && _ints[row] == Label::NO_LABEL)
+		{
+			std::string		dblStr	= Column::doubleToDisplayString(_dbls[row], false, false);
+			Label		*	label	= labelByValue(dblStr);
+			_ints[row]				= label ? label->intsId() : labelsAdd(dblStr);
+		}
 }
 
 void Column::upgradeExtractDoublesIntsFromLabels()
