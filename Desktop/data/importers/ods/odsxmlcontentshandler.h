6330bf519d39c1e59f05fadd558c51ba9e0d6ecd
shun2wang <shuonwang@gmail.com>
2026-02-07 15:59:57 +0800

    remove also from docs

--- Desktop/data/importers/ods/odsxmlcontentshandler.h
+++ Desktop/data/importers/ods/odsxmlcontentshandler.h
@@ -27,8 +27,8 @@ class ODSXmlContentsHandler : public XmlHandler
 public:
 	explicit ODSXmlContentsHandler(ODSImportDataSet *dta);
 
-	bool parse(QXmlStreamReader &reader);
-	void resetDocument();
+	bool 	parse(QXmlStreamReader &reader);
+	void 	resetDocument();
 
 private:
 	void		processStartElement(const QXmlStreamReader &reader);
@@ -46,17 +46,24 @@ private:
 	bool			_tableRead			= false;
 	XmlDatatype		_lastType			= odsType_unknown;
 	int				_colRepeat			= 1,
-					_rowRepeat          = 1;
+					_rowRepeat			= 1;
 	QString			_currentCell,
 					_currentComment;
-
+	// Names of elements
 	static const QString _nameBody, _nameTable, _nameTableRow, _nameDocContent,
 						_nameSpreadsheet, _nameAnnotation, _nameTableCell, _nameText;
+
+	// Attribute names
 	static const QString _attValue, _attValueType, _attDateValue, _attTimeValue,
 						_attBoolValue, _attCellRepeatCount, _attRowRepeatCount;
+
+	// Values of the attribute attValueType
 	static const QString _typeCurrency, _typePercent, _typeBoolean, _typeString,
 						_typeFloat, _typeDate, _typeTime;
 
+	// We don't wants to repeat empty cells too many times.
+	// This is of course not very sensible so instead we detect that and ignore such cells.
+	// To do this we need to know the maximum size of an excelspreadsheet and it is:
 	const int _excelMaxRows = 1048576, _excelMaxCols = 16384;
 };
 
