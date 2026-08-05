3bf6acff5825be48771399c94c81637571f20324
RensDofferhoff <20978635+RensDofferhoff@users.noreply.github.com>
2026-08-05 09:38:00 +0200

    Update BuildBotScript.cmd

--- Tools/windows/BuildBotScript.cmd
+++ Tools/windows/BuildBotScript.cmd
@@ -38,8 +38,4 @@ cmake --build build --target collect-junctions
 
 cmake --build build --target zip
 
-cmake --install build --component MSIX
-
-cmake --build build --target msix
-
 cmake --build build --target upload
